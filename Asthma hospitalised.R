ee4a72a363491a02ccd6f96c72de6acb42f696cd
jvillacampa <jaime.villacampa@hotmail.com>
2018-10-05 10:46:31 +0100

    fixing odbc connection

--- Asthma hospitalised.R
+++ Asthma hospitalised.R
@@ -24,11 +24,11 @@ source("./1.indicator_analysis.R") #Normal indicator functions
 ###############################################.
 # SMRA login information
 channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
-            id=.rs.askForPassword("SMRA Username:"), pwd=.rs.askForPassword("SMRA Password:")))
+            uid=.rs.askForPassword("SMRA Username:"), pwd=.rs.askForPassword("SMRA Password:")))
 
 #Looking to admissions with a main diagnosis of asthma, excluding unknown sex, by financial year. 
 #Creates one record per CIS and selects only one case per patient/year.
-data <- tbl_df(dbGetQuery(channel, statement=
+data_asthma <- tbl_df(dbGetQuery(channel, statement=
   "SELECT distinct link_no linkno, min(AGE_IN_YEARS) age, min(SEX) sex_grp, min(DR_POSTCODE) pc7,
       CASE WHEN extract(month from admission_date) > 3 THEN extract(year from admission_date) 
         ELSE extract(year from admission_date) -1 END as year 
@@ -41,7 +41,7 @@ data <- tbl_df(dbGetQuery(channel, statement=
   setNames(tolower(names(.)))  #variables to lower case
 
 # Creating age groups for standardization.
-data <- data %>% mutate(age_grp = case_when( 
+data_asthma <- data_asthma %>% mutate(age_grp = case_when( 
   age < 5 ~ 1, age > 4 & age <10 ~ 2, age > 9 & age <15 ~ 3, age > 14 & age <20 ~ 4,
   age > 19 & age <25 ~ 5, age > 24 & age <30 ~ 6, age > 29 & age <35 ~ 7, 
   age > 34 & age <40 ~ 8, age > 39 & age <45 ~ 9, age > 44 & age <50 ~ 10,
@@ -55,7 +55,7 @@ data <- data %>% mutate(age_grp = case_when(
 postcode_lookup <- read_csv('/conf/linkage/output/lookups/geography/Scottish_Postcode_Directory_2017_2.csv') %>% 
   setNames(tolower(names(.)))  #variables to lower case
 
-data <- left_join(data, postcode_lookup, "pc7") %>% 
+data_asthma <- left_join(data_asthma, postcode_lookup, "pc7") %>% 
   select(year, age_grp, age, sex_grp, datazone2001, datazone2011, ca2011) %>% 
   subset(!(is.na(datazone2011))) %>%  #select out non-scottish
   mutate_if(is.character, factor) # converting variables into factors
@@ -65,7 +65,7 @@ data <- left_join(data, postcode_lookup, "pc7") %>%
 ###############################################.
 ###############################################.
 # Datazone2011
-dz11 <- data %>% group_by(year, datazone2011, sex_grp, age_grp) %>%  
+dz11 <- data_asthma %>% group_by(year, datazone2011, sex_grp, age_grp) %>%  
   summarize(numerator = n()) %>% ungroup() %>%  rename(datazone = datazone2011)
 
 saveRDS(dz11, file=paste0(prepared_data, 'asthma_dz11_raw.rds'))
@@ -73,14 +73,14 @@ datadz <- readRDS(paste0(prepared_data, 'asthma_dz11_raw.rds'))
 
 ###############################################.
 # CA file for under 16 cases 
-ca_under16 <- data %>% subset(age<16) %>% group_by(year, ca2011, sex_grp, age_grp) %>%  
+ca_under16 <- data_asthma %>% subset(age<16) %>% group_by(year, ca2011, sex_grp, age_grp) %>%  
   summarize(numerator = n()) %>% ungroup() %>%   rename(ca = ca2011)
 
 saveRDS(ca_under16, file=paste0(prepared_data, 'asthma_under16_raw.rds'))
 
 ###############################################.
 # Datazone2001. Only used for IRs
-dz01 <- data %>% group_by(year, datazone2001, sex_grp, age_grp) %>%  
+dz01 <- data_asthma %>% group_by(year, datazone2001, sex_grp, age_grp) %>%  
   summarize(numerator = n()) %>% ungroup() %>% subset(year<2011) %>% rename(datazone = datazone2001)
 
 saveRDS(dz01, file=paste0(prepared_data, 'asthma_dz01_raw.rds'))
