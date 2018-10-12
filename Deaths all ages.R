38a65f6d8cc7f991bd65516b0eb216032ea1e714
vicky e <victoria.elliott@nhs.net>
2018-10-12 13:35:20 +0100

    deaths indicator taking data from SMRA

--- Deaths all ages.R
+++ Deaths all ages.R
@@ -1,17 +1,16 @@
-# ScotPHO indicators: Deaths all ages
+# ScotPHO indicators: Deaths all ages (could also be called All Cause Mortality (All Ages))
 
-#   Part 1 - Extract data from SMRA.
-#   Part 2 -  Create the different geographies basefiles
+#   Part 1 - Extract data from SMRA - Deaths file.
+#   Part 2 - Create the different geographies basefiles
 #   Part 3 - Run macros
 
-library(odbc) #load first or packages seem to conflict
-library(haven) #for SPPS file reading
-library(dplyr)
-library(readr)
+
 
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
+lapply(c("odbc", "readr", "dplyr"), library, character.only = TRUE)
+
 
 server_desktop <- "server" # change depending if you are using R server or R desktop
 if (server_desktop == "server") {
@@ -21,36 +20,31 @@ if (server_desktop == "server") {
 }
 
 source("./1.indicator_analysis.R") #Normal indicator functions
+source("./2.deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Extract data from SMRA ----
 ###############################################.
-# # SMRA login information
-# channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
-#        id=.rs.askForPassword("SMRA Username:"), pwd=.rs.askForPassword("SMRA Password:")))
-# 
-#  
-# #Looking to admissions with a main diagnosis of asthma, excluding unknown sex, by financial year. 
-# 
-# data <- tbl_df(dbGetQuery(channel, statement=
-#                             "SELECT year_of_registration year, age, SEX sex_grp, POSTCODE pc7
-#    FROM ANALYSIS.GRO_DEATHS_C z
-#    where date_of_registration between '2002-01-01' and '2017-12-31'
-#    AND regexp_like(country_of_residence, 'XS')      
-#    AND sex <> 9
-#    AND age is not NULL")) %>% 
-#   setNames(tolower(names(.))  #variables to lower case
-           
-##can't access SMRA tables yet so data coming from spss extract
-data<- read_csv("/conf/phip/Projects/Profiles/Data/Indicators/Deaths, Injury and Disease/Raw Data/Prepared Data/deaths_allages_smraextract.csv")%>%
-  setNames(tolower(names(.))) %>% #variables to lower case
-  mutate_if(is.character, factor) # converting variables into factors
-#  mutate_all(factor) # converting variables into factors
-data$sex_grp <- as.factor(data$sex_grp) #reading in data needed to cheat to get factors correct
-  
-  
+# SMRA login information
+channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
+                                      uid=.rs.askForPassword("SMRA Username:"), pwd=.rs.askForPassword("SMRA Password:")))
+
+#select data from deaths file
+#select only deaths for scottish residents (XS)
+#exclude any with null age group
+#exclude deaths where sex is unknown (9)
+
+data_deaths <- tbl_df(dbGetQuery(channel, statement=
+                                   "SELECT year_of_registration year, age, SEX sex_grp, POSTCODE pc7
+                                 FROM ANALYSIS.GRO_DEATHS_C 
+                                 WHERE date_of_registration between '1 January 2002' AND '31 December 2017'
+                                 AND country_of_residence ='XS'
+                                 AND age is not NULL
+                                 AND sex <> 9")) %>%
+  setNames(tolower(names(.)))  #variables to lower case
+
 # Creating age groups for standardization.
-data <- data %>% mutate(age_grp = case_when( 
+data_deaths <- data_deaths %>% mutate(age_grp = case_when( 
   age < 5 ~ 1, age > 4 & age <10 ~ 2, age > 9 & age <15 ~ 3, age > 14 & age <20 ~ 4,
   age > 19 & age <25 ~ 5, age > 24 & age <30 ~ 6, age > 29 & age <35 ~ 7, 
   age > 34 & age <40 ~ 8, age > 39 & age <45 ~ 9, age > 44 & age <50 ~ 10,
@@ -59,7 +53,7 @@ data <- data %>% mutate(age_grp = case_when(
   age > 79 & age <85 ~ 17, age > 84 & age <90 ~ 18, age > 89 ~ 19, 
   TRUE ~ as.numeric(age)
 ))
-           
+
 # Open LA and datazone info.
 # Pc 2017_2 used even though 2018 available to avoid issues with changing codes (but no diff to geographies) of fife/tayside LA/NHS board
 
@@ -67,7 +61,7 @@ postcode_lookup <- read_csv('/conf/linkage/output/lookups/geography/Scottish_Pos
   setNames(tolower(names(.)))  #variables to lower case
 
 
-data <- left_join(data, postcode_lookup, "pc7") %>% 
+data_deaths <- left_join(data_deaths, postcode_lookup, "pc7") %>% 
   select(year, age_grp, age, sex_grp, datazone2001, datazone2011, ca2011) %>% 
   subset(!(is.na(datazone2011))) %>%  #select out non-scottish
   mutate_if(is.character, factor) # converting variables into factors
@@ -77,22 +71,14 @@ data <- left_join(data, postcode_lookup, "pc7") %>%
 ###############################################.
 ###############################################.
 # Datazone2011
-dz11 <- data %>% group_by(year, datazone2011, sex_grp, age_grp) %>%  
+dz11 <- data_deaths %>% group_by(year, datazone2011, sex_grp, age_grp) %>%  
   summarize(numerator = n()) %>% ungroup() %>%  rename(datazone = datazone2011)
 
 saveRDS(dz11, file=paste0(prepared_data, 'deaths_allages_dz11_raw.rds'))
 datadz <- readRDS(paste0(prepared_data, 'deaths_allages_dz11_raw.rds'))
 
-###############################################.
-# # CA file for under 16 cases 
-# ca_under16 <- data %>% subset(age<16) %>% group_by(year, ca2011, sex_grp, age_grp) %>%  
-#   summarize(numerator = n()) %>% ungroup() %>%   rename(ca = ca2011)
-# 
-# saveRDS(ca_under16, file=paste0(prepared_data, 'asthma_under16_raw.rds'))
-
-###############################################.
 # Datazone2001. Only used for IRs
-dz01 <- data %>% group_by(year, datazone2001, sex_grp, age_grp) %>%  
+dz01 <- data_deaths %>% group_by(year, datazone2001, sex_grp, age_grp) %>%  
   summarize(numerator = n()) %>% ungroup() %>% subset(year<2011) %>% rename(datazone = datazone2001)
 
 saveRDS(dz01, file=paste0(prepared_data, 'deaths_allages_dz01_raw.rds'))
@@ -103,6 +89,14 @@ ir_file <- dz11 %>% subset(year>2010)
 ir_file <- rbind(dz01, ir_file) #joining together
 
 saveRDS(ir_file, file=paste0(prepared_data, 'DZ_deaths_allages_IR_raw.rds'))
+
+###############################################.
+#Deprivation indicator numerator file
+
+#deaths_allages_depr <- ir_file %>% rename(code = datazone)
+
+saveRDS(ir_file, file=paste0(prepared_data, 'deaths_allages_depr_raw.rds'))
+
 
 
 ###############################################.
@@ -118,4 +112,16 @@ analyze_second(filename = "deaths_allages_dz11", measure = "stdrate", time_agg =
                epop_total = 200000, ind_id = 20103, year_type = "calendar", 
                profile = "HN", min_opt = 1245385)
 
-##END
+
+
+#############################################.
+#Deprivation analysis function
+
+analyze_deprivation(filename="deaths_allages_depr", measure="stdrate", time_agg=3, 
+                    crude_rate = 1000, yearstart= 2004, yearend=2017,  
+                    year_type = "calendar", pop = "depr_pop_allages", 
+                    epop_age="normal", epop_total =200000, ind_id = 10101)
+
+#END
+
+
