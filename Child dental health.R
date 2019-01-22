eb61fcd5c80ae19f1c008316ed1cae20704ebe3a
jvillacampa <jaime.villacampa@hotmail.com>
2019-01-22 12:35:18 +0000

    ranges, ind_id

--- Child dental health.R
+++ Child dental health.R
@@ -30,13 +30,13 @@ data_p1 <- as.data.frame(rbind(
   read_excel_sheet(sheet = "2014_P1_C_DZ2011", range = "A5:E13189"),
   read_excel_sheet(sheet = "2015_P1_C_DZ2011", range = "A5:E13186"),
   read_excel_sheet(sheet = "2016_P1_C_DZ2011", range = "A5:E13004"),
-  read_excel_sheet(sheet = "2017_P1_C_DZ2011", range = "A5:E12972"),
-  read_excel_sheet(sheet = "2018_P1_letter_C", range = "A4:E13074"))) %>% 
+  read_excel_sheet(sheet = "2017_P1_letter_C", range = "A4:E12971"),
+  read_excel_sheet(sheet = "2018_P1_letter_C", range = "A4:E13075"))) %>% 
   group_by(datazone, year) %>% #aggregating 
   mutate(numerator = as.numeric(numerator)) %>% 
   summarise(numerator = sum(numerator, na.rm =T), 
             denominator = sum(denominator, na.rm =T)) %>%  ungroup()
-  
+
 saveRDS(data_p1, file=paste0(data_folder, 'Prepared Data/child_dental_p1_raw.rds'))
 #Saving file for deprivation, only from 2014 for simd2016
 data_p1_depr <- data_p1 %>% filter(year>=2014)
@@ -50,8 +50,8 @@ data_p7 <- as.data.frame(rbind(
   read_excel_sheet(sheet = "2014_P7_C_DZ2011", range = "A5:E12775"),
   read_excel_sheet(sheet = "2015_P7_C_DZ2011", range = "A5:E12721"),
   read_excel_sheet(sheet = "2016_P7_C_DZ2011", range = "A5:E12836"),
-  read_excel_sheet(sheet = "2017_P7_C_DZ2011", range = "A5:E12955"),
-  read_excel_sheet(sheet = "2018_P7_letter_C", range = "A4:E12955"))) %>% 
+  read_excel_sheet(sheet = "2017_P7_letter_C", range = "A4:E12954"),
+  read_excel_sheet(sheet = "2018_P7_letter_C", range = "A4:E12968"))) %>% 
   group_by(datazone, year) %>% #aggregating 
   mutate(numerator = as.numeric(numerator)) %>% 
   summarise(numerator = sum(numerator, na.rm =T), 
@@ -91,6 +91,6 @@ analyze_second(filename = "child_dental_p7", measure = "perc_pcf", time_agg = 1,
 
 analyze_deprivation(filename="child_dental_p7_depr", measure="perc_pcf",  
                     yearstart= 2014, yearend=2017, time_agg=1,
-                    year_type = "school", pop_pcf = "depr_pop_11", ind_id = 21005)
+                    year_type = "school", pop_pcf = "depr_pop_11", ind_id = 21006)
 
 ##END
