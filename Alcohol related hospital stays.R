9798ad64eb369b0e80adf42d87aedb32c49407ef
Jaime Villacampa <jaime.villacampa@hotmail.com>
2019-05-15 12:40:23 +0100

    tidying

--- Alcohol related hospital stays.R
+++ Alcohol related hospital stays.R
@@ -8,50 +8,55 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-
-#libraries
-
-# library(odbc)  # run odbc first
-# library(dplyr)
-# library(readr)
 library(lubridate) #requires lubridate to derive financial year of stay
 
-source("./1.indicator_analysis.R") #Normal indicator functions
-source("./2.deprivation_analysis.R") # deprivation function
-
+source("1.indicator_analysis.R") #Normal indicator functions
+source("2.deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Extract data from SMRA ----
 ###############################################.
 # SMRA login information
 channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
-                                      uid=.rs.askForPassword("SMRA Username:"), pwd=.rs.askForPassword("SMRA Password:")))
+                                      uid=.rs.askForPassword("SMRA Username:"), 
+                                      pwd=.rs.askForPassword("SMRA Password:")))
 
 # Extract alcohol stay CIS data 
-# This extraction should give figures that match the methodology used by ISD alcohol team who publish national statistics for alcohol related admissions.
-# ScotPHO should not publish updated indicator until after the national statistics publication has been released.
-# Diagnostic codes should match those use by the ISD ARHS national statistics publication.  The codes used may differ from alcohol related deaths indicators since
+# This extraction should give figures that match the methodology used by ISD alcohol 
+# team who publish national statistics for alcohol related admissions.
+# ScotPHO should not publish updated indicator until after the national 
+# statistics publication has been released.
+# Diagnostic codes should match those use by the ISD ARHS national statistics publication.  
+# The codes used may differ from alcohol related deaths indicators since
 # alcohol related deaths focus on conditions wholly attributable to alcohol.
 
 # SMRA extraction returns all episodes data within a continuous inpatient stay
-# The CIS are restricted to stays where an individual has one or more alcohol related ICD10 diagnosis codes (in any position)
-# Date restrictions are based on financial year of hospital discharge (not episode discharge) therefore date filters in extract are set to the 
-# year before desired data (eg if period for reporting is 2002/2003 then request data from 2001 if you want to be sure to capture all CIS that end in 2002/03)
+# The CIS are restricted to stays where an individual has one or more alcohol 
+# related ICD10 diagnosis codes (in any position)
+# Date restrictions are based on financial year of hospital discharge (not episode discharge) 
+# therefore date filters in extract are set to the 
+# year before desired data (eg if period for reporting is 2002/2003 then request 
+# data from 2001 if you want to be sure to capture all CIS that end in 2002/03)
 
+alc_diag <- "E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721"
 
-data_alcohol_episodes <- tbl_df(dbGetQuery(channel, statement=
-       "SELECT link_no linkno, cis_marker cis, AGE_IN_YEARS age, admission_date, discharge_date, DR_POSTCODE pc7, SEX sex_grp, ADMISSION, DISCHARGE, URI
+data_alcohol_episodes <- tbl_df(dbGetQuery(channel, statement= paste0(
+  "SELECT link_no linkno, cis_marker cis, AGE_IN_YEARS age, admission_date, 
+      discharge_date, DR_POSTCODE pc7, SEX sex_grp, ADMISSION, DISCHARGE, URI
   FROM ANALYSIS.SMR01_PI z
-  where discharge_date between  '1 April 2001' and '31 March 2018'
-  and sex <> 9
-  and exists (select * from ANALYSIS.SMR01_PI  where link_no=z.link_no and cis_marker=z.cis_marker
-  and discharge_date between '1 April 2001' and '31 March 2018'
-  and (regexp_like(main_condition,'E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721')
-  or regexp_like(other_condition_1,'E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721')
-  or regexp_like(other_condition_2,'E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721')
-  or regexp_like(other_condition_3,'E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721')
-  or regexp_like(other_condition_4,'E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721')
-  or regexp_like(other_condition_5,'E244|E512|F10|G312|G621|G721|I426|K292|K70|K852|K860|O354|P043|Q860|R780|T510|T511|T519|X45|X65|Y15|Y573|Y90|Y91|Z502|Z714|Z721')))")) %>%
+  WHERE discharge_date between  '1 April 2001' and '31 March 2018'
+      and sex <> 9
+      and exists (
+          select * 
+          from ANALYSIS.SMR01_PI  
+          where link_no=z.link_no and cis_marker=z.cis_marker
+            and discharge_date between '1 April 2001' and '31 March 2018'
+            and (regexp_like(main_condition, '", alc_diag ,"')
+              or regexp_like(other_condition_1,'", alc_diag ,"')
+              or regexp_like(other_condition_2,'", alc_diag ,"')
+              or regexp_like(other_condition_3,'", alc_diag ,"')
+              or regexp_like(other_condition_4,'", alc_diag ,"')
+              or regexp_like(other_condition_5,'", alc_diag ,"')))"))) %>%
   setNames(tolower(names(.)))  #variables to lower case
 
 # Group episode level alcohol data into hospital stays (number of rows will reduce)
@@ -71,7 +76,6 @@ data_alcoholstays <- data_alcohol_episodes  %>%
 
 # Creating age groups for standardization.
 data_alcoholstays <- data_alcoholstays %>%
-  # subset(year>=2002) %>%
   mutate(age_grp = case_when( 
     age < 5 ~ 1, age > 4 & age <10 ~ 2, age > 9 & age <15 ~ 3, age > 14 & age <20 ~ 4,
     age > 19 & age <25 ~ 5, age > 24 & age <30 ~ 6, age > 29 & age <35 ~ 7, 
@@ -95,12 +99,10 @@ data_alcoholstays <- data_alcoholstays %>%
   subset(!(is.na(datazone2011))) %>%  #select out non-scottish
   mutate_if(is.character, factor) # converting variables into factors
 
-
 ###############################################.
 ## Part 2 - Create the different geographies basefiles ----
 ###############################################.
 ###############################################.
-
 # Datazone2011
 dz11 <- data_alcoholstays %>% 
   group_by(year, datazone2011, sex_grp, age_grp) %>%  
@@ -109,8 +111,6 @@ dz11 <- data_alcoholstays %>%
 saveRDS(dz11, file=paste0(data_folder, 'Prepared Data/alcohol_stays_dz11_raw.rds'))
 datadz <- readRDS(paste0(data_folder, 'Prepared Data/alcohol_stays_dz11_raw.rds')) %>%
   mutate_if(is.character, factor)
-
-
 
 ###############################################.
 #Deprivation basefile
@@ -125,7 +125,6 @@ dep_file <- rbind(dz01_dep, dz11 %>% subset(year>=2014)) #joing dz01 and dz11
 
 saveRDS(dep_file, file=paste0(data_folder, 'Prepared Data/alcohol_stays_depr_raw.rds'))
 
-
 ###############################################.
 # CA (council area) file for separate indicator in CYP profile for those aged 11 to 25 years
 
@@ -138,17 +137,15 @@ alcoholstays_11to25 <- data_alcoholstays %>%
 
 saveRDS(alcoholstays_11to25, file=paste0(data_folder, 'Prepared Data/alcohol_stays_11to25_raw.rds'))
 
-
-
 ###############################################.
 ## Part 3 - Run analysis functions ----
 ###############################################.
-
+###############################################.
 ##Run macros to generate HWB and Alcohol Profile indicator data
-#All ages alcohol related hospital stays ----
-analyze_first(filename = "alcohol_stays_dz11", geography = "datazone11", adp = TRUE, measure = "stdrate", 
+# All ages alcohol related hospital stays 
+analyze_first(filename = "alcohol_stays_dz11", geography = "datazone11", measure = "stdrate", 
               pop = "DZ11_pop_allages", yearstart = 2002, yearend = 2017,
-              time_agg = 1, epop_age = "normal")
+              adp = TRUE, time_agg = 1, epop_age = "normal")
 
 analyze_second(filename = "alcohol_stays_dz11", measure = "stdrate", time_agg = 1, 
                epop_total = 200000, ind_id = 20203, year_type = "financial", 
@@ -160,8 +157,9 @@ analyze_deprivation(filename="alcohol_stays_depr", measure="stdrate", time_agg=1
                     pop = "depr_pop_allages", epop_age="normal",
                     epop_total =200000, ind_id = 20203)
 
+###############################################.
 ##Run macros again to generate CYP indicator data
-#Alcohol related stays in 11 to 25 year olds ----
+#Alcohol related stays in 11 to 25 year olds 
 analyze_first(filename = "alcohol_stays_11to25", geography = "council", measure = "stdrate", 
               pop = "CA_pop_11to25", yearstart = 2002, yearend = 2017,
               time_agg = 3, epop_age = '11to25')
@@ -171,11 +169,6 @@ analyze_second(filename = "alcohol_stays_11to25", measure = "stdrate", time_agg 
                profile = "CP", min_opt = 2999)
 
 
-
-obcClose(channel) # closing connection to SMRA
+rm(channel) # closing connection to SMRA
 
 ##END
-
-
-
-
