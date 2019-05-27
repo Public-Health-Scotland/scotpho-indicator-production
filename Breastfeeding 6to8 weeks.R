2a621c30721fce44f026879e023984acbb645cf5
Jaime Villacampa <jaime.villacampa@hotmail.com>
2019-05-27 16:02:42 +0100

    simplifying code and progressing with exclusions

--- Breastfeeding 6to8 weeks.R
+++ Breastfeeding 6to8 weeks.R
@@ -9,19 +9,8 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-# Varies filepaths depending on if using server or not.
-if (sessionInfo()$platform == "x86_64-redhat-linux-gnu (64-bit)") {
-  cl_out_depr <- "/conf/linkage/output/lookups/Unicode/Deprivation/"
-} else {
-  cl_out_depr <- "//stats/linkage/output/lookups/Unicode/Deprivation/"
-}
-
 source("1.indicator_analysis.R") #Normal indicator functions
 source("2.deprivation_analysis.R") # deprivation function
-
-
-library(stringi) #for manipulation of string text to generate year field
-
 
 ###############################################.
 ## Part 1 - Prepare basefile ----
@@ -31,18 +20,15 @@ breastfed <- read_csv(paste0(data_folder, "Received Data/IR2018-01997_6to8_breas
   filter(!is.na(datazone2011)) %>% # exclude rows with no datazone.
   mutate(datazone = as.factor(datazone2011))
 
-
 #fin year is financial year - needs reformatting 203 = 2002/03 recode to 2002 
   breastfed <- breastfed %>%
-    mutate(length=stri_length(fin_year), #find lenght of year field, if length 3 then year period prior to 2010
-           year1=as.character(fin_year), 
-           year=case_when(length==3 ~ paste0("200",substr(year1,1,1)), TRUE ~ paste0("20",substr(year1,1,2)))) %>%
+    mutate( #creates year field based on lenght of fin_year
+           year=case_when(nchar(fin_year)==3 ~ paste0("200",substr(fin_year,1,1)), 
+                          TRUE ~ paste0("20",substr(fin_year,1,2)))) %>%
     group_by(year, datazone) %>%
-    summarise(numerator = sum(excbf_6to8wk),
-              denominator = sum(tot_6to8wk))
+    summarise(numerator = sum(excbf_6to8wk), denominator = sum(tot_6to8wk))
   
 saveRDS(breastfed, file=paste0(data_folder, 'Prepared Data/breastfed_raw.rds')) 
-  
   
 ###############################################.
 ## Part 2 - Run analysis functions ----
@@ -51,11 +37,10 @@ analyze_first(filename = "breastfed", geography = "datazone11", measure = "perce
               yearstart = 2002, yearend = 2017, time_agg = 3 )
 
 ## Exclusions at this point for geographies where denominator <=5 for an area  
-data_indicator <- readRDS(file=paste0(data_folder, "Temporary/breastfed", "_formatted.rds"))%>%
+data_indicator <- readRDS(file=paste0(data_folder, "Temporary/breastfed_formatted.rds")) %>%
   subset(denominator>5)
 
-saveRDS(data_indicator, file=paste0(data_folder, "Temporary/breastfed", "_formatted.rds"))
-
+saveRDS(data_indicator, file=paste0(data_folder, "Temporary/breastfed_formatted.rds"))
 
 analyze_second(filename = "breastfed", measure = "percent", time_agg = 3, 
                ind_id = 21004, year_type = "financial", min_opt = 1423811, profile="HN") 
@@ -72,17 +57,38 @@ analyze_second(filename = "breastfed", measure = "percent", time_agg = 3,
 #   *Shetland (S08000026, S12000027) data available from 2008/09
 #   *Grampian (S08000020, S12000020, S12000033, S12000034) & Orkney (S08000025, S12000023) data available  from 2010/11.
 #   
+# Excluding data for boards, hscps, las, localities and izs with incomplete data
+# Merging final data with parent geographies lookup and then filtering
+geo_parents <- readRDS(paste0(lookups, "Geography/IZtoPartnership_parent_lookup.rds")) %>% 
+  #TEMPORARY FIX. dealing with change in ca, hb and hscp codes
+  mutate(hscp_partnership = recode(hscp_partnership, "S37000014"='S37000032', 
+                                   "S37000023"='S37000033')) %>% 
+  gather(geotype, code, c(intzone2011, hscp_locality)) %>% distinct() %>% 
+  select(-geotype) %>% rename(parent_area = hscp_partnership)
 
-data_shiny <- readRDS(file = paste0(data_folder, "Shiny Data/breastfed", "_shiny.rds")) %>%
-  filter(!(code %in% c('S08000028','S12000013') & year == 2005|2006)) %>%
-  filter(!(code %in% c('S08000022','S12000017') & year <= 2008)) %>%
-  filter(!(code %in% c('S08000026','S12000027') & year  == 2007|2008)) %>%
-  filter(!(code %in% c('S08000020', 'S08000025', 'S12000020', 'S12000023', 'S12000033', 'S12000034') & year  == 2009|2010))
-         
+data_shiny <- left_join(readRDS(file = paste0(data_folder, "Shiny Data/breastfed_shiny.rds")),                         
+                         geo_parents, by = "code") %>%
+  filter(!((code %in% c('S08000028','S12000013', "S37000031") | parent_area == "S37000031") &
+             year %in% c(2005, 2006))) %>%
+  # this bit doesn't work as expected and excludes some that it shouldn't (e.g. scotland 2003-2008)
+  # very puzzled about this, it doesn't work with year %in% c(2003,2004,2005,2006,2007, 2008)
+  mutate(test1 = case_when(((code %in% c('S08000022','S12000017', "S37000016") | parent_area == "S37000016") &
+                              year <= 2008)  ~ "Yes",
+         TRUE ~ "No")) %>% #creating the variable with the condition works
+  filter(!((code %in% c('S08000022','S12000017', "S37000016") | parent_area == "S37000016") &
+             year <= 2008)) %>% 
+  # end of chunk that fails
+  filter(!((code %in% c('S08000026','S12000027', "S37000026") | parent_area == "S37000026") &
+             year %in% c(2007, 2008))) %>%
+  filter(!((code %in% c('S08000020', 'S08000025', 'S12000020', 'S12000023', 'S12000033', 
+                       'S12000034', "S37000019", "S37000022", "S37000001", "S37000002") |
+              parent_area %in% c("S37000019", "S37000022", "S37000001", "S37000002")) & 
+             year %in% c(2009, 2010))) %>% 
+  select(-parent_area)
 
 # Resave both rds and csv files with exclusions
-saveRDS(data_shiny, file = paste0(data_folder, "Shiny Data/breastfed", "_shiny.rds"))
-write_csv(data_shiny, path = paste0(data_folder, "Shiny Data/breastfed", "_shiny.csv"))
+saveRDS(data_shiny, file = paste0(data_folder, "Shiny Data/breastfed_shiny.rds"))
+write_csv(data_shiny, path = paste0(data_folder, "Shiny Data/breastfed_shiny.csv"))
   
-
+##END
   
