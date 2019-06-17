5c18958b57b04d0b952777e0de42b2bbca6fe0df
Jaime Villacampa <jaime.villacampa@hotmail.com>
2019-06-17 15:00:47 +0100

    pop not needed and small changes

--- Child immunisation uptake - 5in1 & MMR.R
+++ Child immunisation uptake - 5in1 & MMR.R
@@ -8,11 +8,8 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-
-server_desktop <- "server" # change depending if you are using R server or R desktop
-
-source("./1.indicator_analysis.R") #Normal indicator functions
-source("./2.deprivation_analysis.R") # deprivation function
+source("1.indicator_analysis.R") #Normal indicator functions
+source("2.deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Prepare basefile: 5 in 1 ----
@@ -31,9 +28,6 @@ five01_data <- read.spss(paste0(data_folder, "Received Data/2003_2018_Scotpho_Ch
 # Rename variables into numerator and denominator
 five01_data <- five01_data %>% rename(denominator = total24, numerator = five24)
 
-saveRDS(five01_data, file=paste0(data_folder, 'Prepared Data/Immunisation_5in1_dz01_raw.rds'))
-
-
 # Datazone2011 - Read data (Immunisation uptake at 24 month) provided by child health team and aggregate
 
 five11_data <- read.spss( paste0(data_folder, "Received Data/2004_2018_Scotpho_ChildhoodImms_DZ_DZ2011.zsav"), 
@@ -47,12 +41,11 @@ five11_data <- read.spss( paste0(data_folder, "Received Data/2004_2018_Scotpho_C
 # Rename variables into numerator and denominator
 five11_data <- five11_data %>% rename(denominator = total24, numerator = five24)
 
+saveRDS(five11_data, file=paste0(data_folder, 'Prepared Data/Immunisation_5in1_dz11_raw.rds'))
 
 #Deprivation basefile
-five01_dep <- five01_data # to user later for deprivation basefile
-
 # DZ 2001 data needed up to 2013 to enable matching to advised SIMD
-five_dep_file <- rbind(five01_dep %>% subset(year<=2013), five11_data %>% subset(year>=2014)) 
+five_dep_file <- rbind(five01_data %>% subset(year<=2013), five11_data %>% subset(year>=2014)) 
 
 saveRDS(five_dep_file, file=paste0(data_folder, 'Prepared Data/Immunisation_5in1_depr_raw.rds'))
 
@@ -103,8 +96,8 @@ analyze_second(filename = "Immunisation_5in1_dz11", measure = "percent", time_ag
 
 #Deprivation analysis function
 analyze_deprivation(filename="Immunisation_5in1_depr", measure="percent", time_agg=3, 
-                    yearstart= 2014, yearend=2018,   year_type = "calendar", 
-                    pop = "depr_pop_allages", ind_id = 21103)
+                    yearstart= 2003, yearend=2018,   year_type = "calendar", 
+                    ind_id = 21103)
 
 ###############################################.
 ## Part 4 - Call analysis macros: MMR ----
@@ -116,10 +109,9 @@ analyze_first(filename = "Immunisation_MMR_dz11", geography = "datazone11", meas
 analyze_second(filename = "Immunisation_MMR_dz11", measure = "percent", time_agg = 3, 
                ind_id = 21104, year_type = "calendar", profile = "HN", min_opt = 1106333)
 
-
 #Deprivation analysis function
 analyze_deprivation(filename="Immunisation_MMR_dz11_depr", measure="percent", time_agg=3, 
                     yearstart= 2014, yearend=2018,   year_type = "calendar", 
-                    pop = "depr_pop_allages", ind_id = 21104)
+                    ind_id = 21104)
 
 ##END
