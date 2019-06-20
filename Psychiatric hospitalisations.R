f3362593295906d7050a4ef2c142b2d1a53a8f0d
Jaime Villacampa <jaime.villacampa@hotmail.com>
2019-06-20 14:46:17 +0100

    sql as mh team method

--- Psychiatric hospitalisations.R
+++ Psychiatric hospitalisations.R
@@ -7,29 +7,37 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("./1.indicator_analysis.R") #Normal indicator functions
-source("./2.deprivation_analysis.R") # deprivation function
+source("1.indicator_analysis.R") #Normal indicator functions
+source("2.deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Extract data from SMRA ----
 ###############################################.
 # SMRA login information
 channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
-                                      uid=.rs.askForPassword("SMRA Username:"), pwd=.rs.askForPassword("SMRA Password:")))
+                                      uid=.rs.askForPassword("SMRA Username:"), 
+                                      pwd=.rs.askForPassword("SMRA Password:")))
 
-#Looking to admissions with a main diagnosis of asthma, excluding unknown sex, by financial year. 
-#Creates one record per CIS and selects only one case per patient/year.
+#Extracting one record per patient with a pshychiatric admission per financial year
+#Only cases with a valid sex, excluding learning disabilities and those with no datazone. 
+#Selects last value as its mh team methodology and potentially more accurate.
+#Also using smra geo variables as mh team approach
 data_psychiatric <- tbl_df(dbGetQuery(channel, statement=
-                                        "SELECT distinct link_no linkno, max(AGE_IN_YEARS) age, max(SEX) sex_grp, max(DATAZONE_2001) datazone_2001, max(DATAZONE_2011) datazone_2011,
-                                      CASE WHEN extract(month from discharge_date) > 3 THEN extract(year from discharge_date) 
-                                      ELSE extract(year from discharge_date) -1 END as year 
-                                      FROM ANALYSIS.SMR04_PI z
-                                      WHERE discharge_date between '1 April 2002' and '31 March 2018'
-                                      AND specialty <> 'G5' 
-                                      AND sex <> 0 
-                                      AND datazone_2011 is not null 
-                                      GROUP BY link_no, CASE WHEN extract(month from discharge_date) > 3 THEN
-                                      extract(year from discharge_date) ELSE extract(year from discharge_date) -1 END")) %>% 
+  "SELECT distinct link_no linkno, max(AGE_IN_YEARS) age, max(SEX) sex_grp, 
+        max(DATAZONE_2001) datazone_2001, max(DATAZONE_2011) datazone_2011,
+        CASE WHEN extract(month from discharge_date) > 3 THEN extract(year from discharge_date) 
+            ELSE extract(year from discharge_date) -1 END as year,
+        MAX(age_in_years ) KEEP ( DENSE_RANK LAST ORDER BY discharge_date) as age, 
+        MAX( sex ) KEEP ( DENSE_RANK LAST ORDER BY discharge_date) as sex_grp, 
+        MAX( datazone_2011 ) KEEP ( DENSE_RANK LAST ORDER BY discharge_date) as datazone_2011, 
+        MAX( datazone_2001 ) KEEP ( DENSE_RANK LAST ORDER BY discharge_date) as datazone_2001 
+   FROM ANALYSIS.SMR04_PI z
+   WHERE discharge_date between '1 April 2002' and '31 March 2018'
+         AND specialty <> 'G5' 
+         AND sex in ('1', '2')
+         AND datazone_2011 is not null 
+   GROUP BY link_no, CASE WHEN extract(month from discharge_date) > 3 THEN
+      extract(year from discharge_date) ELSE extract(year from discharge_date) -1 END")) %>% 
   setNames(tolower(names(.)))  #variables to lower case
 
 # Creating age groups for standardization.
@@ -41,8 +49,7 @@ data_psychiatric <- data_psychiatric %>% mutate(age_grp = case_when(
   age > 49 & age <55 ~ 11, age > 54 & age <60 ~ 12, age > 59 & age <65 ~ 13, 
   age > 64 & age <70 ~ 14, age > 69 & age <75 ~ 15, age > 74 & age <80 ~ 16,
   age > 79 & age <85 ~ 17, age > 84 & age <90 ~ 18, age > 89 ~ 19, 
-  TRUE ~ as.numeric(age)
-))
+  TRUE ~ as.numeric(age) ))
 
 # Datazone2001 - Equivalent of aggregate
 dz01 <- data_psychiatric %>% group_by(year, datazone_2001, sex_grp, age_grp) %>%  
