d9b7eaff04d41b43c1af6ab118a034c1a8479ee8
Jaime Villacampa <jaime.villacampa@hotmail.com>
2019-07-03 16:18:47 +0100

    ir file not needed, deprivation can go longer back

--- Single Adult Dwellings.R
+++ Single Adult Dwellings.R
@@ -7,7 +7,7 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("./1.indicator_analysis.R") #Normal indicator functions
+source("1.indicator_analysis.R") #Normal indicator functions
 
 ###############################################.
 ## Part 1 - Create basefile ----
@@ -27,6 +27,8 @@ sad_data_numer <- sad_data_extract %>% select(-c(d2007:d2018)) %>%
     mutate(year = substr(year, 2, 5)) #Remove leading "n" in year
 
 sad_data <- inner_join(sad_data_numer, sad_data_denom) #joining together 
+
+saveRDS(sad_data, file=paste0(data_folder, 'Prepared Data/Single_Dwellings_depr_raw.rds'))
 
 #### Match lookup - datazone with local authority
 
@@ -62,16 +64,15 @@ sad_data_raw <- full_join(sad01_data, sad11_data)
 ###############################################.
 
 #### Prepare / Aggregate for specified geographies - LA, DZ11 & base IRs
-
 # Prepare / Aggregate by la
-sadLA_data_raw <- sad_data_raw %>%
+sadla_data_raw <- sad_data_raw %>%
   group_by(ca, year, dz) %>% 
   summarise_at(c("numerator", "denominator"), funs(sum), na.rm =T) %>% 
   filter(dz != "dz01" | year != "2014") %>% ungroup()
 
-sadLA_data_raw <- select(sadLA_data_raw,-c(dz))
+sadla_data_raw <- select(sadla_data_raw,-c(dz))
 
-saveRDS(sadLA_data_raw, file=paste0(data_folder, 'Prepared Data/Single_Dwellings_LA_raw.rds'))
+saveRDS(sadla_data_raw, file=paste0(data_folder, 'Prepared Data/Single_Dwellings_LA_raw.rds'))
 
 # Prepare / Aggregate by dz11
 sad11_data_raw <- sad_data_raw %>%
@@ -83,20 +84,11 @@ sad11_data_raw <- select(sad11_data_raw,-c(dz))
 
 saveRDS(sad11_data_raw, file=paste0(data_folder, 'Prepared Data/Single_Dwellings_dz11_raw.rds'))
 
-# Prepare for IR - dz01 & dz11
-sadIR_data_raw <- sad_data_raw %>%
-  filter(dz != "dz01" | year != "2014")
-
-sadIR_data_raw <- select(sadIR_data_raw,-c(ca, dz))
-
-saveRDS(sadIR_data_raw, file=paste0(data_folder, 'Prepared Data/DZ_single_dwellings_IR_raw.rds'))
-
 ###############################################.
 ## Part 3 - Call analysis macros ----
 ###############################################.
 
 #Calling first analysis function
-
 analyze_first(filename = "Single_Dwellings_LA", geography = "council", measure = "percent", 
               yearstart = 2007, yearend = 2013, time_agg = 1)
 
@@ -104,21 +96,17 @@ analyze_first(filename = "Single_Dwellings_dz11", geography = "datazone11", meas
               yearstart = 2014, yearend = 2018, time_agg = 1)
 
 # Merging CA, DZ11 together and save both periods together
-
 all_data <- rbind(readRDS(paste0(data_folder, "Temporary/Single_Dwellings_LA_formatted.rds")),
                   readRDS(paste0(data_folder, "Temporary/Single_Dwellings_dz11_formatted.rds")))
 saveRDS(all_data, file = paste0(data_folder, "Temporary/Single_Dwellings_all_formatted.rds"))
 
 #Calling second analysis function
-
 analyze_second(filename = "Single_Dwellings_all", measure = "percent", time_agg = 1, 
                ind_id = 20504, year_type = "calendar", profile = "HN", min_opt = 1133276)
 
 #Deprivation analysis function
-
-analyze_deprivation(filename="Single_Dwellings_dz11", measure="percent", time_agg=1, 
-                    yearstart= 2014, yearend=2018,   year_type = "calendar", 
+analyze_deprivation(filename="Single_Dwellings_depr", measure="percent", time_agg=1, 
+                    yearstart= 2007, yearend=2018,   year_type = "calendar", 
                     ind_id = 20504)
-
 
 ##END
