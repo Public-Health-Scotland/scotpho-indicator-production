d368d68dc9ae6ef9f82b4f9d235f3464e8884dfe
vicky e <victoria.elliott@nhs.net>
2019-07-22 17:25:05 +0100

    Apply exclusions where data incompleteness results in invalid data points

--- Breastfeeding 6to8 weeks.R
+++ Breastfeeding 6to8 weeks.R
@@ -46,17 +46,16 @@ analyze_second(filename = "breastfed", measure = "percent", time_agg = 3,
                ind_id = 21004, year_type = "financial", min_opt = 1423811, profile="HN") 
 
 
-##Exclusion section incomplete- 
-  
-#These exclusions need to be applied somewhere in the program
-#assuming that if NHS boards & council are excluded then all sub geographies should also be excluded - currrent spss program not doing this?
+
+#These exclusions need to be applied to exlcude areas with incomplete data submissions.
+#If NHS boards & council have incomplete data then all sub geographies should also be excluded.
   
 #   *Excluding years/areas where the data is incomplete during 3 year average (year before through to year after).
 #   *Western Isles (S08000028, S12000013) data available from 2006/07
 #   *Highland (S08000022, S12000017) data available from 2008/09. Pre May 2008 data is only for Argyll & Bute LA from 2001/02
 #   *Shetland (S08000026, S12000027) data available from 2008/09
 #   *Grampian (S08000020, S12000020, S12000033, S12000034) & Orkney (S08000025, S12000023) data available  from 2010/11.
-#   
+   
 # Excluding data for boards, hscps, las, localities and izs with incomplete data
 # Merging final data with parent geographies lookup and then filtering
 geo_parents <- readRDS(paste0(lookups, "Geography/IZtoPartnership_parent_lookup.rds")) %>% 
@@ -66,29 +65,29 @@ geo_parents <- readRDS(paste0(lookups, "Geography/IZtoPartnership_parent_lookup.
   gather(geotype, code, c(intzone2011, hscp_locality)) %>% distinct() %>% 
   select(-geotype) %>% rename(parent_area = hscp_partnership)
 
+
 data_shiny <- left_join(readRDS(file = paste0(data_folder, "Shiny Data/breastfed_shiny.rds")),                         
-                         geo_parents, by = "code") %>%
-  filter(!((code %in% c('S08000028','S12000013', "S37000031") | parent_area == "S37000031") &
-             year %in% c(2005, 2006))) %>%
-  # this bit doesn't work as expected and excludes some that it shouldn't (e.g. scotland 2003-2008)
-  # very puzzled about this, it doesn't work with year %in% c(2003,2004,2005,2006,2007, 2008)
-  mutate(test1 = case_when(((code %in% c('S08000022','S12000017', "S37000016") | parent_area == "S37000016") &
-                              year <= 2008)  ~ "Yes",
-         TRUE ~ "No")) %>% #creating the variable with the condition works
-  filter(!((code %in% c('S08000022','S12000017', "S37000016") | parent_area == "S37000016") &
-             year <= 2008)) %>% 
-  # end of chunk that fails
-  filter(!((code %in% c('S08000026','S12000027', "S37000026") | parent_area == "S37000026") &
-             year %in% c(2007, 2008))) %>%
+                        geo_parents, by = "code") %>%
+  mutate(parent_area=coalesce(as.character(parent_area),"x")) #assign value to an 'NA' values to allow filtering to work correctly.
+
+
+data_shiny <- data_shiny %>%
+  filter(!((code %in% c('S08000028','S12000013', 'S37000031') | parent_area == 'S37000031') &
+             year %in% c(2005, 2006))) %>% #filter for Western Isle and associated sub geographies
+  filter(!((code %in% c('S08000022','S12000017', 'S37000016') | parent_area == 'S37000016') &
+             year <= 2008)) %>%  # filter for Highland and associated sub geographies
+  filter(!((code %in% c('S08000026','S12000027', 'S37000026') | parent_area == 'S37000026') &
+            year %in% c(2007, 2008))) %>% #filter for Shetland
   filter(!((code %in% c('S08000020', 'S08000025', 'S12000020', 'S12000023', 'S12000033', 
-                       'S12000034', "S37000019", "S37000022", "S37000001", "S37000002") |
-              parent_area %in% c("S37000019", "S37000022", "S37000001", "S37000002")) & 
-             year %in% c(2009, 2010))) %>% 
+                         'S12000034', 'S37000019', 'S37000022', 'S37000001', 'S37000002') |
+               parent_area %in% c('S37000019', 'S37000022', 'S37000001', 'S37000002')) & 
+              year %in% c(2009, 2010))) %>% #filter for Grampian and Orkney + associated geographies.
   select(-parent_area)
 
+
 # Resave both rds and csv files with exclusions
-saveRDS(data_shiny, file = paste0(data_folder, "Shiny Data/breastfed_shiny.rds"))
-write_csv(data_shiny, path = paste0(data_folder, "Shiny Data/breastfed_shiny.csv"))
+saveRDS(data_shiny, file = paste0(data_folder, "Data to be checked/breastfed_shiny.rds"))
+write_csv(data_shiny, path = paste0(data_folder, "Data to be checked/breastfed_shiny.csv"))
   
 ##END
   
