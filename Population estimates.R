0e22667903fd83b8de11679a7d60be4e4180d91b
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-09-12 14:15:49 +0100

    2018 pops and simplifying code

--- Population estimates.R
+++ Population estimates.R
@@ -39,81 +39,28 @@ format_pops <- function(filename) {
   
   pop <- left_join(numer, denom, by = c("year", "code"))
   
-  saveRDS(pop_data, paste0(data_folder, "Temporary/", filename, "_formatted.rds"))
+  saveRDS(pop, paste0(data_folder, "Temporary/", filename, "_formatted.rds"))
 
 }
 
 ###############################################.
 ## Part 1 - Creating files for each indicator ----
 ###############################################.
-format_pops("pop_under16")
-format_pops("pop_under18")
-format_pops("pop_16to64")
-format_pops("pop_65to74")
-format_pops("pop_65+")
-format_pops("pop_75+")
-format_pops("pop_85+")
-format_pops("pop_16+")
-format_pops("pop_18+")
-format_pops("pop_under26")
-format_pops("pop_16to25")
-format_pops("pop_under1")
-format_pops("pop_1to4")
-format_pops("pop_5to15")
-format_pops("pop_16to39")
-format_pops("pop_40to64")
+# defining list of indicators
+file_list <- c("pop_under16", "pop_under18", "pop_16to64", "pop_65to74", 
+               "pop_65+", "pop_75+", "pop_85+", "pop_16+", "pop_18+", 
+               "pop_under26", "pop_16to25", "pop_under1", "pop_1to4", 
+               "pop_5to15", "pop_16to39", "pop_40to64")
+
+mapply(format_pops, file_list) #formatting all files
 
 ###############################################.
 ## Part 2 - Running analysis function for each indicator ----
 ###############################################.
-# Population 0-15.
-analyze_second(filename = "pop_under16", measure = "percent", year_type = "calendar",
-              time_agg = 1, ind_id = 20002, profile = "PO", min_opt = 1006439)
-# Population 0-17 (under 18).
-analyze_second(filename = "pop_under18", measure = "percent", year_type = "calendar",
-              time_agg = 1, ind_id = 4162, profile = "PO", min_opt = 1006439)
-# Population 0-25.
-analyze_second(filename = "pop_under26", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 13101, profile = "PO", min_opt = 1006439)
-# Population 16-25.
-analyze_second(filename = "pop_16to25", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 13105, profile = "PO", min_opt = 1006439)
-# Population under 1.
-analyze_second(filename = "pop_under1", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 13102, profile = "PO", min_opt = 1006439)
-# Population 1-4.
-analyze_second(filename = "pop_1to4", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 13103, profile = "PO", min_opt = 1006439)
-# Population 5-15.
-analyze_second(filename = "pop_5to15", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 13104, profile = "PO", min_opt = 1006439)
-# Population 18 plus.
-analyze_second(filename = "pop_18+", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 4161, profile = "PO", min_opt = 1006439)
-# Population 16 plus.
-analyze_second(filename = "pop_16+", measure = "percent", year_type = "calendar",
-              time_agg = 1, ind_id = 20004, profile = "PO", min_opt = 1006439)
-# Population 16-39.
-analyze_second(filename = "pop_16to39", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 1502, profile = "PO", min_opt = 1006439)
-# Population 40-64.
-analyze_second(filename = "pop_40to64", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 1503, profile = "PO", min_opt = 1006439)
-# Population 16-64.
-analyze_second(filename = "pop_16to64", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 20003, profile = "PO", min_opt = 1006439)
-# Population 65 to 74.
-analyze_second(filename = "pop_65to74", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 20005, profile = "PO", min_opt = 1006439)
-# Population 65 plus.
-analyze_second(filename = "pop_65+", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 1504, profile = "PO", min_opt = 1006439)
-# Population 75 plus.
-analyze_second(filename = "pop_75+", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 20006, profile = "PO", min_opt = 1006439)
-# Population 85 plus.
-analyze_second(filename = "pop_85+", measure = "percent", year_type = "calendar",
-               time_agg = 1, ind_id = 20007, profile = "PO", min_opt = 1006439)
+mapply(analyze_second, filename = file_list, measure = "percent", 
+       year_type = "calendar", time_agg = 1, 
+       ind_id = c(20002, 4162, 20003, 20005, 1504, 20006, 20007, 20004, 4161, 
+                  13101, 13105, 13102, 13103, 13104, 1502, 1403))
 
 ###############################################.
 ## Part 3 - Preparing data for all ages indicator ----
@@ -129,6 +76,4 @@ allages_pop <- readRDS(paste0(data_folder, "Lookups/Population/DZ11_pop_allages.
 saveRDS(allages_pop, file = paste0(data_folder, "Data to be checked/pop_allages_shiny.rds"))
 write_csv(allages_pop, path = paste0(data_folder, "Data to be checked/pop_allages_shiny.csv"))
 
-
 ##END
-
