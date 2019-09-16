8212f948f4439f0b3ddd24ceb4421f442ee830ef
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-09-16 10:58:37 +0100

    income deprived update

--- Population income deprived.R
+++ Population income deprived.R
@@ -7,7 +7,7 @@
 ## Packages/Filepaths/Functions ----
 ###############################################.
 # Varies filepaths depending on if using server or not.
-if (sessionInfo()$platform == "x86_64-redhat-linux-gnu (64-bit)") {
+if (sessionInfo()$platform %in% c("x86_64-redhat-linux-gnu (64-bit)", "x86_64-pc-linux-gnu (64-bit)")) {
   cl_out_depr <- "/conf/linkage/output/lookups/Unicode/Deprivation/"
 } else {
   cl_out_depr <- "//stats/linkage/output/lookups/Unicode/Deprivation/"
@@ -17,50 +17,37 @@ source("1.indicator_analysis.R") #Normal indicator functions
 source("2.deprivation_analysis.R") # deprivation function
 
 #Small function to standarize each years info. Function parameters:
-#Data is for what basefile to use, Datazone is for what dz type using, 
+#Data is for what basefile to use, list_pos is for the position of the data frame
 #simd for which simd variables-year to look at, year for what year is the data created.
-read_simd_quint <- function(data, simd, datazone, year) {
-  data_simd <- read.spss(paste0(cl_out_depr, data, '.sav'), 
-                         to.data.frame=TRUE, use.value.labels=FALSE) %>% 
+read_simd <- function(data, simd, year, list_pos) {
+  
+  datazone <- tolower(substr(data,1,12))
+  
+  data_simd <- readRDS(paste0(cl_out_depr, data, '.rds')) %>% 
     setNames(tolower(names(.))) %>%   #variables to lower case
-    select_(simd, datazone) %>% 
-    rename_(numerator = simd, datazone = datazone) %>% 
+    select({{simd}}, datazone) %>% 
+    rename(numerator = {{simd}}, datazone = datazone) %>% 
     mutate(year = year)
+  
+  data_inc_dep[[list_pos]] <<- data_simd #assigning to list
 }
 
 ###############################################.
 ## Part 1 - Format raw data ready for analysis functions ----
 ###############################################.
-data_inc_dep <- as.data.frame(rbind(
-  read_simd_quint(year = 2004, data = "DataZone2001_all_simd", 
-                  simd = "simd2006_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2005, data = "DataZone2001_all_simd", 
-                  simd = "simd2006_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2006, data = "DataZone2001_all_simd", 
-                  simd = "simd2006_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2007, data = "DataZone2001_all_simd", 
-                  simd = "simd2009v2_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2008, data = "DataZone2001_all_simd", 
-                  simd = "simd2009v2_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2009, data = "DataZone2001_all_simd", 
-                  simd = "simd2009v2_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2010, data = "DataZone2001_all_simd", 
-                  simd = "simd2012_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2011, data = "DataZone2001_all_simd", 
-                  simd = "simd2012_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2012, data = "DataZone2001_all_simd", 
-                  simd = "simd2012_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2013, data = "DataZone2001_all_simd", 
-                  simd = "simd2012_inc_dep_n", datazone = "datazone2001"),
-  read_simd_quint(year = 2014, data = "DataZone2011_simd2016", 
-                  simd = "simd2016_inc_dep_n", datazone = "datazone2011"),
-  read_simd_quint(year = 2015, data = "DataZone2011_simd2016", 
-                  simd = "simd2016_inc_dep_n", datazone = "datazone2011"),
-  read_simd_quint(year = 2016, data = "DataZone2011_simd2016", 
-                  simd = "simd2016_inc_dep_n", datazone = "datazone2011"),
-  read_simd_quint(year = 2017, data = "DataZone2011_simd2016", 
-                  simd = "simd2016_inc_dep_n", datazone = "datazone2011")
-))
+data_inc_dep <- list() #creating empty list for placing data created by function
+
+# The function creates the dataset and assigns it to the list
+mapply(read_simd, data = "DataZone2001_all_simd", simd = "simd2006_inc_dep_n", 
+       year = 2004:2006, list_pos = 1:3) #simd version 2006
+mapply(read_simd, data = "DataZone2001_all_simd", simd = "simd2009v2_inc_dep_n", 
+       year = 2007:2009, list_pos = 4:6) #simd version 2009
+mapply(read_simd, data = "DataZone2001_all_simd", simd = "simd2012_inc_dep_n", 
+       year = 2010:2013, list_pos = 7:10) #simd version 2012
+mapply(read_simd, data = "DataZone2011_simd2016", simd = "simd2016_inc_dep_n", 
+       year = 2014:2018, list_pos = 11:15) #simd version 2016
+
+data_inc_dep <- do.call("rbind", data_inc_dep) # converting from list into dataframe
 
 #File for deprivation analysis
 saveRDS(data_inc_dep, file = paste0(data_folder, "Prepared Data/income_deprived_depr_raw.rds"))
@@ -73,14 +60,17 @@ saveRDS(data_inc_depdz11, file = paste0(data_folder, "Prepared Data/income_depri
 #Preparing file for CA for period 2004 to 2013
 data_inc_depdz01 <- data_inc_dep %>% filter(year<2014)
 #Lookup file for CA
-ca_lookup <- read.spss('/conf/linkage/output/lookups/geography/other_ref_files/DataZone2001.sav',
+ca_lookup <- read.spss('/conf/linkage/output/lookups/Archive/geography/other_ref_files/DataZone2001.sav',
                        to.data.frame=TRUE, use.value.labels=FALSE) %>% 
-  setNames(tolower(names(.))) %>%   #variables to lower case
-  select(ca2011, datazone2001)
+  setNames(tolower(names(.))) %>% select(ca2011, datazone2001)
 
 #Merging with lookup and aggregating by ca
 data_inc_depdz01 <- left_join(data_inc_depdz01, ca_lookup, by = c("datazone" = "datazone2001")) %>% 
-  rename(ca = ca2011) %>% group_by(ca, year) %>% summarise(numerator=sum(numerator, na.rm = T))
+  rename(ca = ca2011) %>% group_by(ca, year) %>% 
+  summarise(numerator=sum(numerator, na.rm = T)) %>% ungroup() %>% 
+  #Dealing with changes in ca codes. Transforms old code versions into 2019 ones
+  mutate(ca = recode(ca, "S12000015"='S12000047', "S12000024"='S12000048', 
+                     "S12000046"='S12000049', "S12000044"='S12000050'))
 
 saveRDS(data_inc_depdz01, file = paste0(data_folder, "Prepared Data/income_deprived_ca_raw.rds"))
 
@@ -91,7 +81,7 @@ saveRDS(data_inc_depdz01, file = paste0(data_folder, "Prepared Data/income_depri
 analyze_first(filename = "income_deprived_ca", geography = "council", measure = "percent", 
               yearstart = 2004, yearend = 2013, time_agg = 1, pop = "CA_pop_allages")
 analyze_first(filename = "income_deprived_dz11", geography = "datazone11", measure = "percent", 
-              yearstart = 2014, yearend = 2017, time_agg = 1, pop = "DZ11_pop_allages")
+              yearstart = 2014, yearend = 2018, time_agg = 1, pop = "DZ11_pop_allages")
 
 #Merging CA and DZ11 together
 all_data <- rbind(readRDS(paste0(data_folder, "Temporary/income_deprived_dz11_formatted.rds")),
@@ -99,13 +89,13 @@ all_data <- rbind(readRDS(paste0(data_folder, "Temporary/income_deprived_dz11_fo
 saveRDS(all_data, file = paste0(data_folder, "Temporary/income_deprived_all_formatted.rds"))
 
 #Calling second analysis function
-analyze_second(filename = "income_deprived_all", measure = "percent", time_agg = 1,
-               ind_id = 20701, year_type = "calendar", profile = "HN", min_opt = 2999)
+analyze_second(filename = "income_deprived_all", measure = "percent", 
+               time_agg = 1, ind_id = 20701, year_type = "calendar")
 
 ###############################################.
 #Deprivation analysis function
 analyze_deprivation(filename="income_deprived_depr", measure="percent", time_agg=1, 
-                    yearstart= 2004, yearend=2017,  
+                    yearstart= 2004, yearend=2018,  
                     year_type = "calendar", pop = "depr_pop_allages", ind_id = 20701)
 
 #END
