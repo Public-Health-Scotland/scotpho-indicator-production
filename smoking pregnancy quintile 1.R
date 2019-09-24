ab4d15e6677c8be7b8dca7aa87364a05481b7aba
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-09-24 11:06:36 +0100

    smoking pregnancy updates

--- smoking pregnancy quintile 1.R
+++ smoking pregnancy quintile 1.R
@@ -1,68 +1,96 @@
-
-#Incomplete script - still work in progress ###################################
-
 # ScotPHO indicators: smoking during pregnancy quintile 1
 
 ## Part 1 - Format raw data ready for analysis functions 
 ## Part 2 - calling the analysis functions 
-## Part 3 - Editing shiny data file to exclude data for regions where data is known to be incomplete
 
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
 source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
 
-
-smoking_pregnant <- read_excel(paste0(data_folder, "Received Data/IR2019-00231_smoking_pregnancy.xlsx"), 
-                          sheet = "IR2019-00231_smoking_pregnancy", range = cell_limits(c(1, 1), c(NA, 4))) %>%
-  setNames(tolower(names(.))) %>%
-  rename(year = finyear, datazone = datazone2011) %>%
-  filter(!is.na(datazone)) # exclude rows with no datazone.
-
-dep_lookup <- read_rds(paste0(data_folder, "Lookups/Geography/deprivation_geography.rds")) %>%
-  setNames(tolower(names(.))) %>% #variables to lower case
-  select(hb2014, ca2011, datazone, sc_quin) %>%
-  rename(hb = hb2014, ca = ca2011, quintile = sc_quin)
-
-
-###ALL QUINTILES##
-
-smoking_pregnant <- left_join(smoking_pregnant, dep_lookup, "datazone") %>% 
-  setNames(tolower(names(.))) %>% #variables to lower case
-  select(year, hb, ca, datazone, quintile, smoker, known_status) %>%
-  mutate_if(is.character, factor) # converting variables into factors
-
-##SMOKING DURING PREGNANCY QUINTILE 1##
-
+#Function to create files for each quintile
 create_quintile_data <- function(quint_number) {
-  smoking_preg_quint <- left_join(smoking_pregnant, dep_lookup) %>% 
-    setNames(tolower(names(.))) %>% #variables to lower case
+  smoking_preg_quint <-smok_preg_depr %>% 
     subset(quintile == quint_number) %>%
-    select(year, datazone, smoker, known_status) %>%
-    rename(numerator = smoker, denominator = known_status) %>%
-    mutate_if(is.character, factor) # converting variables into factors
+    select(year, datazone, numerator, denominator)
   
-  
-  # olddata <- readRDS(paste0(data_folder, "Prepared Data/smoking_preg_q1_raw.rds"))
   saveRDS(smoking_preg_quint, file=paste0(data_folder, 'Prepared Data/smoking_preg_q', quint_number, '_raw.rds'))
 }
 
+###############################################.
+## Part 1 - Format raw data ready for analysis functions ----
+###############################################.
+# Bringing both dz01 and dz11 data as we use dz01 for period before 2014 and dz11 onwards
+smoking_pregnant_dz11 <- read_excel(paste0(data_folder, "Received Data/IR2019-00231_smoking_pregnancy.xlsx"), 
+                          sheet = "IR2019-00231_smoking_pregnancy") %>%
+  setNames(tolower(names(.))) %>%
+  rename(year = finyear, datazone = datazone2011, numerator = smoker, 
+         denominator = known_status) %>%
+# As mentioned by the maternity team when receiving the data, the year variable refers to the year end e.g. 2004 = 2003/04.
+# Change this to fit the method used for profiles i.e year=start of financial year.
+  mutate(year = year-1) %>% 
+  filter(!is.na(datazone)) # exclude rows with no datazone.
+
+# Saving file used for all women
+saveRDS(smoking_pregnant_dz11, file=paste0(data_folder, 'Prepared Data/smoking_preg_raw.rds'))
+
+# Datazone 2001 data
+smoking_pregnant_dz01 <- read_excel(paste0(data_folder, "Received Data/smoking pregnancy (IR2015-02431) dz01-dont delete.xlsx"), 
+                                    sheet = "smoking pregnancy (IR2015-02431") %>%
+  setNames(tolower(names(.))) %>%
+  rename(year = finyear, datazone = datazone2001, numerator = smk, 
+         denominator = total) %>%
+  # As mentioned by the maternity team when receiving the data, the year variable refers to the year end e.g. 2004 = 2003/04.
+  # Change this to fit the method used for profiles i.e year=start of financial year.
+  mutate(year = year-1) %>% 
+  filter(!is.na(datazone)) # exclude rows with no datazone.
+
+# Joining them together
+smok_preg_depr <- rbind(smoking_pregnant_dz11 %>% filter(year>2013),
+                        smoking_pregnant_dz01 %>% filter(year<2014))
+
+# Reading deprivation lookup
+dep_lookup <- read_rds(paste0(data_folder, "Lookups/Geography/deprivation_geography.rds")) %>%
+  setNames(tolower(names(.))) %>% #variables to lower case
+  select(hb, ca, datazone, sc_quin, year) %>%
+  rename(quintile = sc_quin)
+
+# Joining with deprivation lookup
+smok_preg_depr <- left_join(smok_preg_depr, dep_lookup, by=c("datazone", "year")) %>% 
+  setNames(tolower(names(.))) %>% #variables to lower case
+  select(year, hb, ca, datazone, quintile, numerator, denominator) %>%
+  mutate_if(is.character, factor) # converting variables into factors
+
+# Saving file used for the health inequalities section
+saveRDS(smok_preg_depr %>% select(datazone, year, numerator, denominator), 
+        file=paste0(data_folder, 'Prepared Data/smoking_preg_depr_raw.rds'))
+
+# Creating a file for each quintile
 mapply(create_quintile_data, quint_number = c(1,2,3,4,5))
 
+###############################################.
+## Part 2 - calling the analysis functions
+###############################################.
+# For all quintiles analysis
+analyze_first(filename = "smoking_preg", geography = "datazone11", measure = "percent", 
+              yearstart = 2003, yearend = 2017, time_agg = 3)
 
+analyze_second(filename = "smoking_preg", measure = "percent", time_agg = 3, 
+               ind_id = 13021, year_type = "financial")
+
+#Deprivation analysis function
+analyze_deprivation(filename="smoking_preg_depr", measure="percent", time_agg=3, 
+                    yearstart= 2003, yearend=2017, year_type = "financial", ind_id = 13021)
 
 ###############################################.
-## ANALYSE
-###############################################.
+# For deprivation quintile indicators 
+quint_files <- c("smoking_preg_q1", "smoking_preg_q2", "smoking_preg_q3", 
+                 "smoking_preg_q4", "smoking_preg_q5")
 
-mapply(analyze_first, filename = c("smoking_preg_q1", "smoking_preg_q2", "smoking_preg_q3", 
-                                   "smoking_preg_q4", "smoking_preg_q5"), 
-       geography = "datazone11", measure = "percent", 
-              yearstart = 2002, yearend = 2016, time_agg = 3)
+mapply(analyze_first, filename = quint_files, geography = "datazone11", 
+       measure = "percent", yearstart = 2003, yearend = 2017, time_agg = 3)
 
-mapply(analyze_second, filename = c("smoking_preg_q1", "smoking_preg_q2", "smoking_preg_q3", 
-                                    "smoking_preg_q4", "smoking_preg_q5"),
-       measure = "percent", time_agg = 3, 
-               ind_id = c(1521, 1522, 1523, 1524, 1525), 
-       year_type = "financial", profile = "TP", min_opt = 1013120)
+mapply(analyze_second, filename = quint_files, measure = "percent", time_agg = 3, 
+       year_type = "financial", ind_id = c(1521, 1522, 1523, 1524, 1525))
+
+#END
