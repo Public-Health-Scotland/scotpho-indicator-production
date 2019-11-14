90f4f97aa3f2d17fd94b94d0a774a0feacf0faaa
victoe01 <victoria.elliott@nhs.net>
2019-11-14 16:45:21 +0000

    test file for checking qa_report function

--- testing rmd.R
+++ testing rmd.R
@@ -8,40 +8,29 @@ source("1.indicator_analysis.R") #Normal indicator functions
 #below is a list of various different combinations of report.
 
 
-####ISSUES TO BE FIXED
-#can't text how report works when CI are absent - although it ought to work
-#something weird with one of the flextables in certain circumstances.
-
-
 # option to have different old file name
-qa_function(filename = "alcohol_deaths_male", old_file = "alcohol_deaths_male")
+run_qa(filename = "alcohol_deaths_male", old_file = "alcohol_deaths_male")
 
 # simple check no optional parameters
-qa_function(filename = "copd_hospital_dz11")
-qa_function(filename = "alcohol_deaths_female")
-
-# Function runs fine for alcohol indicator when no additional geos therefore something goes wrong when extra s12?
-qa_function(filename = "alcohol_deaths_dz11")
-
-# include additional optional geographies (stops working)
-qa_function(filename = "alcohol_deaths_dz11",check_extras = c("S02001255","S12000014"))
-
-# include additional optional geographies (seems ok when S02 geo added)
-qa_function(filename = "alcohol_deaths_dz11",check_extras = c("S02001255"))
-
-# include additional optional geographies (S12 geo introduces issue - can't figure out why)
-qa_function(filename = "alcohol_deaths_dz11",check_extras = c("S12000014"))
+run_qa(filename = "copd_hospital_dz11")
+run_qa(filename = "alcohol_deaths_female")
+run_qa(filename = "alcohol_deaths_dz11")
 
 #no numerator (works fine)
-qa_function(filename = "life_expectancy_male")
+run_qa(filename = "life_expectancy_male")
 
 
-#no confidence intervals 
+# include additional optional geographies 
+run_qa(filename = "alcohol_deaths_dz11",check_extras = c("S12000014")) #works
+run_qa(filename = "alcohol_deaths_dz11",check_extras = c("S12000020","S12000014")) #stops working with 2 geos
+run_qa(filename = "alcohol_deaths_dz11",check_extras = c("S02001255","S12000014")) #not working
+
+
+
+##no confidence intervals 
 # abi or active travel - files in shiny data folder don't seem to have usual naming conventions
 # abi don't have council data which causes report checking to fail at the moment.
-qa_function(filename = "ABIs_delivered_final")
+run_qa(filename = "ABIs_delivered_final")
 
 
-#Read in indicator file being checked
-data_indicator <- readRDS(paste0(data_folder, "Data to be checked/", filename, "_shiny.rds")) 
 
