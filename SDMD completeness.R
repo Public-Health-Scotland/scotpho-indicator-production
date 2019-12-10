3ff1eebe164b9a95f1db66ea17776dcd3245488f
Laura Downie <laurad05@spsssrv02.csa.scot.nhs.uk>
2019-12-10 12:57:31 +0000

    indicator update - sdmd completeness/followup

--- SDMD completeness.R
+++ SDMD completeness.R
@@ -10,32 +10,54 @@
 ## Packages/Filepaths/Functions ----
 ###############################################.
 source("1.indicator_analysis.R") #Normal indicator functions
-
+library(janitor)
 ###############################################.
 ## Part 1 - Prepare basefile ----
 ###############################################.
 #Initial completeness file from drugs team
-sdmd_initial <- read.spss(paste0(data_folder, "Received Data/IR2018-01102_SDMD_compliance_with_DATWT.sav"),
-                                to.data.frame=TRUE, use.value.labels=FALSE) %>% 
-  setNames(tolower(names(.))) %>%   #variables to lower case
-  rename(denominator = sdmdpeople, numerator = datwtpeople) %>% select(-hbt)
+sdmd_initial <- read_csv(paste0(data_folder, "Received Data/IR2019-00925_SDMD_initial_compliance.csv")) %>%
+  clean_names() %>% 
+  rename(denominator = sdmdpeople, numerator = datwtpeople, hb2019name = location_code) %>%
+  mutate_at(c("denominator", "numerator"), as.numeric)
+
+#sdmd_initial[, 4:6][sdmd_initial[, 4:6] == "X"] <- NA
+#sdmd_initial[, 4:6][sdmd_initial[, 4:6] == "*"] <- NA
+
+#lookup for geogrpahy codes
+geography_codes <- readRDS('/PHI_conf/ScotPHO/Profiles/Data/Lookups/Geography/codedictionary.rds') %>% 
+  setNames(tolower(names(.))) %>%
+  rename(hb2019name = areaname)
+  
+sdmd_initial <- left_join(sdmd_initial, geography_codes, "hb2019name") %>%
+  select(code, hb2019name, year, numerator, denominator) %>%
+  mutate(year = as.numeric(substr(year,1,4))) %>%
+  select(year, code, numerator, denominator)
 
 saveRDS(sdmd_initial, file=paste0(data_folder, 'Temporary/sdmd_initialcompl_formatted.rds'))
 
 #Follow-up completeness file from drugs team
-sdmd_follow <- read.spss(paste0(data_folder, "Received Data/IR2018-01102_SDMD_follow_up_completeness.sav"),
-                              to.data.frame=TRUE, use.value.labels=FALSE) %>%
-  setNames(tolower(names(.))) %>%   #variables to lower case
-  rename(denominator = sdmdpeople, numerator = followuppeople) %>% select(-name)
+sdmd_follow <- read_csv(paste0(data_folder, "Received Data/IR2019-00925_SDMD_followup_completeness.csv")) %>%
+  clean_names() %>% 
+  rename(denominator = sdmdpeople, numerator = followuppeople, hb2019name = location) %>%
+  mutate_at(c("denominator", "numerator"), as.numeric)
 
-saveRDS(sdmd_follow, file=paste0(data_folder, 'Temporary/sdmd_followcompl_formatted.rds'))
+#sdmd_follow[, 4:6][sdmd_follow[, 4:6] == "X"] <- NA
+#sdmd_follow[, 4:6][sdmd_follow[, 4:6] == "*"] <- NA
+
+#adding geography codes
+sdmd_follow <- left_join(sdmd_follow, geography_codes, "hb2019name") %>%
+  select(code, hb2019name, year, numerator, denominator) %>%
+  mutate(year = as.numeric(substr(year,1,4))) %>%
+  select(year, code, numerator, denominator)
+  
+saveRDS(sdmd_follow, file=paste0(data_folder, 'Temporary/sdmd_follow_formatted.rds'))
 
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
 #Initial completeness
 analyze_second(filename = "sdmd_initialcompl", measure = "percent", time_agg = 1, 
-               ind_id = 4137, year_type = "financial", profile = "DU", min_opt = 160720)
+               ind_id = 4137, year_type = "financial")
 
 # Merging with old adp data that it has not been produced in latest years because 
 # of data quality issues
@@ -47,7 +69,6 @@ write_csv(sdmd_shiny_initial, paste0(data_folder, "Shiny Data/sdmd_initialcompl_
 
 ###############################################.
 # Follow-up completeness
-analyze_second(filename = "sdmd_followcompl", measure = "percent", time_agg = 1, 
-               ind_id = 4138, year_type = "financial", profile = "DU", min_opt = 160450)
-
+analyze_second(filename = "sdmd_follow", measure = "percent", time_agg = 1, 
+               ind_id = 4138, year_type = "financial")
 ##END
