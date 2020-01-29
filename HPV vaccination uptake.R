f226da4f7ef072b3ce46fab2e9ab8984fb1c147a
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-01-29 11:10:37 +0000

    updating hpv

--- HPV vaccination uptake.R
+++ HPV vaccination uptake.R
@@ -26,7 +26,15 @@ hpv_data_17 <- read.spss(paste0(data_folder, "Received Data/2018_HPVuptakeAndRat
   mutate(year = 2017) %>% #so it follows standard in functions
   select(-rates, -schoolyear_ending) 
 
-hpv_data <- rbind(hpv_data_17, hpv_data_old) #merging both together
+# Data for 2018/19
+hpv_data_18 <- read.spss(paste0(data_folder, "Received Data/2019_HPVuptakeAndRates.zsav"), 
+                         to.data.frame=TRUE, use.value.labels=FALSE) %>% 
+  setNames(tolower(names(.))) %>% rename(datazone = geography) %>% 
+  filter(substr(datazone,1,3) == "S01") %>% 
+  mutate(year = 2018) %>% #so it follows standard in functions
+  select(-rates, -schoolyear_ending) 
+
+hpv_data <- rbind(hpv_data_18, hpv_data_17, hpv_data_old) #merging both together
 
 saveRDS(hpv_data, file=paste0(data_folder, 'Prepared Data/hpv_uptake_raw.rds'))
 
@@ -34,14 +42,14 @@ saveRDS(hpv_data, file=paste0(data_folder, 'Prepared Data/hpv_uptake_raw.rds'))
 ## Part 2 - Run analysis functions ----
 ###############################################.
 analyze_first(filename = "hpv_uptake", geography = "datazone11", measure = "percent", 
-              yearstart = 2009, yearend = 2017, time_agg = 3)
+              yearstart = 2009, yearend = 2018, time_agg = 3)
 
 analyze_second(filename = "hpv_uptake", measure = "percent", time_agg = 3, 
                ind_id = 13032, year_type = "school")
 
 #Deprivation analysis function
 analyze_deprivation(filename="hpv_uptake", measure="percent", time_agg=3, 
-                    yearstart= 2014, yearend=2017,   year_type = "school", 
+                    yearstart= 2014, yearend=2018,   year_type = "school", 
                     ind_id = 13032)
 
 ##END
