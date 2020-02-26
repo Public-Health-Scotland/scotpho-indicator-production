ae7eedf6bd94dda8c9ead13784002a8af4e85b6f
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-02-26 10:54:39 +0000

    tidying, fix to adp data

--- hepatitis C in pwid.R
+++ hepatitis C in pwid.R
@@ -8,7 +8,6 @@
 ## Packages/Filepaths/Functions ----
 ###############################################.
 source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Read in data ----
@@ -24,12 +23,13 @@ hepc_pwid <- read_csv(paste0(data_folder, "Received Data/Hep C Among PWID_2008-0
   mutate_at(c("denominator", "numerator"), as.numeric)
 
 geography_codes <- readRDS('/PHI_conf/ScotPHO/Profiles/Data/Lookups/Geography/codedictionary.rds') %>% 
-  setNames(tolower(names(.)))
+  filter(substr(code,1,3) %in% c("S08", "S12", "S00")) #names only needed for HB, CA, Scot
 
 hepc_pwid <- left_join(hepc_pwid, geography_codes, "areaname") %>%
   mutate(year = as.numeric(substr(year,1,4))) %>% #format as financial year
-  distinct(year, areaname, .keep_all =TRUE) 
+  select(year, code, numerator, denominator) 
 
+# Aggregating to obtain ADP level
 hepc_adp <- hepc_pwid %>% filter(substr(code,1,3) == "S12") %>% 
   mutate(code = case_when(
     code == "S12000005" ~ "S11000005", code == "S12000006" ~ "S11000006", code == "S12000008" ~ "S11000008", 
@@ -42,22 +42,17 @@ hepc_adp <- hepc_pwid %>% filter(substr(code,1,3) == "S12") %>%
     code == "S12000040" ~ "S11000031", code == "S12000041" ~ "S11000003", code == "S12000042" ~ "S11000007", 
     code == "S12000050" ~ "S11000052", code == "S12000045" ~ "S11000009", code == "S12000049" ~ "S11000015", 
     code == "S12000047" ~ "S11000014", code == "S12000048" ~ "S11000023", code == "S12000013" ~ "S11000032", 
-    code == "S12000027" ~ "S11000026", code == "S12000023" ~ "S11000022", TRUE ~ "Error")) #%>% 
-  #group_by(year, code) %>% summarise_all(sum, na.rm=T) %>% ungroup()
+    code == "S12000027" ~ "S11000026", code == "S12000023" ~ "S11000022", TRUE ~ "Error")) %>% 
+  group_by(year, code) %>% summarise_all(sum, na.rm=T) %>% ungroup()
   
-hepc_pwid <- hepc_pwid %>%
-  select(year, code, numerator, denominator)
+hepc_pwid <- rbind(hepc_adp, hepc_pwid) #joining both datasets together
 
 saveRDS(hepc_pwid, file=paste0(data_folder, 'Temporary/hepc_pwid_formatted.rds'))
-
-saveRDS(hepc_adp, file=paste0(data_folder, 'Temporary/hepc_adp_formatted.rds'))
 
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
-
 analyze_second(filename = "hepc_pwid", measure = "percent", time_agg = 1, 
                ind_id = 4122, year_type = "financial")
 
-analyze_second(filename = "hepc_adp", measure = "percent", time_agg = 1, 
-               ind_id = 4122, year_type = "financial")
+##END
