e67f4921843f845c231514e686f4e787ad03ca80
Gordon Hunt <gordoh01@nssrstudio04.csa.scot.nhs.uk>
2020-07-07 16:27:30 +0100

    Issue with gzfile

--- Child immunisation uptake - 5in1 & MMR.R
+++ Child immunisation uptake - 5in1 & MMR.R
@@ -8,32 +8,42 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
+#install.packages('RcppRoll')
+#install.packages('flextable')
+#install.packages('plotly')
+#install.packages('binom')
+#library(haven)
+
 source("1.indicator_analysis.R") #Normal indicator functions
 source("2.deprivation_analysis.R") # deprivation function
+
+# data_folder <- "/PHI_conf/ScotPHO/Profiles/Data/"
+# lookups <- "/PHI_conf/ScotPHO/Profiles/Data/Lookups/" 
 
 ###############################################.
 ## Part 1 - Prepare basefile: 5 in 1 ----
 ###############################################.
+
 # Datazone2001 - Read data (Immunisation uptake at 24 month) provided by child health team and aggregate 
-five01_data <- read.spss(paste0(data_folder, "Received Data/2003_2018_Scotpho_ChildhoodImms_DZ_DZ2001.zsav"), 
-                         to.data.frame=TRUE, use.value.labels=FALSE) %>% 
+five01_data <- read.spss(paste0(data_folder, "Received Data/2003_2019_Scotpho_ChildhoodImms_DZ_DZ2001.zsav"), 
+                        to.data.frame=TRUE, use.value.labels=FALSE) %>%  
   setNames(tolower(names(.))) %>% rename(datazone = datazone2001) %>% 
   mutate(datazone = substr(datazone, 1, 9)) %>%   #trimming datazone to 9 characters
   # aggregate to get the count, removing sex
   group_by(year, datazone) %>% 
-  summarise_at(c("total24", "five24"), funs(sum), na.rm =T) %>% ungroup()
+  summarise_at(c("total24", "five24"), funs(sum), na.rm =T) %>% 
+  ungroup()
 
 # Rename variables into numerator and denominator
 five01_data <- five01_data %>% rename(denominator = total24, numerator = five24)
 
 # Datazone2011 - Read data (Immunisation uptake at 24 month) provided by child health team and aggregate
-
-five11_data <- read.spss( paste0(data_folder, "Received Data/2004_2018_Scotpho_ChildhoodImms_DZ_DZ2011.zsav"), 
-                        to.data.frame=TRUE, use.value.labels=FALSE) %>% 
-  setNames(tolower(names(.))) %>% rename(datazone = datazone2011) %>% 
+five11_data <- read.spss( paste0(data_folder, "Received Data/2004_2019_Scotpho_ChildhoodImms_DZ_DZ2011.zsav"), 
+                          to.data.frame=TRUE, use.value.labels=FALSE) %>% 
+  setNames(tolower(names(.))) %>% rename(datazone = datazone2011) %>%
   mutate(datazone = substr(datazone, 1, 9)) %>%   #trimming datazone to 9 characters
   # aggregate to get the count, removing sex
-  group_by(year, datazone) %>% 
+  group_by(year, datazone) %>%
   summarise_at(c("total24", "five24"), funs(sum), na.rm =T) %>% ungroup()
 
 # Rename variables into numerator and denominator
@@ -52,8 +62,8 @@ saveRDS(five_dep_file, file=paste0(data_folder, 'Prepared Data/Immunisation_5in1
 ###############################################.
 
 # Datazone2001 - Read data (Immunisation uptake at 24 month) provided by child health team and aggregate 
-mmr01_data <- read.spss( paste0(data_folder, "Received Data/2003_2018_Scotpho_ChildhoodImms_DZ_DZ2001.zsav"), 
-                          to.data.frame=TRUE, use.value.labels=FALSE) %>% 
+mmr01_data <- read.spss( paste0(data_folder, "Received Data/2003_2019_Scotpho_ChildhoodImms_DZ_DZ2001.zsav"), 
+                         to.data.frame=TRUE, use.value.labels=FALSE) %>% 
   setNames(tolower(names(.))) %>% rename(datazone = datazone2001) %>% 
   mutate(datazone = substr(datazone, 1, 9)) %>%   #trimming datazone to 9 characters
   # aggregate to get the count, removing sex
@@ -63,9 +73,8 @@ mmr01_data <- read.spss( paste0(data_folder, "Received Data/2003_2018_Scotpho_Ch
   rename(denominator = total24, numerator = mmr24)
 
 # Datazone2011 - Read data (Immunisation uptake at 24 month) provided by child health team and aggregate
-
-mmr11_data <- read.spss( paste0(data_folder, "Received Data/2004_2018_Scotpho_ChildhoodImms_DZ_DZ2011.zsav"), 
-                          to.data.frame=TRUE, use.value.labels=FALSE) %>% 
+mmr11_data <- read.spss( paste0(data_folder, "Received Data/2004_2019_Scotpho_ChildhoodImms_DZ_DZ2011.zsav"), 
+                         to.data.frame=TRUE, use.value.labels=FALSE) %>% 
   setNames(tolower(names(.))) %>% rename(datazone = datazone2011) %>% 
   mutate(datazone = substr(datazone, 1, 9)) %>%   #trimming datazone to 9 characters
   # aggregate to get the count, removing sex
@@ -87,14 +96,14 @@ saveRDS(mmr_dep_file, file=paste0(data_folder, 'Prepared Data/Immunisation_MMR_d
 ###############################################.
 
 analyze_first(filename = "Immunisation_5in1_dz11", geography = "datazone11", measure = "percent", 
-              yearstart = 2004, yearend = 2018, time_agg = 3)
+              yearstart = 2004, yearend = 2019, time_agg = 3)
 
 analyze_second(filename = "Immunisation_5in1_dz11", measure = "percent", time_agg = 3, 
-               ind_id = 21103, year_type = "calendar", profile = "HN", min_opt = 1090421)
+               ind_id = 21103, year_type = "calendar")
 
 #Deprivation analysis function
 analyze_deprivation(filename="Immunisation_5in1_depr", measure="percent", time_agg=3, 
-                    yearstart= 2003, yearend=2018,   year_type = "calendar", 
+                    yearstart= 2003, yearend=2019,   year_type = "calendar", 
                     ind_id = 21103)
 
 ###############################################.
@@ -102,14 +111,14 @@ analyze_deprivation(filename="Immunisation_5in1_depr", measure="percent", time_a
 ###############################################.
 
 analyze_first(filename = "Immunisation_MMR_dz11", geography = "datazone11", measure = "percent", 
-              yearstart = 2004, yearend = 2018, time_agg = 3)
+              yearstart = 2004, yearend = 2019, time_agg = 3)
 
 analyze_second(filename = "Immunisation_MMR_dz11", measure = "percent", time_agg = 3, 
-               ind_id = 21104, year_type = "calendar", profile = "HN", min_opt = 1106333)
+               ind_id = 21104, year_type = "calendar")
 
 #Deprivation analysis function
 analyze_deprivation(filename="Immunisation_MMR_depr", measure="percent", time_agg=3, 
-                    yearstart= 2003, yearend=2018,   year_type = "calendar", 
+                    yearstart= 2003, yearend=2019,   year_type = "calendar", 
                     ind_id = 21104)
 
 ##END
