17b7d1087fe533eee3e0b29a2b21151c9a963c4e
Michael Webster <michaw01@nssrstudio04.csa.scot.nhs.uk>
2020-12-15 15:44:43 +0000

    updated for 2020 data

--- Quit attempts - total.R
+++ Quit attempts - total.R
@@ -13,7 +13,7 @@ source("1.indicator_analysis.R") #Normal indicator functions
 ## Part 1 - Create basefile ----
 ###############################################.
 #Reading data extracted from table from Smoking cessation annual publication
-quit_total <- read_csv(paste0(data_folder, "Received Data/quit_attempts_total_2019.csv")) %>% 
+quit_total <- read_csv(paste0(data_folder, "Received Data/quit_attempts_total_2020.csv")) %>% 
   setNames(tolower(names(.))) %>%    #variables to lower case
   gather("year", "numerator", -la_name) %>% #from wide to long format
   mutate(year = substr(year,1,4)) #fin year
@@ -35,7 +35,7 @@ saveRDS(quit_total, file=paste0(data_folder, 'Prepared Data/quitattempts_total_r
 ###############################################.
 # to generate geographical aggregations
 analyze_first(filename = "quitattempts_total", geography = "council", 
-              measure = "percent", yearstart = 2009, yearend = 2018, time_agg = 1)
+              measure = "percent", yearstart = 2009, yearend = 2019, time_agg = 1)
 
 ###############################################.
 ## Part 3 - Format the file for Shiny tool ----
