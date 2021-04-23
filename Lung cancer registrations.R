19a3ebb8f2d5ff6ee0e0124e9a09fac024c3bac2
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-04-23 12:35:55 +0100

    cancer registrations updates

--- Lung cancer registrations.R
+++ Lung cancer registrations.R
@@ -20,11 +20,11 @@ channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
 # and excluding records with unknown sex .
 # It counts tumours, not different patients, e.g. a patient can have several tumours over the years.
 # If we were to use SMRA geographical information, the code could be simplified.
-lung_reg <- tbl_df(dbGetQuery(channel, statement=
+lung_reg <- as_tibble(dbGetQuery(channel, statement=
   "SELECT count(*) count, extract (year from INCIDENCE_DATE) year, SEX sex_grp, 
       POSTCODE pc7, age_in_years age 
    FROM ANALYSIS.SMR06_PI
-   WHERE incidence_date between '1 January 2002' and '31 December 2018'
+   WHERE incidence_date between '1 January 2002' and '31 December 2019'
       AND age_in_years>=16 
       AND regexp_like(ICD10S_CANCER_SITE, 'C3[34]')
       AND sex <> 9
@@ -33,7 +33,7 @@ lung_reg <- tbl_df(dbGetQuery(channel, statement=
   create_agegroups() # Creating age groups for standardization.
 
 # Bringing  LA info.
-postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2020_1.rds') %>% 
+postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2020_2.rds') %>% 
   setNames(tolower(names(.))) %>% select(pc7, ca2011) #variables to lower case
 
 lung_reg <- left_join(lung_reg, postcode_lookup, by = "pc7") %>% #merging with lookup
@@ -48,7 +48,7 @@ saveRDS(lung_reg, file=paste0(data_folder, 'Prepared Data/lungcancer_reg_raw.rds
 ## Part 2 - Run analysis functions ----
 ###############################################.
 analyze_first(filename = "lungcancer_reg", geography = "council", measure = "stdrate", 
-              pop = "CA_pop_16+", yearstart = 2002, yearend = 2018, hscp = T,
+              pop = "CA_pop_16+", yearstart = 2002, yearend = 2019, hscp = T,
               time_agg = 3, epop_age = "16+")
 
 analyze_second(filename = "lungcancer_reg", measure = "stdrate", time_agg = 3, 
