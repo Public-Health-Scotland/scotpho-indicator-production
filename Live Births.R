e99ef5a1827ce462e132bfc035b57eef452ea935
ScottMG-PHS <scott.mclaren-gow@phs.scot>
2021-06-14 10:29:57 +0100

    Live Births Update
    Part 1 changed to accommodate different format for provided data

--- Live Births.R
+++ Live Births.R
@@ -1,7 +1,7 @@
 # ScotPHO indicators: Live births
 
-# Part 1 - Format raw data ready for analysis functions 
-# Part 2 - calling the analysis functions 
+# Part 1 - Format raw data ready for analysis functions
+# Part 2 - calling the analysis functions
 
 ###############################################.
 ## Packages/Filepaths/Functions ----
@@ -12,25 +12,21 @@ source("2.deprivation_analysis.R") # deprivation function
 ###############################################.
 ## Part 1 - Format raw data ready for analysis functions ----
 ###############################################.
-live_births <- read.csv(paste0(data_folder, "Received Data/births by 2011 data zones and sex - 2002 -2018.csv")) %>%
+live_births <- read_excel(paste0(data_folder,
+                                 "Received Data/Births 2002-2019 datazone_2011.xlsx")) %>%
   setNames(tolower(names(.))) %>%
-  mutate(male = case_when(male == "-" ~ "0", TRUE ~ paste0(male)),
-         female = case_when(female == "-" ~ "0", TRUE ~ paste0(female)),
-         male = as.numeric(as.character(male)),
-         female = as.numeric(as.character(female))) %>%
-  filter(!is.na(datazone)) %>% # exclude rows with no datazone.
+  rename(year = "registration year", datazone = datazone_2011) %>%
   group_by(year, datazone) %>%
-  mutate(total = sum(male, female)) %>%
-  summarise(numerator = sum(total, na.rm =T)) %>% ungroup()
-         
+  summarise(numerator = sum(count, na.rm = TRUE), .groups = "drop")
+
 saveRDS(live_births, file=paste0(data_folder, 'Prepared Data/live_births_raw.rds'))
 
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
 
-analyze_first(filename = "live_births", geography = "datazone11", measure = "crude", 
-              yearstart = 2002, yearend = 2018, time_agg = 1, pop ='DZ11_pop_allages')
+analyze_first(filename = "live_births", geography = "datazone11", measure = "crude",
+              yearstart = 2002, yearend = 2019, time_agg = 1, pop ='DZ11_pop_allages')
 
-analyze_second(filename = "live_births", measure = "crude", time_agg = 1, crude_rate=1000, 
+analyze_second(filename = "live_births", measure = "crude", time_agg = 1, crude_rate=1000,
                ind_id = 20008, year_type = "calendar")
