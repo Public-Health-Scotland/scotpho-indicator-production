d25c70df546da53e16a8a5ab1185e78b920fb406
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-07-09 14:56:53 +0100

    typo

--- Young people in deprived quintile.R
+++ Young people in deprived quintile.R
@@ -121,7 +121,7 @@ filenames <- c("young_people_crime", "young_people_access", "young_people_income
 mapply(analyze_first, filename = filenames, geography = "datazone11", measure = "percent", 
               yearstart = 2011, yearend = 2019, time_agg = 1, pop = "DZ11_pop_under26")
 
-mapply(analyze_second(filename = "young_people_crime", measure = "percent", time_agg = 1, 
+mapply(analyze_second(filename = filenames, measure = "percent", time_agg = 1, 
                ind_id = c(13005, 13003, 13004), year_type = "calendar", qa = F))
 
 ## END
