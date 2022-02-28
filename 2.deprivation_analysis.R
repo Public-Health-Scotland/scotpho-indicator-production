8579a448a46084e4a0b66c561e5d526b31df866f
victoe01 <victoria.elliott@nhs.net>
2022-02-28 15:51:48 +0000

    adjusting deprivation analysis script to accommodate le indicator

--- 2.deprivation_analysis.R
+++ 2.deprivation_analysis.R
@@ -333,109 +333,16 @@ data_depr_totals <- data_depr_totals %>% summarise_all(sum, na.rm = T) %>%
       select(-pcf, -est_pop, -ci_interval)
   }
   
-  ##################################################.
-  ##  Part 6 - Create SII and RII ----
-  ##################################################.
-  #Splitting into two files: one with quintiles for SII and one without to keep the total values
-  data_depr_sii <- data_depr %>% group_by(code, year, quint_type) %>% 
-    mutate(overall_rate = rate[quintile == "Total"]) %>% 
-    filter(quintile != "Total") %>% 
-    #This variables are used for SII, RII and PAR calculation
-    mutate(total_pop = sum(denominator), # calculate the total population for each area (without SIMD).
-           proportion_pop = denominator/total_pop) %>% # proportion of the population in each SIMD out of the total population. )
-    ungroup()
   
-  data_depr_totals <- data_depr %>% filter(quintile == "Total")
+  # ##################################################.
+  # ##  Part 6 - Create SII and RII ----
+  # ##################################################.
   
-  ###############################################.
-  # Calculate the regression coefficient
-  #Formula from: https://www.scotpho.org.uk/comparative-health/health-inequalities-tools/archive/triple-i-and-hits/
-  #https://pdfs.semanticscholar.org/14e0/c5ba25a4fdc87953771a91ec2f7214b2f00d.pdf
-  #The dataframe sii_model will have a column for sii, lower ci and upper ci for each
-  # geography, year and quintile type
-  sii_model <- data_depr_sii %>% group_by(code, year, quint_type) %>% 
-    #Checking that all quintiles are present, if not excluding as we are not showing
-    #RII and SII for those. Calculations would need to be adjusted and thought well if we wanted to include them
-    mutate(count= n()) %>% filter(count == 5) %>% 
-    #This first part is to adjust rate and denominator with the population weights
-    mutate(cumulative_pro = cumsum(proportion_pop),  # cumulative proportion population for each area
-           relative_rank = case_when(
-              quintile == "1" ~ 0.5*proportion_pop,
-              quintile != "1" ~ lag(cumulative_pro) + 0.5*proportion_pop),
-           sqr_proportion_pop = sqrt(proportion_pop), #square root of the proportion of the population in each SIMD
-           relrank_sqr_proppop = relative_rank * sqr_proportion_pop,
-           rate_sqr_proppop = sqr_proportion_pop * rate) %>% #rate based on population weights
-    nest() %>% #creating one column called data with all the variables not in the grouping
-    # Calculating linear regression for all the groups, then formatting the results
-    # and calculating the confidence intervals
-    mutate(model = map(data, ~ lm(rate_sqr_proppop ~ sqr_proportion_pop + relrank_sqr_proppop + 0, data = .)),
-           #extracting sii from model, a bit fiddly but it works
-           sii = -1 * as.numeric(map(map(model, "coefficients"), "relrank_sqr_proppop")),
-           cis = map(model, confint_tidy)) %>% #calculating confidence intervals
-    ungroup() %>% unnest(cis) %>% #Unnesting the CIs 
-  #selecting only even row numbers which are the ones that have the sii cis
-    filter(row_number() %% 2 == 0) %>% 
-    mutate(lowci_sii = -1 * conf.high, #fixing interpretation
-           upci_sii = -1 * conf.low) %>% 
-    select(-conf.low, -conf.high, -model, -data) %>% #taking out results as not needed anymore
-  mutate_at(c("sii", "lowci_sii", "upci_sii"), ~replace(., is.na(.), NA_real_)) #recoding NAs
-  
-  #Merging sii with main data set
-  data_depr <- left_join(data_depr_sii, sii_model, by = c("code", "year", "quint_type"))
-  
-  #Calculating RII
-  data_depr <- data_depr %>% mutate(rii = sii / overall_rate,
-           lowci_rii = lowci_sii / overall_rate,
-           upci_rii = upci_sii / overall_rate,
-  #Transforming RII into %. This way is interpreted as "most deprived areas are
-  # xx% above the average" For example: Cancer mortality rate is around 55% higher 
-  # in deprived areas relative to the mean rate in the population
-           rii_int = rii * 0.5 *100,
-           lowci_rii_int = lowci_rii * 0.5 *100,
-           upci_rii_int = upci_rii * 0.5 *100)
+  #call function to generate measures of inequality (function appears at bottom of this script)
+  inequality_measures()
 
   ##################################################.
-  ##  Part 7 - Population attributable risk and range  ----
-  ##################################################.
-  #Calculation PAR
-  #Formula here: https://pdfs.semanticscholar.org/14e0/c5ba25a4fdc87953771a91ec2f7214b2f00d.pdf
-  # https://fhop.ucsf.edu/sites/fhop.ucsf.edu/files/wysiwyg/pg_apxIIIB.pdf
-  #Adding columns for Most and least deprived rates
-  most_depr <- data_depr %>% filter(quintile == "1") %>% 
-    select(code, year, quint_type, rate) %>% rename(most_rate = rate)
-  
-  least_depr <- data_depr %>% filter(quintile == "5") %>% 
-    select(code, year, quint_type, rate) %>% rename(least_rate = rate)
-
-  data_depr <- left_join(data_depr, most_depr, by = c("code", "year", "quint_type"))
-  data_depr <- left_join(data_depr, least_depr, by = c("code", "year", "quint_type"))
-  
-  data_depr <- data_depr %>%  group_by(code, year, quint_type) %>%
-    mutate(#calculating PAR. PAR of incomplete groups to NA
-      #CI calculation missing, this can help https://onlinelibrary.wiley.com/doi/pdf/10.1002/sim.2779
-      #https://fhop.ucsf.edu/sites/fhop.ucsf.edu/files/wysiwyg/pg_apxIIIB.pdf
-      par_rr = (rate/least_rate - 1) * proportion_pop,
-      count= n(),
-      par = case_when(count != 5 ~ NA_real_,
-                      count == 5 ~ sum(par_rr)/(sum(par_rr) + 1) * 100),
-      # Calculate ranges 
-      abs_range = most_rate - least_rate,
-      rel_range = most_rate / least_rate) %>% ungroup()
-
-  #Joining with totals.
-  #dataframe with the unique values for the different inequality measures
-  data_depr_match <- data_depr %>% 
-    select(code, year, quint_type, sii, upci_sii, lowci_sii, rii, lowci_rii, upci_rii,
-           rii_int, lowci_rii_int, upci_rii_int, par, abs_range, rel_range) %>% 
-    unique()
-  
-  data_depr_totals <- left_join(data_depr_totals, data_depr_match, 
-                                by = c("code", "year", "quint_type"))
-  
-  data_depr <- bind_rows(data_depr, data_depr_totals) 
-  
-  ##################################################.
-  ##  Part 8 - Adding time labels and indicator info ----
+  ##  Part 7 - Adding time labels and indicator info ----
   ##################################################.
   #Indicator code
   data_depr <- data_depr %>% mutate(ind_id = ind_id) %>% 
@@ -507,7 +414,7 @@ data_depr_totals <- data_depr_totals %>% summarise_all(sum, na.rm = T) %>%
   final_result <<- data_shiny
   
   ##################################################.
-  ##  Part 9 - Checking results ----
+  ##  Part 8 - Checking results ----
   ##################################################.
   #Selecting Health boards and Scotland for latest year in dataset
   ggplot(data=(data_shiny %>% subset((substr(code, 1, 3)=="S08" | code=="S00000001") 
@@ -517,5 +424,119 @@ data_depr_totals <- data_depr_totals %>% summarise_all(sum, na.rm = T) %>%
     geom_errorbar(aes(ymax=upci, ymin=lowci), width=0.5)
 
 }
+
+
+##################################################.
+## Inequality measures function ---- 
+## generating SII, RII, PAF and Ranges.
+##################################################.
+
+inequality_measures <- function(){
+
+  ##################################################.
+  ##  SII/RII  ----
+  ##################################################.
+  
+  #Splitting into two files: one with quintiles for SII and one without to keep the total values
+  data_depr_sii <- data_depr %>% group_by(code, year, quint_type) %>% 
+    mutate(overall_rate = rate[quintile == "Total"]) %>% 
+    filter(quintile != "Total") %>% 
+    #This variables are used for SII, RII and PAR calculation
+    mutate(total_pop = sum(denominator), # calculate the total population for each area (without SIMD).
+           proportion_pop = denominator/total_pop) %>% # proportion of the population in each SIMD out of the total population. )
+    ungroup()
+  
+  data_depr_totals <- data_depr %>% filter(quintile == "Total")
+  
+  ###############################################.
+  # Calculate the regression coefficient
+  #Formula from: https://www.scotpho.org.uk/comparative-health/health-inequalities-tools/archive/triple-i-and-hits/
+  #https://pdfs.semanticscholar.org/14e0/c5ba25a4fdc87953771a91ec2f7214b2f00d.pdf
+  #The dataframe sii_model will have a column for sii, lower ci and upper ci for each
+  # geography, year and quintile type
+  sii_model <- data_depr_sii %>% group_by(code, year, quint_type) %>% 
+    #Checking that all quintiles are present, if not excluding as we are not showing
+    #RII and SII for those. Calculations would need to be adjusted and thought well if we wanted to include them
+    mutate(count= n()) %>% filter(count == 5) %>% 
+    #This first part is to adjust rate and denominator with the population weights
+    mutate(cumulative_pro = cumsum(proportion_pop),  # cumulative proportion population for each area
+           relative_rank = case_when(
+             quintile == "1" ~ 0.5*proportion_pop,
+             quintile != "1" ~ lag(cumulative_pro) + 0.5*proportion_pop),
+           sqr_proportion_pop = sqrt(proportion_pop), #square root of the proportion of the population in each SIMD
+           relrank_sqr_proppop = relative_rank * sqr_proportion_pop,
+           rate_sqr_proppop = sqr_proportion_pop * rate) %>% #rate based on population weights
+    nest() %>% #creating one column called data with all the variables not in the grouping
+    # Calculating linear regression for all the groups, then formatting the results
+    # and calculating the confidence intervals
+    mutate(model = map(data, ~ lm(rate_sqr_proppop ~ sqr_proportion_pop + relrank_sqr_proppop + 0, data = .)),
+           #extracting sii from model, a bit fiddly but it works
+           sii = -1 * as.numeric(map(map(model, "coefficients"), "relrank_sqr_proppop")),
+           cis = map(model, confint_tidy)) %>% #calculating confidence intervals
+    ungroup() %>% unnest(cis) %>% #Unnesting the CIs 
+    #selecting only even row numbers which are the ones that have the sii cis
+    filter(row_number() %% 2 == 0) %>% 
+    mutate(lowci_sii = -1 * conf.high, #fixing interpretation
+           upci_sii = -1 * conf.low) %>% 
+    select(-conf.low, -conf.high, -model, -data) %>% #taking out results as not needed anymore
+    mutate_at(c("sii", "lowci_sii", "upci_sii"), ~replace(., is.na(.), NA_real_)) #recoding NAs
+  
+  #Merging sii with main data set
+  data_depr <- left_join(data_depr_sii, sii_model, by = c("code", "year", "quint_type"))
+  
+  #Calculating RII
+  data_depr <- data_depr %>% mutate(rii = sii / overall_rate,
+                                    lowci_rii = lowci_sii / overall_rate,
+                                    upci_rii = upci_sii / overall_rate,
+                                    #Transforming RII into %. This way is interpreted as "most deprived areas are
+                                    # xx% above the average" For example: Cancer mortality rate is around 55% higher 
+                                    # in deprived areas relative to the mean rate in the population
+                                    rii_int = rii * 0.5 *100,
+                                    lowci_rii_int = lowci_rii * 0.5 *100,
+                                    upci_rii_int = upci_rii * 0.5 *100)
+
+##################################################.
+##  PAF Population attributable risk and range  ----
+##################################################.
+
+#Calculation PAR
+#Formula here: https://pdfs.semanticscholar.org/14e0/c5ba25a4fdc87953771a91ec2f7214b2f00d.pdf
+# https://fhop.ucsf.edu/sites/fhop.ucsf.edu/files/wysiwyg/pg_apxIIIB.pdf
+#Adding columns for Most and least deprived rates
+most_depr <- data_depr %>% filter(quintile == "1") %>% 
+  select(code, year, quint_type, rate) %>% rename(most_rate = rate)
+
+least_depr <- data_depr %>% filter(quintile == "5") %>% 
+  select(code, year, quint_type, rate) %>% rename(least_rate = rate)
+
+data_depr <- left_join(data_depr, most_depr, by = c("code", "year", "quint_type"))
+data_depr <- left_join(data_depr, least_depr, by = c("code", "year", "quint_type"))
+
+data_depr <- data_depr %>%  group_by(code, year, quint_type) %>%
+  mutate(#calculating PAR. PAR of incomplete groups to NA
+    #CI calculation missing, this can help https://onlinelibrary.wiley.com/doi/pdf/10.1002/sim.2779
+    #https://fhop.ucsf.edu/sites/fhop.ucsf.edu/files/wysiwyg/pg_apxIIIB.pdf
+    par_rr = (rate/least_rate - 1) * proportion_pop,
+    count= n(),
+    par = case_when(count != 5 ~ NA_real_,
+                    count == 5 ~ sum(par_rr)/(sum(par_rr) + 1) * 100),
+    # Calculate ranges 
+    abs_range = most_rate - least_rate,
+    rel_range = most_rate / least_rate) %>% ungroup()
+
+#Joining with totals.
+#dataframe with the unique values for the different inequality measures
+data_depr_match <- data_depr %>% 
+  select(code, year, quint_type, sii, upci_sii, lowci_sii, rii, lowci_rii, upci_rii,
+         rii_int, lowci_rii_int, upci_rii_int, par, abs_range, rel_range) %>% 
+  unique()
+
+data_depr_totals <- left_join(data_depr_totals, data_depr_match, 
+                              by = c("code", "year", "quint_type"))
+
+data_depr <- bind_rows(data_depr, data_depr_totals) 
+data_depr <<- data_depr
+} #end sii/rii/paf/range function
+
 
 ##END
