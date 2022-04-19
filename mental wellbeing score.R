282368c7040a3756cfecf49e096acf0146644768
monicamcgibbon <monica.mcgibbon@phs.scot>
2022-04-19 15:01:37 +0100

    fix notes

--- mental wellbeing score.R
+++ mental wellbeing score.R
@@ -29,7 +29,7 @@ data_extract <- bind_rows(
   read_csv(paste0("https://statistics.gov.scot/slice/observations.csv?&dataset=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2Fscottish-health-survey-local-area-level-data&http%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23measureType=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fmeasure-properties%2F95-lower-confidence-limit&http%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23refPeriod=http%3A%2F%2Freference.data.gov.uk%2Fid%2Fgregorian-interval%2F",start_year,"-01-01T00%3A00%3A00%2FP4Y&http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fdimension%2FscottishHealthSurveyIndicator=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fconcept%2Fscottish-health-survey-indicator%2Fmental-wellbeing"), skip = 8) %>%
     mutate(measure = "lowci"),
   
-  # percentage hazardous/harmful drinker (exceeding 14 units a week)
+  # mean 
   read_csv(paste0("https://statistics.gov.scot/slice/observations.csv?&dataset=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2Fscottish-health-survey-local-area-level-data&http%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23measureType=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fmeasure-properties%2Fmean&http%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23refPeriod=http%3A%2F%2Freference.data.gov.uk%2Fid%2Fgregorian-interval%2F",start_year,"-01-01T00%3A00%3A00%2FP4Y&http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fdimension%2FscottishHealthSurveyIndicator=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fconcept%2Fscottish-health-survey-indicator%2Fmental-wellbeing"), skip = 8) %>%
     mutate(measure = "rate")
 ) %>%
