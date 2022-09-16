045469f00e33ff5ca81936d8511e0de184d3f5cd
Paul Fejiro <paulfe02@nssrstudio03.csa.scot.nhs.uk>
2022-09-16 10:36:33 +0100

    updating indicator script

--- Derelict site within 500m.R
+++ Derelict site within 500m.R
@@ -8,47 +8,22 @@
 
 
 ## This script prepares SG Population within 500 metres of a derelict site indicator.
-## Data formost recent update requested from SG SDVLS team.  
-## Data are now published on statistics.scot.gov.uk:
-## https://statistics.gov.scot/resource?uri=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2Fproximity-to-derelict-site
-## NOTE: does not currently include pop numbers to allow profile indicator production.
+## Data formost recent update requested from SG contact (see technical document).  
 
-################################################################################
-#####                          install packages etc                        #####
-################################################################################
-## remove any existing objects from global environment
-rm(list=ls()) 
 
-## install packages
-library(tidyverse) # all kinds of stuff 
-#library(stringr) # for strings
-
-## set file pathways
-# NHS HS PHO Team Large File repository file pathways
-data_folder <- "X:/ScotPHO Profiles/Data/" 
-lookups <- "X:/ScotPHO Profiles/Data/Lookups/" 
+source("1.indicator_analysis.R") 
 
 
 ################################################################################
-#####  Part 1)  format prepared data --------------------------------
+#####  Part 1)  format received data --------------------------------
 ################################################################################
 
-# open received data
-df_received <- read.csv(paste0(data_folder,"/Received Data/20901 - 500m derelict site.csv")) %>% 
-  as_tibble()
+derelict_site_data = read.csv(paste0(data_folder, "Received Data/Population within 500 metres of a derelict site.csv"))%>% 
+  select(c("datazone"="DataZone_Code","year","numerator"="u500pop","denominator"="totpop")) %>%
+  mutate_at(vars(numerator), ~replace(., is.na(.), 0))
 
-names(df_received) <- tolower(names(df_received)) # make col names lower case
 
-df_received$datazone <- as.character(df_received$datazone)
-
-saveRDS(df_received, paste0(data_folder,"Prepared Data/derelict_site_raw.rds"))
-
-###############################################.
-## Packages/Filepaths/Functions ----
-###############################################.
-organisation  <-  "HS"
-source("./1.indicator_analysis.R") #Normal indicator functions
-#source("./2.deprivation_analysis.R") # deprivation function - not required
+saveRDS(derelict_site_data, file=paste0(data_folder, 'Prepared Data/derelict_site_raw.rds')) 
 
 
 ###############################################.
@@ -56,11 +31,32 @@ source("./1.indicator_analysis.R") #Normal indicator functions
 ###############################################.
 
 analyze_first(filename = "derelict_site", geography = "datazone11",
-              measure = "percent", yearstart = 2016, yearend = 2017,
+              measure = "percent", yearstart = 2016, yearend = 2021,
               time_agg = 1)
 
-# then complete analysis with the updated '_formatted.rds' file
-analyze_second(filename = "derelict_site", measure = "percent", 
+analyze_second(filename = "derelict_site", measure = "percent",
                time_agg = 1, ind_id = "20901", year_type = "calendar")
 
+###############################################.
+## Part 3 - Retrieve missing years ----
+###############################################.
+# only run if years are missing (historic data missing)
+# extracting missing years from shiny data file already published in last 
+# because 2007-2015 data unavailable from contact at datazone11 
+
+
+# create function that retrieves missing years
+# filename is name of file in the shiny data folder
+# retrieve_year is the year up to which you want to retrieve data for
+merger <- function(filename,retrieve_year){
+  years_needed <- read_csv(paste0(data_folder, "Shiny Data/", filename, "_shiny.csv")) %>% subset(year<=retrieve_year)
+  new_file <- readRDS(paste0(data_folder, "Data to be checked/", filename, "_shiny.rds"))
+  new_complete_file <- rbind(years_needed,new_file) %>% arrange(code,year)
+  
+  saveRDS(new_complete_file, file = paste0(data_folder, "Data to be checked/", filename, "_shiny.rds"))
+  write_csv(new_complete_file, path = paste0(data_folder, "Data to be checked/", filename, "_shiny.csv"))
+}
+
+merger("derelict_site",2015)# retrieve and create total data for indicator
+run_qa("derelict_site") # run QA on new data file to ensure no missing years anymore
 
