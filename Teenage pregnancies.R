19c295241e115a8fd74a1c40b3be49cfc30b6b9b
monicamcgibbon <monica.mcgibbon@phs.scot>
2023-01-27 10:04:33 +0000

    include 2020 data

--- Teenage pregnancies.R
+++ Teenage pregnancies.R
@@ -16,7 +16,7 @@ source("./2.deprivation_analysis.R") # deprivation function
 # Reading data provided by maternity team
 # All data is included, but to calculate the rates only 15-19 female pop is used, 
 # following ISD publication methodology.
-teen_preg <- read_csv( paste0(data_folder, "Received Data/IR2021-00738_teenpreg_2021.csv")) %>% 
+teen_preg <- read_csv( paste0(data_folder, "Received Data/IR2023-00055 - teenage pregnancies.csv")) %>% 
   setNames(tolower(names(.))) %>% #set names to lower case
   rename(datazone = datazone2011, numerator = tp, year = yearcon) %>% 
   subset(!(is.na(datazone) | datazone == "         ")) %>% #excluding non-Scottish residents
@@ -30,7 +30,7 @@ saveRDS(teen_preg, file=paste0(data_folder, 'Prepared Data/teen_preg_raw.rds'))
 ## Part 2 - Run analysis functions ----
 ###############################################.
 analyze_first(filename = "teen_preg", geography = "datazone11", measure = "crude", 
-              yearstart = 2002, yearend = 2019, time_agg = 3, pop='DZ11_pop_fem15to19')
+              yearstart = 2002, yearend = 2020, time_agg = 3, pop='DZ11_pop_fem15to19')
 
 analyze_second(filename = "teen_preg", measure = "crude", time_agg = 3, crude_rate=1000,
                ind_id = 21001, year_type = "calendar")
