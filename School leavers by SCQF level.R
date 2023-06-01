a78fed89b1cc72e1c1aaaa364ffaae4baa934218
Monica McGibbon <monica.mcgibbon@phs.scot>
2023-06-01 08:47:58 +0100

    modifying scqf indicators script to test new source_suppressed argument

--- School leavers by SCQF level.R
+++ School leavers by SCQF level.R
@@ -43,28 +43,28 @@ scqf_level <- scqf_level %>%
          across(contains(c("scqf", "leaver", "year")), as.numeric)) %>% 
   rename("denominator" = "number of leavers") %>%
   left_join(ca, by = c("local authority" = "areaname")) %>% # join with council area lookup
-  mutate(code = ifelse(`local authority` == "Scotland", "S00000001", code)) # assign scotland totals a geography code
+  mutate(ca = ifelse(`local authority` == "Scotland", "S00000001", code)) # assign scotland totals a geography code
 
 
 # school leavers with 1 or more qualification at scqf level 4 ----
 scqf_4 <- scqf_level %>%
   filter(`simd quintile [note 5]` == "Total") %>%
   rename("numerator" = "1+ at scqf\r\nlevel 4 or better") %>%
-  select(year, code, denominator, numerator)
+  select(year, ca, denominator, numerator)
 
 
 # school leavers with 1 or more qualification at scqf level 6 ----
 scqf_6 <- scqf_level %>%
   filter(`simd quintile [note 5]` == "Total") %>%
   rename("numerator" = "1+ at scqf\r\nlevel 6 or better") %>%
-  select(year, code, denominator, numerator) 
+  select(year, ca, denominator, numerator) 
 
 
 # school leavers in most deprived quantile with 1 or more qualification at level 6
 scqf_6_depr <- scqf_level %>%#
   filter(`simd quintile [note 5]` == "0-20% (Most Deprived)") %>%
   rename("numerator" = "1+ at scqf\r\nlevel 6 or better") %>%
-  select(year, code, denominator, numerator) 
+  select(year, ca, denominator, numerator) 
 
 
 #1.d. Save files - do some QA checks at this point ----
@@ -82,51 +82,67 @@ saveRDS(scqf_6_depr, file=paste0(data_folder, 'Prepared Data/school_leavers_scqf
 # workaround below formats data to be used in analyze_second() function 
 #it ensures Scotland totals remain accurate and are not just the sum of all council area figures
 
-yearstart <- 2009
-yearend <- 2022 # note: change each time updating indicator
+# yearstart <- 2009
+# yearend <- 2022 # note: change each time updating indicator
+# 
+# #read data back in (containing council area and scotland figures)
+# scqf_4 <- readRDS(paste0(data_folder, "Prepared Data/school_leavers_scqf_4_raw.rds"))
+# scqf_6 <- readRDS(paste0(data_folder, "Prepared Data/school_leavers_scqf_6_raw.rds"))
+# scqf_6_depr <- readRDS(paste0(data_folder, "Prepared Data/school_leavers_scqf_6_depr_raw.rds"))
+# 
+# 
+# # read in geography lookup to get health boards
+# geo_lookup <- readRDS(paste0(lookups, "Geography/DataZone11_All_Geographies_Lookup.rds")) %>%
+#   select(ca2019, hb2019) %>%
+#   distinct(.)
+# 
+# 
+# # function to combine health board figures with council area and scotland figures
+# include_hb_figures <- function(df) {
+#   
+#   df %>%
+#     filter(year >= yearstart & year <= yearend,
+#            code != "S00000001") %>% 
+#     
+#     # add health board codes
+#     left_join(geo_lookup, by = c("code" = "ca2019")) %>%
+#     select(-code) %>%
+#     rename("code" = "hb2019") %>%
+#     group_by(year, code) %>%
+#     
+#     # only calculate health board figures if no suppression applied to corresponding council areas
+#     # otherwise apply suppression
+#     summarise(numerator = ifelse(any(is.na(numerator)), NA, sum(numerator)), 
+#               denominator = ifelse(any(is.na(numerator)), NA, sum(denominator)), .groups = "drop") %>%
+#   rbind(df)
+#   
+# }
+# 
+# scqf_4_final <- include_hb_figures(scqf_4)
+# scqf_6_final <- include_hb_figures(scqf_6)
+# scqf_6_depr_final <- include_hb_figures(scqf_6_depr)
+# 
+# 
+# #save files to be used in analyze_second() function
+# saveRDS(scqf_4_final, file=paste0(data_folder, "Temporary/school_leavers_scqf_4_formatted.rds"))
+# saveRDS(scqf_6_final, file=paste0(data_folder, "Temporary/school_leavers_scqf_6_formatted.rds"))
+# saveRDS(scqf_6_depr_final, file=paste0(data_folder, "Temporary/school_leavers_scqf_6_depr_formatted.rds"))
+# 
 
-#read data back in (containing council area and scotland figures)
-scqf_4 <- readRDS(paste0(data_folder, "Prepared Data/school_leavers_scqf_4_raw.rds"))
-scqf_6 <- readRDS(paste0(data_folder, "Prepared Data/school_leavers_scqf_6_raw.rds"))
-scqf_6_depr <- readRDS(paste0(data_folder, "Prepared Data/school_leavers_scqf_6_depr_raw.rds"))
 
 
-# read in geography lookup to get health boards
-geo_lookup <- readRDS(paste0(lookups, "Geography/DataZone11_All_Geographies_Lookup.rds")) %>%
-  select(ca2019, hb2019) %>%
-  distinct(.)
+analyze_first(filename = "school_leavers_scqf_4", measure = "percent", 
+               yearstart = "2009", yearend = "2022", time_agg = 1, 
+               source_suppressed = TRUE, geography = "council")
 
 
-# function to combine health board figures with council area and scotland figures
-include_hb_figures <- function(df) {
-  
-  df %>%
-    filter(year >= yearstart & year <= yearend,
-           code != "S00000001") %>% 
-    
-    # add health board codes
-    left_join(geo_lookup, by = c("code" = "ca2019")) %>%
-    select(-code) %>%
-    rename("code" = "hb2019") %>%
-    group_by(year, code) %>%
-    
-    # only calculate health board figures if no suppression applied to corresponding council areas
-    # otherwise apply suppression
-    summarise(numerator = ifelse(any(is.na(numerator)), NA, sum(numerator)), 
-              denominator = ifelse(any(is.na(numerator)), NA, sum(denominator)), .groups = "drop") %>%
-  rbind(df)
-  
-}
+analyze_first(filename = "school_leavers_scqf_6", measure = "percent", 
+              yearstart = "2009", yearend = "2022", time_agg = 1, 
+              source_suppressed = TRUE)
 
-scqf_4_final <- include_hb_figures(scqf_4)
-scqf_6_final <- include_hb_figures(scqf_6)
-scqf_6_depr_final <- include_hb_figures(scqf_6_depr)
-
-
-#save files to be used in analyze_second() function
-saveRDS(scqf_4_final, file=paste0(data_folder, "Temporary/school_leavers_scqf_4_formatted.rds"))
-saveRDS(scqf_6_final, file=paste0(data_folder, "Temporary/school_leavers_scqf_6_formatted.rds"))
-saveRDS(scqf_6_depr_final, file=paste0(data_folder, "Temporary/school_leavers_scqf_6_depr_formatted.rds"))
+analyze_first(filename = "school_leavers_scqf_6_depr", measure = "percent", 
+              yearstart = "2009", yearend = "2022", time_agg = 1, 
+              source_suppressed = TRUE)
 
 
 
