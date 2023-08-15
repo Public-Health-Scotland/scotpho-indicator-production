3858a70ca7188c863d5af39cdc2176ee74849def
Monica McGibbon <monica.mcgibbon@phs.scot>
2023-08-15 10:09:17 +0100

    2023 update

--- 16-24 employment.R
+++ 16-24 employment.R
@@ -9,88 +9,76 @@
 #  - Data downloads
 # 	-Query		(https://www.nomisweb.co.uk/query/select/getdatasetbytheme.asp?opt=3&theme=&subgrp=)
 # 	-Annual population service (APS)
-# 	-Geography  (select countries : Scotland ,  Local authorities (within Scotland) then tick all within Scotland)
+# 	-Geography  (select countries : Scotland ,  Local authorities - District (within Scotland) then tick all within Scotland)
 #   -Date (12 months to December) – pick time periods required – full series if refreshing all or single year to add one year to existing dataset)
 # 	-Cell: Select ‘Change analysis’ option located near title “annual population survey” – then select table T01 “economic activity by age”
 # 	-Select option to download data for those aged 16-19 amd 20-24 in the “All People” (so no need for sex breakdown) and “All” plus “In employment” options [This means 4 options should be ticked in total]
-# 	-Summary of selections should allow you to review the choices
+# 	-Summary of selections should allow you to review the choices (select one table per worksheet and include area codes)
 # 	-Download Data – should present option to download data as an excel file
 
 # Data down load contains raw population total and numbers of 16-19 and 19-24 employed for scotland and local authorities, these datasets need to be manipulated then summed to give
 # totals for 16-24 years.
 
 #   Part 1 - Read in NOMIS extract data
-#   Part 2 - Create the different geographies basefiles
-#   Part 3 - Run analysis functions 
+#   Part 2 - Run analysis functions 
 
 
 
-### 2  - functions/packages ----
+### functions/packages ----
 
 source("1.indicator_analysis.R") 
 library(stringr)
 library(rio)
 
 
-### 3 - read in data and clean/format ----
+### 1 - Read in NOMIS extract data ----
 
   #read in the data
-  data <- import_list(paste0(data_folder,"Received Data/nomis_16-24_employment.xlsx"),  setclass = "tbl",  
+  data <- import_list(paste0(data_folder,"Received Data/NOMIS Data Downloads/nomis_16_to_24_in_employment.xlsx"),  setclass = "tbl",  
                            rbind = TRUE, skip = 5, rbind_label = "excel tab") %>% 
  
   #rename columns
-  rename_at(2, ~"code") %>%
+  rename_at(2, ~"ca") %>%
   rename_at(1, ~"ca name") %>%
 
   #add in Scotland
-  mutate(code = ifelse(`ca name` == 'Column Total', "S00000001", code)) %>%
+  mutate(ca = ifelse(`ca name` == 'Column Total', "S00000001", ca)) %>%
   
   #filter rows and columns
   filter_all(any_vars(grepl("S12|S00", .))) %>%  #remove unwanted rows 
   select(-contains("...")) %>% # remove unwanted cols
   
   #dealing with suppressed values
-  mutate(across(everything(), ~replace(., . %in% c("!", "~", "*"), 0))) %>%
-  mutate(across(-c("code", "ca name"), as.numeric)) %>%
+  mutate(across(everything(), ~replace(., . %in% c("!", "~", "*"), NA))) %>%
+  mutate(across(-c("ca", "ca name"), as.numeric)) %>%
   
   #combine ages and group together
   mutate(variable = ifelse(`excel tab` %in% c(1,3), "denominator", "numerator")) %>%
   select(-`excel tab`, -`ca name`) %>%
-  group_by(code, variable) %>%
+  group_by(ca, variable) %>%
   summarise_all(sum) %>%
   ungroup() %>%
 
   #reshape the data
-  pivot_longer(cols = -c("code", "variable"), names_to = "year") %>%
+  pivot_longer(cols = -c("ca", "variable"), names_to = "year") %>%
   pivot_wider(names_from = "variable", values_from = "value") %>%
   
   #replace names of year
-  mutate(year = str_sub(year, start= -4))
+  mutate(year = as.numeric(str_sub(year, start= -4)))
   
-  
-  # read in geography lookup to get health boards
-  geo_lookup <- readRDS(paste0(lookups, "Geography/DataZone11_All_Geographies_Lookup.rds")) %>%
-    select(ca2019, hb2019) %>%
-    distinct(.)
-  
-  #include health board figures
-  final <- data %>%
-   filter(code != "S00000001") %>% 
-      left_join(geo_lookup, by = c("code" = "ca2019")) %>%
-      select(-code) %>%
-      rename("code" = "hb2019") %>%
-      group_by(year, code) %>%
-      summarise_all(sum) %>% ungroup() %>%
-      rbind(data_list)
-    
- #save files to be used in analyze_second() function
-  saveRDS(final, file=paste0(data_folder, "Temporary/1308_16-24_employment_formatted.rds"))
+
+ #save files to be used in analysis functions
+  saveRDS(data, file=paste0(data_folder, "Prepared Data/16-24_employment_raw.rds"))
   
   
   
-# 4 - use analyze_second() function to create final shiny app file  
+# 2. Run analysis functions ----
   
-  analyze_second(filename = "1308_16-24_employment", measure = "percent", 
+  analyze_first(filename = "16-24_employment", measure = "percent",  geography = "council",
+                 time_agg = 1, source_suppressed = TRUE, yearstart = 2004, yearend = 2022)
+  
+
+  analyze_second(filename = "16-24_employment", measure = "percent", 
                  time_agg = 1, ind_id = "13018",year_type = "calendar")
   
   
