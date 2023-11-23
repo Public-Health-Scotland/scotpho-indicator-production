997c3639eeeba56383e08de589448b0c0e10b1fd
callid01 <calli.dougall@phs.scot>
2023-11-23 17:04:56 +0000

    Reading in data and producing files for employees on the living wage

---
+++ Employees on the living wage.R
@@ -0,0 +1,75 @@
+###   Update ScotPHO Care and Wellbeing indicator: 
+#   99111: Employees on the living wage
+
+
+
+# Data source is ONS Annual Survey of Hours and Earnings published on the Scottish Government website:
+# https://www.gov.scot/publications/annual-survey-of-hours-and-earnings-2022/
+
+
+
+### functions/packages -----
+source("1.indicator_analysis.R") 
+library(readxl)
+library(dplyr)
+library(janitor)
+
+
+
+### 1. Read in data ----
+
+# Identify file
+file <- paste0(data_folder, "Received Data/", list.files(path=paste0(data_folder,"Received Data/"), 
+                                                         pattern="Annual-Survey-of-Hours-and-Earnings-2022-Tables"))
+
+# Set ranges for all, men and women tables
+ranges <- c("A5:F17", "A20:F31", "A34:F45")
+
+
+# Function to read in tables
+data <- lapply(ranges, function(i){
+  
+  x = read_excel(file, sheet = "Table 5.2", range = i)
+  colnames(x) = c("year", "less_number", "more_number", "blank", "less_prop", "percent")
+  x$sex = i
+  x
+  
+})
+
+
+# Save as one data frame
+data <- do.call("rbind.data.frame", data) 
+
+
+
+### 2. Prepare data  -----
+
+data <- data %>%
+  
+  # Select relevant columns
+  select(sex, year, percent) %>%
+  
+  # Remove NAs
+  na.omit() %>%
+  
+  # Transform range into sex and create columns
+  mutate(sex = str_replace(sex, "A5:F17", "all"),
+         sex = str_replace(sex, "A20:F31", "men"),
+         sex = str_replace(sex, "A34:F45", "women"),
+         ind_id = 99111,
+         code = "S00000001",
+         def_period = paste0(year, " survey year"),
+         trend_axis = year,
+         lowci = NA, upci = NA,
+         numerator = NA) %>%
+  
+  # Filter for 2021 and 2022 (earlier data not directly comparable)
+  filter(year %in% c("2021", "2022"))
+  
+
+
+### 3. Prepare final files -----
+
+# Save files in folder to be checked
+write.csv(data, paste0(data_folder, "Data to be checked/", "employees_on_the_living_wage_shiny.csv"), row.names = FALSE)
+write_rds(data, paste0(data_folder, "Data to be checked/", "employees_on_the_living_wage_shiny.rds"))
