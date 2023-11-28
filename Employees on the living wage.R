dd995e2b3ca08bae1b58531bfb2ad972e43542de
callid01 <calli.dougall@phs.scot>
2023-11-28 13:07:14 +0000

    Add LA breakdowns

--- Employees on the living wage.R
+++ Employees on the living wage.R
@@ -36,32 +36,59 @@ data <- lapply(ranges, function(i){
 data <- do.call("rbind.data.frame", data) 
 
 
+# Read in table of LA breakdowns (Table 5.7)
+data_ca <- read_excel(source_file, sheet = "Table 5.7", range = "X6:Y38", na = "x")
+
+
+# Read in LA lookup
+CAdictionary <- readRDS(paste0(lookups, "Geography/CAdictionary.rds")) %>%
+  arrange(areaname) %>%
+  select(code)
+
+
 
 ### 2. Prepare data  -----
 
+# LA level data
+data_ca <- data_ca %>%
+  
+  # Bind area codes
+  bind_cols(CAdictionary) %>%
+  
+  # Transform to long format
+  pivot_longer(where(is.numeric), names_to = "year", values_to = "rate") %>%
+  
+  # Add sex column
+  mutate(sex = "all")
+
+
+# Combine LA and Scotland level data
 data <- data %>%
+  
+  # Filter for 2021 and 2022 (earlier data not directly comparable)
+  filter(year %in% c("2021", "2022")) %>%
   
   # Select relevant columns
   select(sex, year, rate) %>%
   
-  # Remove all
-  filter(year != "All") %>%
-  
-  # Transform range into sex and create columns
+  # Transform range into sex and create code column
   mutate(sex = str_replace(sex, "A5:F17", "all"),
          sex = str_replace(sex, "A20:F31", "men"),
          sex = str_replace(sex, "A34:F45", "women"),
-         ind_id = 99111,
-         code = "S00000001",
+         code = "S00000001") %>%
+  
+  # Bind LA rows
+  rbind(data_ca) %>%
+  
+  # Create new columns
+  mutate(ind_id = 99111,
          def_period = paste0(year, " survey year"),
          trend_axis = year,
          lowci = NA, upci = NA,
          numerator = NA) %>%
   
-  # Filter for 2021 and 2022 (earlier data not directly comparable)
-  filter(year %in% c("2021", "2022")) %>%
   arrange(code, year, sex)
-  
+
 
 
 ### 3. Prepare final files -----
@@ -69,3 +96,10 @@ data <- data %>%
 # Save files in folder to be checked
 write.csv(data, paste0(data_folder, "Data to be checked/", "employees_on_the_living_wage_shiny.csv"), row.names = FALSE)
 write_rds(data, paste0(data_folder, "Data to be checked/", "employees_on_the_living_wage_shiny.rds"))
+
+
+# Run QA report and check the output file
+run_qa(filename = "employees_on_the_living_wage")
+
+
+# END
