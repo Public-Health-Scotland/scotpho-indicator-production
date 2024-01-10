a0769c99dc03c1b66b25a3e20ced7dce3033c43f
callid01 <calli.dougall@phs.scot>
2024-01-10 14:55:58 +0000

    Remove loneliness indicator, bind area codes and remove inequalities breakdowns for now

--- National Performance Framework.R
+++ National Performance Framework.R
@@ -1,17 +1,10 @@
-###SCRIPT CURRENTLY UNDER DEVELOPMENT!!
-# To do:
-  # Decide how inequalities data needs to be formatted
-  # Bind datazone codes
-
-
 ###   Update ScotPHO Care and Wellbeing indicators: 
-#   99113: Loneliness 
 #   99116: Persistent poverty
 #   99117: Child wellbeing and happiness
 #   99118: Child material deprivation
 #   99121: Health risk behaviours
 #   99122: Quality of care experience
-#   99123: Gender balance in organisations
+#   99123: Gender balance in organisations (for minority ethnic population)
 
 
 # Data source is the National Performance Framework open data on statistics.gov.scot
@@ -23,11 +16,16 @@ source("1.indicator_analysis.R")
 
 
 
-### 1 - Read in NPF data -----
+### 1 - Read in data -----
 
-# Read in data 
+# Read in NPF data 
 data <- read_xlsx(paste0(data_folder, "Received Data/NPF Database - 01.11.23.xlsx")) %>%
   clean_names()
+
+
+# Read in geography lookup
+dictionary <- readRDS(paste0(lookups, "Geography/opt_geo_lookup.rds")) %>%
+  select(!c(parent_area, areaname_full))
 
 
 
@@ -36,8 +34,7 @@ data <- read_xlsx(paste0(data_folder, "Received Data/NPF Database - 01.11.23.xls
 data <- data %>%
   
   # Select relevant indicators
-  filter(indicator %in% c("Loneliness",
-                          "Persistent Poverty",
+  filter(indicator %in% c("Persistent Poverty",
                           "Child Wellbeing and Happiness",
                           "Child material deprivation",
                           "Health risk behaviours",
@@ -48,10 +45,13 @@ data <- data %>%
   mutate(indicator = str_replace_all(tolower(indicator), " ", "_"),
          
          # Ensure breakdowns are named consistently
+         disaggregation = str_replace_all(disaggregation, "Health and Social Care Partnership", "HSC partnership"),
+         disaggregation = str_replace_all(disaggregation, "Health Board", "Health board"),
          disaggregation = str_replace_all(disaggregation, "SIMD", "Scottish Index of Multiple Deprivation"),
          disaggregation = str_replace_all(disaggregation, "Total Difficulties Score X ", ""),
          disaggregation = str_replace_all(disaggregation, "Gender", "Sex"),
          disaggregation = if_else(indicator == "persistent_poverty" & breakdown == "Total", "Total", disaggregation),
+         disaggregation = if_else(str_detect(breakdown, "Ethnic minority"), "Total", disaggregation),
 
          breakdown = if_else(str_detect(breakdown, "1|(?i)most"), "1 - most deprived", breakdown),
          breakdown = if_else(str_detect(breakdown, "2$|2nd"), "2", breakdown),
@@ -63,15 +63,14 @@ data <- data %>%
          breakdown = str_replace_all(breakdown, "Male", "Men"),
          breakdown = str_replace_all(breakdown, "Female", "Women"),
          
-         breakdown = if_else(disaggregation == "Health Board" & !grepl("NHS", breakdown), paste0("NHS ", breakdown), breakdown),
+         breakdown = if_else(disaggregation == "Health board" & !grepl("NHS", breakdown), paste0("NHS ", breakdown), breakdown),
          breakdown = str_replace_all(breakdown, "\\band\\b", "&"),
          breakdown = str_replace_all(breakdown, "Edinburgh, City of", "City of Edinburgh"),
          
-         year = str_replace_all(year, "2017-2021 (excl. 2020)", "2017-2021"), # Doesn't work for some reason
+         year = if_else(str_detect(year, "(excl. 2020)"), "2017-2021", year),
          
          # Add indicator ids
-         ind_id = case_when(indicator == "loneliness" ~ 99113,
-                            indicator == "persistent_poverty" ~ 99116,
+         ind_id = case_when(indicator == "persistent_poverty" ~ 99116,
                             indicator == "child_wellbeing_and_happiness" ~ 99117,
                             indicator == "child_material_deprivation" ~ 99118,
                             indicator == "health_risk_behaviours" ~ 99121,
@@ -79,7 +78,7 @@ data <- data %>%
                             indicator == "gender_balance_in_organisations" ~ 99123),
          
          # Create new columns
-         geography = if_else(str_detect(disaggregation, "Local Authority|Health Board|Health and Social Care Partnership"), breakdown, "Scotland"),
+         areaname = if_else(str_detect(disaggregation, "Local Authority|Health board|HSC partnership"), breakdown, "Scotland"),
          numerator = "",
          rate = as.numeric(figure)) %>%
   
@@ -88,21 +87,31 @@ data <- data %>%
                                "Scottish Index of Multiple Deprivation",
                                "SIMD",
                                "Local Authority",
-                               "Health and Social Care Partnership",
-                               "Health Board",
+                               "HSC partnership",
+                               "Health board",
                                "Gender",
                                "Total Difficulties Score",
                                "Total Difficulties Score X Sex",
                                "Total Difficulties Score X SIMD"),
          
-         # Remove duplicate total row for gender balance indicator
-         !(breakdown == "Total" & disaggregation == "Scottish Index of Multiple Deprivation")) %>%
+         # Remove duplicate rows
+         !(breakdown == "Total" & disaggregation == "Scottish Index of Multiple Deprivation"),
+         !(indicator == "gender_balance_in_organisations" & breakdown == "Total")) %>%
+  
+  # Also remove inequalities breakdowns for now until it's decided how they'll be presented on OPT
+  filter(disaggregation != "Scottish Index of Multiple Deprivation") %>%
+  
+  # Create new area type variable from disaggregation
+  mutate(areatype = if_else(str_detect(disaggregation, "Total"), "Scotland", disaggregation)) %>%
+  
+  # Join area codes
+  left_join(dictionary, by = c("areaname", "areatype")) %>%
   
   # Select relevant variables
-  select(c(ind_id, indicator, geography, year, disaggregation, breakdown, rate, numerator)) %>%
+  select(c(ind_id, indicator, code, year, rate, numerator)) %>%
   
   # Reorder data frame
-  arrange(indicator, disaggregation, breakdown, year)
+  arrange(indicator, code, year)
   
 
 
@@ -116,14 +125,21 @@ prepare_shiny_file <- function(ind) {
   
   # Create different date variables depending on what indicator it is:
   
-  # Single years
-  if (ind == "loneliness" | ind == "health_risk_behaviours" | ind == "gender_balance_in_organisations") {
+  # Single survey year
+  if (ind == "health_risk_behaviours") {
     
     dat <- dat %>%
       mutate(trend_axis = year,
-             def_period = paste0(year, " survey year")) # Check to see if this should just be year
+             def_period = paste0(year, " survey year"))
+    
+  # Single calendar year
+  } else  if (ind == "gender_balance_in_organisations") {
+      
+      dat <- dat %>%
+        mutate(trend_axis = year,
+               def_period = paste0(year, " calendar year"))
   
-  # Single survey years  
+  # Combined survey years  
   } else if (ind == "quality_of_care_experience") {
     
     dat <- dat %>% 
@@ -165,7 +181,7 @@ for (i in unique(data$indicator)){
   
     run_qa(filename = i)
     
-  }
+}
 
 
 #END
