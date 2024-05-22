32fb95deb77b8c80178d03ce65835ffcd4ba0ed2
abigap01 <abigail.prescott@phs.scot>
2024-05-22 11:06:58 +0100

    Updates to 3 indicators

--- developmental concerns 27-30 months.R
+++ developmental concerns 27-30 months.R
@@ -9,25 +9,30 @@
 source("1.indicator_analysis.R") #Normal indicator functions
 source("2.deprivation_analysis.R") # deprivation function
 
-library(janitor)
-
 ###############################################.
 ## Part 1 - Prepare basefile ----
 ###############################################.
-dev_concerns <- read_csv(paste0(data_folder, "Received Data/IR2023-00042_development27months.csv")) %>%
-  setNames(tolower(names(.))) %>%
-  clean_names() %>%
-  mutate(datazone = as.factor(datazone2011))
-#removes all other geographies apart from datazone(needed if received data contains Scotland and hb data)
-dev_concerns <- dev_concerns %>%
+
+# read in the data
+dev_concerns <- read_excel(paste0(data_folder, "Received Data/Developmental concerns/IR2024-00253_development27months.xlsx")) 
+
+# tidy up col names
+dev_concerns <- dev_concerns |> 
+  setNames(tolower(names(dev_concerns))) |> #sets all column names to lowercase
+  clean_names() |> 
+  rename(datazone = datazone2011,
+         fin_year = finyr_eligible) #renaming variables to datazone and fin_year
+
+
+#removes all other geographies apart from datazone (needed if received data contains Scotland and hb data)
+dev_concerns <- dev_concerns |> 
   filter(!(is.na(datazone))|hb_residence_desc=="Unknown")
   
-dev_concerns <- dev_concerns %>%
-  mutate( #creates year field based on lenght of fin_year
-    year=case_when(nchar(fin_year)==3 ~ paste0("200",substr(fin_year,1,1)), 
-                   TRUE ~ paste0("20",substr(fin_year,1,2)))) %>%
-  group_by(year, datazone) %>%
-  summarise(numerator = sum(concerns), denominator = sum(reviews)) %>%
+dev_concerns <- dev_concerns |> 
+  mutate( #creates year field based on length of fin_year
+    year=substr(fin_year, start=1, stop=4)) |> 
+  group_by(year, datazone) |> 
+  summarise(numerator = sum(concerns), denominator = sum(reviews)) |> 
   ungroup()
 
 saveRDS(dev_concerns, file=paste0(data_folder, 'Prepared Data/dev_concerns_raw.rds')) 
@@ -37,7 +42,7 @@ saveRDS(dev_concerns, file=paste0(data_folder, 'Prepared Data/dev_concerns_raw.r
 ###############################################.
 
 analyze_first(filename = "dev_concerns", geography = "datazone11", hscp = T, measure = "percent", 
-              yearstart = 2013, yearend = 2021, time_agg = 3)
+              yearstart = 2013, yearend = 2022, time_agg = 3)
 
 analyze_second(filename = "dev_concerns", measure = "percent", time_agg = 3, 
                ind_id = 13048, year_type = "financial")
