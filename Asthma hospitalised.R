a78dd2d3ad079a86061140d9b8a443e75abcdd8d
Monica McGibbon <monica.mcgibbon@phs.scot>
2024-09-09 09:23:46 +0100

    asthma <16 2022/23 update

--- Asthma hospitalised.R
+++ Asthma hospitalised.R
@@ -31,14 +31,14 @@ data_asthma <- tbl_df(dbGetQuery(channel, statement=
       CASE WHEN extract(month from admission_date) > 3 THEN extract(year from admission_date) 
         ELSE extract(year from admission_date) -1 END as year 
    FROM ANALYSIS.SMR01_PI z
-   WHERE admission_date between '1 April 2002' and '31 March 2022'
+   WHERE admission_date between '1 April 2002' and '31 March 2023'
       AND sex <> 0 
       AND regexp_like(main_condition, 'J4[5-6]') ")) %>% 
   setNames(tolower(names(.))) %>%   #variables to lower case
   create_agegroups() # Creating age groups for standardization
 
 # Bringing  LA and datazone info.
-postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2022_2.rds') %>% 
+postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2024_2.rds') %>% 
   setNames(tolower(names(.))) %>%   #variables to lower case
   select(pc7, datazone2001, datazone2011, ca2019)
 
@@ -103,7 +103,7 @@ analyze_deprivation(filename="asthma_depr", measure="stdrate", time_agg=3,
 
 #Under 16 asthma patients
 analyze_first(filename = "asthma_under16", geography = "council", measure = "stdrate", 
-              pop = "CA_pop_under16", yearstart = 2002, yearend = 2021, hscp = T,
+              pop = "CA_pop_under16", yearstart = 2002, yearend = 2022, hscp = T,
               time_agg = 3, epop_age = '<16')
 
 analyze_second(filename = "asthma_under16", measure = "stdrate", time_agg = 3, 
