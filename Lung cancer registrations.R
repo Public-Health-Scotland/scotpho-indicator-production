4f30c6dc72320f0678f96598e656586422f9922e
apigap01 <abigail.prescott@phs.scot>
2024-09-30 11:21:17 +0100

    Lung cancer registrations and deaths 2022 update

--- Lung cancer registrations.R
+++ Lung cancer registrations.R
@@ -24,7 +24,7 @@ lung_reg <- as_tibble(dbGetQuery(channel, statement=
   "SELECT count(*) count, extract (year from INCIDENCE_DATE) year, SEX sex_grp, 
       POSTCODE pc7, age_in_years age 
    FROM ANALYSIS.SMR06_PI
-   WHERE incidence_date between '1 January 2002' and '31 December 2021'
+   WHERE incidence_date between '1 January 2002' and '31 December 2022'
       AND age_in_years>=16 
       AND regexp_like(ICD10S_CANCER_SITE, 'C3[34]')
       AND sex <> 9
@@ -33,7 +33,7 @@ lung_reg <- as_tibble(dbGetQuery(channel, statement=
   create_agegroups() # Creating age groups for standardization.
 
 # Bringing  LA info.
-postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2023_2.rds') %>% 
+postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2024_2.rds') %>% 
   setNames(tolower(names(.))) %>% select(pc7, ca2019) #variables to lower case
 
 lung_reg <- left_join(lung_reg, postcode_lookup, by = "pc7") %>% #merging with lookup
@@ -48,7 +48,7 @@ saveRDS(lung_reg, file=paste0(data_folder, 'Prepared Data/lungcancer_reg_raw.rds
 ## Part 2 - Run analysis functions ----
 ###############################################.
 analyze_first(filename = "lungcancer_reg", geography = "council", measure = "stdrate", 
-              pop = "CA_pop_16+", yearstart = 2002, yearend = 2021, hscp = T,
+              pop = "CA_pop_16+", yearstart = 2002, yearend = 2022, hscp = T,
               time_agg = 3, epop_age = "16+")
 
 analyze_second(filename = "lungcancer_reg", measure = "stdrate", time_agg = 3, 
