35d5ddcc46b083e59c41e088a9a6ccafce0a3d42
apigap01 <abigail.prescott@phs.scot>
2024-11-08 15:40:16 +0000

    Pop groups file created for meeting MVPA guidelines

--- Meeting MVPA guidelines.R
+++ Meeting MVPA guidelines.R
@@ -3,12 +3,12 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
+source("1.indicator_analysis.R") #normal indicator functions
+source("2.deprivation_analysis.R") #deprivation function
 
 
 ###############################################.
-## Part 1 - Prepare basefile ----
+## Part 1 - Prepare file for main dataset ----
 ###############################################.
 
 #read in HB/CA lookups
@@ -18,16 +18,16 @@ hb_codes <- readRDS(paste0(lookups, "Geography/HBdictionary.rds")) |>
   mutate(Geographylevel = "Health Board")
 
 lookup<-rbind(ca_codes, hb_codes) |> 
-  rename(Location = areaname) #aligning for lookup
+  rename(Location = areaname) #aligning col names to match lookup
 
 
 #read in main data (no splits)
 mvpa <- read.csv(paste0(data_folder, "Received Data/Physical Activity/MVPA/shs_activity_levels_rank_data_2012_15_2018_22_v2.csv")) |>
   select(-c(4,8,11)) |> #drop unnecessary cols
   filter(Categories == "Meets recommendations",
-         Sex == "All") |>  #filter for both sexes and for only meets recommendations
+         Sex == "All") |>  #filter for both sexes combined and for only meets recommendations
   mutate(Location = case_when(Geographylevel == "Health Board"~ paste0("NHS ", Location), #Paste NHS onto the front of HB names to match lookup
-                              Location == "Edinburgh City" ~ "City of Edinburgh", #Change Edinburgh City to City of Edinburgh
+                              Location == "Edinburgh City" ~ "City of Edinburgh", #Change Edinburgh City to City of Edinburgh to match lookup
                               TRUE ~ Location) 
          )
 
@@ -45,10 +45,59 @@ mvpa <- left_join(mvpa, lookup) |>
          upci = UpperCI) |> 
   select(c(9:10, 12, 11, 6:8, 13, 1)) #dropping and reordering columns
   
-
-###############################################.
-## Part 2 - Save Basefile ----
-###############################################.
-
+#save files
 saveRDS(mvpa, file = paste0(data_folder, "Data to be checked/meeting_mvpa_shiny.rds"))
 write.csv(mvpa, file = paste0(data_folder, "Data to be checked/meeting_mvpa_shiny.csv"),row.names = F)
+
+###############################################.
+## Part 2 - Prepare deprivation file ----
+###############################################.
+depr <- read.csv(paste0(data_folder, "Received Data/Physical Activity/MVPA/trend_data_simd_2012_2022.csv"))
+
+
+###############################################.
+## Part 3 - Prepare population groups file ----
+###############################################.
+
+#Aggregates - HB and CA reporting 4-year aggregate vs. single year figures here. 
+
+#read in data files
+Sex_split <- read.csv(paste0(data_folder, "Received Data/Physical Activity/MVPA/trend_data_sex_2012_2022.csv"))
+Age_split <- read.csv(paste0(data_folder, "Received Data/Physical Activity/MVPA/trend_data_age_2012_2022.csv"))
+Disability_split <- read.csv(paste0(data_folder, "Received Data/Physical Activity/MVPA/trend_data_ltc_2012_2022.csv"))
+
+#create data cleaning function
+data_cleaning <- function(df){
+  
+  df_name <- deparse(substitute(df)) #convert df name into a string to extract split name
+  
+  df <- df |> 
+    select(-c(2:3, 10)) |> #drop unnecessary cols
+    filter(Categories == "Meets recommendations") |>  #keep only rate of those meeting guidelines
+    rename(split_value = 3, #rename third column split_value
+           year = Year, #rename other cols to necessary format
+           rate = Percent,
+           numerator = Mean,
+           lowci = LowerCI,
+           upci = UpperCI) |> 
+    mutate(code = "S00000001", #create col with Scotland s-code
+           ind_id = c("88007"), #add indicator id
+           split_name = c(str_extract(df_name, "[^_]+")), #extract df name before underscore to populate split_name column
+           def_period = paste0(year, " survey year"),
+           trend_axis = year) |> 
+    select(c(8:10, 3, 1, 5, 4, 6:7, 11:12))
+  
+}
+
+#run data cleaning function
+Sex_split_cleaned <- data_cleaning(Sex_split)
+Age_split_cleaned <- data_cleaning(Age_split)
+Disability_split_cleaned <- data_cleaning(Disability_split)
+
+#join dfs for sex, age and disability
+meeting_mvpa_shiny_popgrp <- rbind(Sex_split_cleaned, Age_split_cleaned, Disability_split_cleaned)
+
+#save files
+saveRDS(meeting_mvpa_shiny_popgrp, file = paste0(data_folder, "Data to be checked/meeting_mvpa_shiny_popgrp.rds"))
+write.csv(meeting_mvpa_shiny_popgrp, file = paste0(data_folder, "Data to be checked/meeting_mvpa_shiny_popgrp.csv"),row.names = F)
+
