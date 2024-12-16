08cb32a66a80501004ac5a8aa88e1f8c9962603a
Monica McGibbon <monica.mcgibbon@phs.scot>
2024-12-16 10:23:51 +0000

    rerun of entire time period up to 2023/24

--- Healthy weight at birth.R
+++ Healthy weight at birth.R
@@ -12,11 +12,11 @@ source("2.deprivation_analysis.R") # deprivation function
 ###############################################.
 ## Part 1 - Prepare basefile ----
 ###############################################.
-birthweight <- read_csv(paste0(data_folder, "Received Data/Healthy birth weight/IR2024-00001_bwt.csv")) %>%
+birthweight <- read_csv(paste0(data_folder, "Received Data/Healthy birth weight/IR2024-00846_live_singleton_births.csv")) %>%
   setNames(tolower(names(.))) %>%   #variables to lower case
-  rename(year = finyear, datazone = datazone2011, numerator = numerator_normal) %>% 
-  mutate(year = substr(year, start=1, stop=4)) %>%  # Fyear coded by year ending Mar31. Change to match profiles. 
-  select(-numerator_low)
+  rename(datazone = datazone2011, numerator = fullterm_appropriate_weight, denominator = total_singleton_births) %>% 
+  mutate(year = substr(finyear, start=1, stop=4)) %>%  # Fyear coded by year ending Mar31. Change to match profiles. 
+  select(datazone, year, numerator, denominator)
 
 saveRDS(birthweight, file=paste0(data_folder, 'Prepared Data/healthy_birth_raw.rds'))
 
@@ -30,7 +30,7 @@ saveRDS(birthweight_dep, file=paste0(data_folder, 'Prepared Data/healthy_birth_d
 ###############################################.
 #CYP profile
 analyze_first(filename = "healthy_birth", geography = "datazone11", measure = "percent", 
-              yearstart = 2002, yearend = 2022, time_agg = 3)
+              yearstart = 2002, yearend = 2023, time_agg = 3)
 
 analyze_second(filename = "healthy_birth", measure = "percent", time_agg = 3, 
                ind_id = 21105, year_type = "financial")
