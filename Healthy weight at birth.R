7abfa4ff4974d87efe0f4bfb4357067ebb77be62
vaelliott <victoria.elliott@phs.scot>
2024-12-16 18:12:01 +0000

    adding some notes

--- Healthy weight at birth.R
+++ Healthy weight at birth.R
@@ -1,4 +1,8 @@
-# ScotPHO indicators: Healthy weight at birth
+# ScotPHO indicators: Healthy weight at birth (HWB and CYP profile)
+
+# Dec 2024: indicator definition was revised to define healthy weight as 'appropriate weight for gestational age'
+# ScotPHO apply restrictions to our indicator to remove impact of variation cause by things like prematurity/multiple births
+# Scotpho indicator only considers live singleton fullterm births - which may lead to differences with official births in scotland publication.
 
 #   Part 1 - Prepare basefile
 #   Part 2 - Run analysis functions
@@ -30,7 +34,7 @@ saveRDS(birthweight_dep, file=paste0(data_folder, 'Prepared Data/healthy_birth_d
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
-#CYP profile
+
 analyze_first(filename = "healthy_birth", geography = "datazone11", measure = "percent", 
               yearstart = 2002, yearend = 2023, time_agg = 3)
 
@@ -38,9 +42,10 @@ analyze_second(filename = "healthy_birth", measure = "percent", time_agg = 3,
                ind_id = 21105, year_type = "financial")
 
 #Deprivation analysis function
-#Deprivation function not applicable for this indicator
-#analyze_deprivation(filename="healthy_birth_depr", measure="percent", time_agg=3, 
-                    #yearstart= 2014, yearend=2022, year_type = "financial", ind_id = 21105)
+#Deprivation function could technically be run be results suggest limited difference between quintiles - possibly needs more investigation
+# into whether this information might be useful
+# analyze_deprivation(filename="healthy_birth_depr", measure="percent", time_agg=3, 
+#                     yearstart= 2014, yearend=2023, year_type = "financial", ind_id = 21105)
 
 ##END
 
