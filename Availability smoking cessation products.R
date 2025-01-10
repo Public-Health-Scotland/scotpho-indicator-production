37f61dab9a5023b295a22f8e7f084fae0eeaca9e
apigap01 <abigail.prescott@phs.scot>
2025-01-10 17:24:27 +0000

    Availability of smoking cessation products rework v1 2023/24

--- Availability smoking cessation products.R
+++ Availability smoking cessation products.R
@@ -12,32 +12,26 @@ source("1.indicator_analysis.R") #Normal indicator functions
 ## Part 1 - Create basefile ----
 ###############################################.
 #Reading data provided by Prescribing team
-data_products <- read_excel(paste0(data_folder, "Received Data/IR2022-00878_smoking_products.xlsx"), 
-                            sheet = "Output", range = cell_limits(c(8, 2), c(NA, 10))) %>% 
-  janitor::clean_names()
+data_products <- readRDS(paste0(data_folder, "Received Data/Availability of smoking cessation products/scotpho_smoking_ddds_20241127.rds")) |> 
+  clean_names() |> 
+  rename(datazone = datazone2011,
+         numerator = ddds,
+         year = financial_year) |> 
+  ungroup() |>
+  select(c(1,4:5))
 
-# Bringing code information for LA.
-ca_lookup <- readRDS(paste0(data_folder, "Lookups/Geography/CAdictionary.rds"))
-
-data_products <- left_join(data_products, ca_lookup, #Merging both
-                           by = c("dispensing_council_area" = "areaname")) %>% 
-  filter(!(is.na(code))) #excluding values without a valid ca
-
-# aggregate to get total DDDs for each datazone/year.
-data_products <- data_products %>% rename(year = financial_year, ca = code) %>% 
-  group_by(ca, year) %>% summarize(numerator = sum(defined_daily_doses, na.rm = T)) %>%  
-  mutate(numerator = numerator/365) %>%  # value is defined daily doses, so needs to be divided by 365.
-  ungroup() %>% select(ca, year, numerator) 
 
 saveRDS(data_products, file=paste0(data_folder, 'Prepared Data/cessation_products_raw.rds'))
 
 ###############################################.
 ## Part 3 - Run analysis functions ----
 ###############################################.
-analyze_first(filename = "cessation_products", geography = "council", measure = "crude", hscp = T,
-              yearstart = 2002, yearend = 2021, time_agg = 1, pop="CA_pop_12+")
+analyze_first(filename = "cessation_products", geography = "datazone11", measure = "crude", hscp = T, 
+              pop="DZ11_pop_12+", adp = F,
+              yearstart = 2002, yearend = 2023, time_agg = 1)
 
 analyze_second(filename = "cessation_products", measure = "crude", time_agg = 1,
                crude_rate = 1000, ind_id = 1544, year_type = "financial")
 
 ##END
+
