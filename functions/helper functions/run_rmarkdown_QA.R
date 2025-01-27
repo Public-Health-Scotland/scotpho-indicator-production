feac5c07eada10ea0ab971b0821c3026bbc700bf
Monica McGibbon <monica.mcgibbon@phs.scot>
2025-01-27 13:53:49 +0000

    ensure QA works with new function

---
+++ functions/helper functions/run_rmarkdown_QA.R
@@ -0,0 +1,7 @@
+run_qa <- function(filename, old_file="default", check_extras=c(), type = c("main", "deprivation")){
+  if(type == "main"){
+  run("3.Data Quality Checks.Rmd")
+  } else {
+    run("4.Data Quality Checks_inequalities indicators.Rmd")  
+  }
+}  
