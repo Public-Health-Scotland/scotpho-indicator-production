a5182294043fc18d9bee90bc989a57706e892265
Monica McGibbon <monica.mcgibbon@phs.scot>
2025-02-27 16:13:29 +0000

    move to new functions and add deprivation analysis

--- developmental concerns 27-30 months.R
+++ developmental concerns 27-30 months.R
@@ -1,4 +1,15 @@
-# ScotPHO indicators: DEVELOPMENTAL CONCERNS AT 27-30 MONTHS
+#######################.
+# Analyst notes ----
+#######################.
+
+# ScotPHO indicator: Developmental concerns at 27-30 weeks
+
+# Data is requested annually from the child health team following this release (typically published in April)
+# https://publichealthscotland.scot/publications/early-child-development/
+
+# When data is recieved save it in the 'Developmental Concerns subfolder within the Recieved data folder and
+# a. update the file-path to the data to be read in 
+# b. update the 'yearend' parameters in the analysis functions
 
 ## Part 1 - Format raw data ready for analysis functions 
 ## Part 2 - calling the analysis functions 
@@ -6,43 +17,52 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
+source("functions/main_analysis.R") #Normal indicator functions
+source("functions/deprivation_analysis.R") # deprivation function
+library(readxl)
+
 
 ###############################################.
 ## Part 1 - Prepare basefile ----
 ###############################################.
 
-# read in the data
-dev_concerns <- read_excel(paste0(data_folder, "Received Data/Developmental concerns/IR2024-00253_development27months.xlsx")) 
+# read in the data (update filepath when recieve new data )
+dev_concerns <- readxl::read_excel(file.path(profiles_data_folder, "Received Data/Developmental concerns/IR2024-00253_development27months.xlsx")) |>
+  janitor::clean_names()
 
-# tidy up col names
+
+# removes all other geographies apart from datazone (needed if received data contains Scotland and hb data)
 dev_concerns <- dev_concerns |> 
-  setNames(tolower(names(dev_concerns))) |> #sets all column names to lowercase
-  clean_names() |> 
-  rename(datazone = datazone2011,
-         fin_year = finyr_eligible) #renaming variables to datazone and fin_year
+  filter(!(is.na(datazone2011)) | hb_residence_desc == "Unknown")
 
 
-#removes all other geographies apart from datazone (needed if received data contains Scotland and hb data)
-dev_concerns <- dev_concerns |> 
-  filter(!(is.na(datazone))|hb_residence_desc=="Unknown")
-  
-dev_concerns <- dev_concerns |> 
-  mutate( #creates year field based on length of fin_year
-    year=substr(fin_year, start=1, stop=4)) |> 
-  group_by(year, datazone) |> 
-  summarise(numerator = sum(concerns), denominator = sum(reviews)) |> 
-  ungroup()
+# fix year column to be starting year of the financial year 
+dev_concerns <- dev_concerns |>
+  mutate(year = substr(finyr_eligible, start = 1, stop = 4))
 
-saveRDS(dev_concerns, file=paste0(data_folder, 'Prepared Data/dev_concerns_raw.rds')) 
+# summarise data per datazone and year/drop unwanted columns 
+dev_concerns <- dev_concerns |>
+  group_by(year, datazone2011) |>
+  summarise(
+    numerator = sum(concerns), 
+    denominator = sum(reviews),
+    .groups = "drop"
+    )
+
+# save temporary file to use in analysis functions
+saveRDS(dev_concerns, file = file.path(profiles_data_folder, 'Prepared Data/dev_concerns_raw.rds')) 
 
 ###############################################.
 ## Part 2 - calling analysis functions ----
 ###############################################.
 
-analyze_first(filename = "dev_concerns", geography = "datazone11", hscp = T, measure = "percent", 
-              yearstart = 2013, yearend = 2022, time_agg = 3)
+# create main file
+main_analysis(filename = "dev_concerns", ind_id = 13048, 
+              geography = "datazone11", measure = "percent", 
+              yearstart = 2013, yearend = 2022, time_agg = 3, year_type = "financial")
 
-analyze_second(filename = "dev_concerns", measure = "percent", time_agg = 3, 
-               ind_id = 13048, year_type = "financial")
+# create deprivation file 
+deprivation_analysis(filename = "dev_concerns", ind_id = 13048, measure = "percent", 
+                     yearstart = 2013, yearend = 2022, time_agg = 3, year_type = "financial")
+
+
