166f47809e8f7f165622b5f97dd8c40093d513ed
apigap01 <abigail.prescott@phs.scot>
2025-04-02 15:55:16 +0100

    Source data from opendata rather than using a data download

--- Domestic abuse.R
+++ Domestic abuse.R
@@ -17,44 +17,36 @@
 #sourcing analysis function
 source("./functions/main_analysis.R")
 
-#council area lookup
-ca_lookup <- readRDS(paste0(profiles_data_folder, "/Lookups/Geography/CAdictionary.rds"))
-
 ###############################################.
 ## Read in data  ----
 ###############################################.
 
-data <- readr::read_csv(paste0(profiles_data_folder, "/Received Data/Domestic Abuse/domestic_abuse.csv"),
-                        skip = 6, col_select = -1) |> #skipping first rows of metadata and first column of s-codes formatting incorrectly
-
+data <- opendatascot::ods_dataset("domestic-abuse-recorded-by-the-police-number-of-incidents-and-rates")  |> 
+  janitor::clean_names() |> 
+  
 ###############################################.
 ## Tidy up data  ----
 ###############################################.
 
-  #removing unneeded rows/cols
-  select(-c(2:5)) |>  #dropping early years of data to align with existing output
-  slice(-c(1)) |>  #dropping Scotland figs - to be re-added in function
-
-  #pivot longer
-  tidyr::pivot_longer(cols = c(2:22), names_to = "year", values_to = "numerator") |> #create 1 year column instead of 1 per year
-  mutate(numerator = as.numeric(numerator)) |>  #convert numerator from character to numeric
-  rename(areaname = `Reference Area`) #|> #rename to areaname to match lookup
-    
-#join data to lookup 
-data_cleaned <-  left_join(data, ca_lookup, by = "areaname") |> 
-  mutate(year = substr(year, 1, 4), #abbreviate fin year to first calendar year
-         year = as.numeric(year)) |> #convert year to numeric
-  select(4, 2:3) #select final columns
+filter(ref_area != "S9200000" & ref_area != "S92000003") |> #remove Scotland figs - to be re-added in analysis function
+filter(measure_type == "count") |> #drop ratio measure and keep count which forms numerator
+mutate(ref_period = substr(ref_period, 1, 4), #abbreviate fin year to first calendar year
+       ref_period = as.numeric(ref_period)) |> #convert to numeric from character
+rename(code = ref_area, #rename columns to format expected by analysis function
+       year = ref_period,
+       numerator = value) |> 
+filter(year > 2012) |> 
+select(-c(measure_type)) #drop count as no longer needed
 
 #save prepared data
-saveRDS(data_cleaned, file = paste0(profiles_data_folder, "/Prepared Data/domestic_abuse_raw.rds"))
+saveRDS(data, file = paste0(profiles_data_folder, "/Prepared Data/domestic_abuse_raw.rds"))
   
 ###############################################.
 ## Run analysis function
 ###############################################.
 
 main_analysis(filename = "domestic_abuse", measure = "crude", geography = "council", 
-              year_type = "financial", ind_id = 20804, time_agg = 1, yearstart = 2003,
+              year_type = "financial", ind_id = 20804, time_agg = 1, yearstart = 2013,
               yearend = 2023, pop = "CA_pop_allages", crude_rate = 10000)
 
 ##End.
