e5e93883a44b7caa41a56ee9bfcac95f09467c88
abigap01 <abigail.prescott@phs.scot>
2025-07-02 16:26:48 +0000

    Annual participation - rerun for 2024

--- Annual Participation Measure.R
+++ Annual Participation Measure.R
@@ -13,72 +13,42 @@
 
 # data source: https://www.skillsdevelopmentscotland.co.uk/publications-statistics/statistics/annual-participation-measure/
 
-
 ###############################################.
 ## Part 1 - Prepare data ----
 ###############################################.
 
-
 ###1.a load dependencies/functions ----
 
-source("1.indicator_analysis.R") 
-
-library("janitor") #for row_to_names() function 
-library("stringr")#for string_replace() function
-
+source("./functions/main_analysis.R") 
 
 ###1.b read in data ----
 
-dat <- read_xlsx(paste0(data_folder, "Received Data/annual-participation-measure-2023-supplementary-tables.xlsx"), sheet = "Table 1.7") #aps data
-
-ca <- readRDS(paste0(lookups,"Geography/CAdictionary.rds")) #council area lookup
-
-
+dat <- read_xlsx(paste0(profiles_data_folder, "/Received Data/Annual participation measure/annual-participation-measure-2024-supplementary-tables.xlsx"), sheet = "Table 1.7") #aps data
 
 ###1.c clean data ----
 
-#select required columns
-dat <- dat %>%
-  tail(-7) %>% #remove metadata from top of spreadsheet
-  row_to_names(row_number = 1) %>% #set 1st row as column names
-  setNames(tolower(names(.))) %>%
-  select(year, `local authority`, `total cohort (16-19)`, `participating (16-19)`)
-
-
-
-#rename council areas and match with ca lookup
-dat <- dat %>%
-  mutate(`local authority` = str_replace(`local authority`, "&","and"),
-         `local authority` = str_replace(`local authority`, "Edinburgh City","City of Edinburgh"),
-         `local authority` = str_replace(`local authority`, "Eilean Siar","Na h-Eileanan Siar")) %>%
-  filter(`local authority` != "Scotland") %>%
-  left_join(ca, by = c("local authority" = "areaname"))
-
-
-#final formatting of data to ensure works with analyse functions 
-  dat <- dat %>%
-    mutate(across(contains(c("16-19")), as.numeric)) %>%
-    rename("ca" = "code",
-         "denominator" = "total cohort (16-19)",
-         "numerator" = "participating (16-19)") %>%
-  select(year, ca, numerator, denominator) 
-
-
+dat_cleaned <- dat |> 
+  slice(8:305) |>  #remove metadata from top and bottom of spreadsheet
+  row_to_names(row_number = 1) |>  #set 1st row as column names
+  clean_names() |> #tidy up column names
+  select(year, council_area_2019_code, participating_16_19, total_cohort_16_19) |> #drop unnecessary variables
+  rename(numerator = participating_16_19,
+         denominator = total_cohort_16_19,
+         ca = council_area_2019_code) |> #rename columns to format expected by analysis functions
+  mutate(across(c(year, numerator, denominator), as.numeric)) |>  #convert numerator and denominator from string to numeric
+  filter(!is.na(ca)) #drop Scotland totals which are showing as NA codes - will be re-added by analysis functions
 
 #1.d. Save file to be used in part 2 ----
-  saveRDS(dat, file=paste0(data_folder, 'Prepared Data/participation_raw.rds'))
-
+  saveRDS(dat_cleaned, file=paste0(profiles_data_folder, '/Prepared Data/participation_raw.rds'))
 
 
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
   
-  analyze_first(filename = "participation", geography = "council", measure = "percent",  
-                yearstart = 2015, yearend = 2023, time_agg = 1)
-  
-  
-  analyze_second(filename = "participation", measure = "percent", 
-                 time_agg = 1, ind_id = "13053", year_type = "calendar")
-  
+ main_analysis(filename = "participation", measure = "percent", geography = "council",
+               year_type = "calendar", ind_id = 13053, time_agg = 1, yearstart = 2016,
+               yearend = 2024)
 
+
+#End.
