ead628a788091918d64d7f602f04ddae3ec3b29a
abigap01 <abigail.prescott@phs.scot>
2025-07-24 16:19:11 +0000

    devp concerns 23/24 update

--- developmental concerns 27-30 months.R
+++ developmental concerns 27-30 months.R
@@ -6,20 +6,19 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
+source("./functions/main_analysis.R") #Normal indicator functions
+source("./functions/deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Prepare basefile ----
 ###############################################.
 
 # read in the data
-dev_concerns <- read_excel(paste0(data_folder, "Received Data/Developmental concerns/IR2024-00253_development27months.xlsx")) 
+dev_concerns <- read_excel(file.path(profiles_data_folder, "/Received Data/Developmental concerns/IR2025-00535.xlsx")) 
 
 # tidy up col names
 dev_concerns <- dev_concerns |> 
-  setNames(tolower(names(dev_concerns))) |> #sets all column names to lowercase
-  clean_names() |> 
+  janitor::clean_names() |> #sets all column names to lowercase
   rename(datazone = datazone2011,
          fin_year = finyr_eligible) #renaming variables to datazone and fin_year
 
@@ -35,14 +34,13 @@ dev_concerns <- dev_concerns |>
   summarise(numerator = sum(concerns), denominator = sum(reviews)) |> 
   ungroup()
 
-saveRDS(dev_concerns, file=paste0(data_folder, 'Prepared Data/dev_concerns_raw.rds')) 
+saveRDS(dev_concerns, file=file.path(profiles_data_folder, '/Prepared Data/dev_concerns_raw.rds')) 
 
 ###############################################.
 ## Part 2 - calling analysis functions ----
 ###############################################.
 
-analyze_first(filename = "dev_concerns", geography = "datazone11", hscp = T, measure = "percent", 
-              yearstart = 2013, yearend = 2022, time_agg = 3)
+main_analysis(filename = "dev_concerns", geography = "datazone11", measure = "percent", 
+              yearstart = 2013, yearend = 2023, time_agg = 3, year_type = "financial",
+              ind_id = 13048)
 
-analyze_second(filename = "dev_concerns", measure = "percent", time_agg = 3, 
-               ind_id = 13048, year_type = "financial")
