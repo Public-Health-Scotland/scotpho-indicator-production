ba5474e4acc53a2e337665207bc55313dba2d943
elizabeth-richardson <elizabeth.richardson1@phs.scot>
2025-08-11 14:19:47 +0000

    Simplifying by using main_analysis, which also produces estimates for geogs higher than CA. Using new popgrps QA.

--- School leaver positive destinations.R
+++ School leaver positive destinations.R
@@ -1,10 +1,11 @@
 ## notes ----
 
 # this script produces data for indicator 13030 - school leavers from publicly funded secondary schools in positive destinations, 9 months after leaving school
-# Summary Statistics for Attainment and Initial Leaver Destinations, No. 6: 2024 Edition, released on 27th February 2024
+# Summary Statistics for Attainment and Initial Leaver Destinations, 2025 Edition, released on 25th Feb 2025
 # https://www.gov.scot/news/school-leaver-attainment-and-destinations-8/
 # Script updated in Nov 2024 (by ER) to import more population group splits from the data file (previously only Scotland and CA data were imported)
 # Script updated in March 2025 (by ER) to use the new indicator production paths and functions.
+# Script updated Aug 2025 (by ER) to aggregate to additional geogs
 
 
 ###############################################.
@@ -31,20 +32,10 @@ file <- "SSAILD+2025+supplementary+tables+L1.6+correction.xlsx"
 # council area lookup
 ca <- readRDS(paste0(profiles_lookups,"/Geography/CAdictionary.rds")) 
 
-# ca to hb lookup
-geo_lookup <- readRDS(paste0(profiles_lookups, "/Geography/DataZone11_All_Geographies_Lookup.rds")) |>
-  select(ca2019, hb2019)
-geo_lookup %<>% distinct %>% rename(ca = ca2019, hb = hb2019)
-
 
 ###############################################.
-## Part 2 - Import data ----
+## Part 2 - Import and process Scotland data ----
 ###############################################.
-
-
-
-# National:
-###############
 
 # Scotland data, totals
 scot_series <- read.xlsx(paste0(schdest_data_folder, file),
@@ -59,8 +50,8 @@ scot_series <- read.xlsx(paste0(schdest_data_folder, file),
          denominator = `Number.of.Leavers`,
          numerator = round((rate * `Number.of.Leavers`)/100)) %>%
   select(trend_axis, numerator, denominator, rate) %>%
-  mutate(split_name = "None",
-         split_value = "None")
+  mutate(split_name = "Total",
+         split_value = "Total")
 
 # Scotland data, by population groups
 
@@ -114,7 +105,14 @@ scot_all <- rbind(scot_series,
                   schdest_sex,
                   schdest_urbrur,
                   schdest_simd) %>%
-  mutate(code = "S00000001") # add code for Scotland
+  mutate(code = "S00000001", # add code for Scotland
+         ind_id = 13010,
+         year = as.numeric(substr(trend_axis, 1, 4)),
+         trend_axis = gsub("-", "/", trend_axis),
+         def_period = paste0("School year (", trend_axis, ")")) %>%
+  rename(rate_orig = rate) %>% #checked and it's a perfect match with calculated one, so can drop
+  calculate_percent() %>%
+  select(-c(denominator, rate_orig))
 
 # make population file for school leavers by year and SIMD quintile, for inequalities metrics:
 depr_pop_schoolleavers <- schdest_simd %>%
@@ -125,135 +123,106 @@ depr_pop_schoolleavers <- schdest_simd %>%
 
 saveRDS(depr_pop_schoolleavers, paste0(profiles_lookups, "/Population/depr_pop_schoolleavers.rds"))
 
-# Council area:
-##################
+###############################################.
+## Part 3 - Import and process CA data ----
+###############################################.
 
 # Council area data, totals
 ca_series <- read.xlsx(paste0(schdest_data_folder, file),
                        sheet = "L2.1",
                        colNames = TRUE,
                        startRow = 6) %>%
-  rename(trend_axis = Year) %>%
   filter(LA.Name!="Scotland") %>% # longer time series in the scot_series data
   mutate(across(everything(), ~replace(., . %in% c("[c]", "[z]", "[low]", "S"), NA)), #replace suppression symbols with NA
+         year = as.numeric(substr(Year, 1, 4)),
          rate = as.numeric(`Positive.Destination`),
          numerator = round((rate * `Number.of.leavers`)/100),
          areaname = str_replace(LA.Name, "Edinburgh, City of","City of Edinburgh"),
          areaname = str_replace(areaname, "&","and")) %>%
   merge(ca, by = "areaname") %>% # join with council area lookup
-  select(trend_axis, code, numerator, denominator = `Number.of.leavers`, rate) %>%
-  mutate(split_name = "None",
-         split_value = "None")
+  select(year, code, numerator, denominator = `Number.of.leavers`) 
 
-# aggregate to HB level
-hb_series <- ca_series %>%
-  merge(y=geo_lookup, by.x="code", by.y="ca") %>%
-  group_by(hb, trend_axis) %>%
-  summarise(numerator = sum(numerator, na.rm=TRUE),
-            denominator = sum(denominator, na.rm=TRUE)) %>%
-  ungroup() %>%
-  mutate(rate = 100 * numerator/denominator,
-         split_name = "None",
-         split_value = "None") %>%
-  rename(code = hb)
+# Save ready for the main analysis function
+saveRDS(ca_series, file=paste0(profiles_data_folder, '/Prepared Data/positive_destinations_ca_raw.rds'))
 
+# Run main analysis function to aggregate and calculate rates (CA to higher geogs)
+main_analysis(filename = "positive_destinations_ca", ind_id = 13010, 
+              geography = "council", measure = "percent", 
+              yearstart = 2009, yearend = 2023,
+              time_agg = 1, year_type = "school")
 
-# Combine all:
-###################
+# Aggregated Scotland data do not equal the original Scotland data from the spreadsheet (likely due to some suppression), so drop the aggregated data for Scotland:
+ca_data <- readRDS(file.path(profiles_data_folder, "Data to be checked", "positive_destinations_ca_shiny.rds") ) %>%
+  filter(code != "S00000001")
 
-all_data <- rbind(scot_all, ca_series, hb_series) %>%
-  mutate(ind_id = 13010,
-         year = substr(trend_axis, 1, 4),
-         trend_axis = gsub("-", "/", trend_axis),
-         def_period = paste0("School year (", trend_axis, ")")) %>%
-  # confidence intervals
-  mutate(ci_wald = 100 * (1.96*sqrt(((rate/100)*(1-(rate/100)))/denominator)), # Wald method. 
-         lowci = rate - ci_wald,
-         upci = rate + ci_wald) %>%
-  select(-ci_wald, -denominator)
-
-
+# Also, NA (suppressed) in original data have been replaced by zeroes during the main_analysis function, so need to revert to NA:
+ca_data <- ca_data %>%
+  mutate(upci = ifelse(numerator==0, as.numeric(NA), upci),
+         lowci = ifelse(numerator==0, as.numeric(NA), lowci),
+         rate = ifelse(numerator==0, as.numeric(NA), rate),
+         numerator = ifelse(numerator==0, as.numeric(NA), numerator))
 
 ##########################################################
 ### Part 3 - Prepare final files -----
 ##########################################################
 
+# 1 - main data: (combines Scotland data (since 1992/93) with data derived from CA level (from 2009). The mismatch is OK for the dashboard)
+main_data <- scot_all %>%
+  filter(split_name == "Total") %>%
+  select(-starts_with("split")) %>%
+  rbind(ca_data) %>%
+  arrange(code, year)
 
-# Function to prepare final files: main_data and popgroup
-prepare_final_files <- function(ind){
-  
-  # 1 - main data (ie data behind summary/trend/rank tab)
-  main_data <- all_data %>%
-    filter(split_name == "None") %>%
-    select(code, ind_id, year,
-           numerator, rate, upci, lowci,
-           def_period, trend_axis) %>%
-    unique() %>%
-    arrange(code, year)
-  
-  # save to folder that QA script accesses:
-  write_rds(main_data, paste0(profiles_data_folder, "/Data to be checked/", ind, "_shiny.rds"))
-  write.csv(main_data, paste0(profiles_data_folder, "/Data to be checked/", ind, "_shiny.csv"), row.names = FALSE) 
-  
-  # 2 - population groups data (ie data behind population groups tab)
-  pop_grp_data <- all_data %>%
-    filter(!(split_name %in% c("None", "Deprivation (SIMD)"))) %>%
-    select(code, ind_id, year, numerator, rate, upci,
-           lowci, def_period, trend_axis, split_name, split_value) %>%
-    arrange(code, year)
-  
-  # Save
-  write_rds(pop_grp_data, paste0(profiles_data_folder, "/Data to be checked/", ind, "_shiny_popgrp.rds"))
-  write.csv(pop_grp_data, paste0(profiles_data_folder, "/Data to be checked/", ind, "_shiny_popgrp.csv"), row.names = FALSE) 
-  
-  # 3 - SIMD data (ie data behind deprivation tab)
-  
-  # Process SIMD data
-  simd_data <- all_data %>%
-    filter(split_name == "Deprivation (SIMD)") %>%
-    unique() %>%
-    select(-split_name) %>%
-    rename(quintile = split_value) %>%
-    mutate(quint_type = "sc_quin") %>%
-    arrange(code, year, quintile)
-  
-  # get arguments for the add_population_to_quintile_level_data() function: (done because the ind argument to the current function is not the same as the ind argument required by the next function)
-  ind_name <- ind # dataset will already be filtered to a single indicator based on the parameter supplied to 'prepare final files' function
-  ind_id <- unique(simd_data$ind_id) # identify the indicator number 
-  
-  # add population data (quintile level) so that inequalities can be calculated
-  simd_data <-  simd_data|>
-    add_population_to_quintile_level_data(pop="depr_pop_schoolleavers", # the population file created above
-                                          ind = ind_id, ind_name = ind_name) |>
-    filter(!is.na(rate)) # not all years have data
-  
-  # calculate the inequality measures
-  simd_data <- simd_data |>
-    calculate_inequality_measures() |> # call helper function that will calculate sii/rii/paf
-    select(-c(overall_rate, total_pop, proportion_pop, most_rate,least_rate, par_rr, count)) #delete unwanted fields
-  
-  # save the data as RDS file
-  saveRDS(simd_data, paste0(profiles_data_folder, "/Data to be checked/", ind, "_ineq.rds"))
-  
-  # Make data created available outside of function so it can be visually inspected if required
-  main_data_result <<- main_data
-  pop_grp_data_result <<- pop_grp_data
-  simd_data_result <<- simd_data
-  
-  
-}
+# save to folder that QA script accesses:
+write_rds(main_data, paste0(profiles_data_folder, "/Data to be checked/school_leaver_destinations_shiny.rds"))
+write.csv(main_data, paste0(profiles_data_folder, "/Data to be checked/school_leaver_destinations_shiny.csv"), row.names = FALSE) 
 
+# 2 - population groups data (ie data behind population groups tab)
+pop_grp_data <- scot_all %>%
+  filter(!(split_name %in% c("Total", "Deprivation (SIMD)"))) %>%
+  select(code, ind_id, year, numerator, rate, upci,
+         lowci, def_period, trend_axis, split_name, split_value) %>%
+  arrange(code, year)
 
-# Run function to create final files
-prepare_final_files(ind = "school_leaver_destinations")
+# Save
+write_rds(pop_grp_data, paste0(profiles_data_folder, "/Data to be checked/school_leaver_destinations_shiny_popgrp.rds"))
+write.csv(pop_grp_data, paste0(profiles_data_folder, "/Data to be checked/school_leaver_destinations_shiny_popgrp.csv"), row.names = FALSE) 
 
+# 3 - SIMD data (ie data behind deprivation tab)
 
+# Process SIMD data
+simd_data <- scot_all %>%
+  filter(split_name == "Deprivation (SIMD)") %>%
+  unique() %>%
+  select(-split_name) %>%
+  rename(quintile = split_value) %>%
+  mutate(quint_type = "sc_quin") %>%
+  arrange(code, year, quintile)
+
+# add population data (quintile level) so that inequalities can be calculated
+simd_data <-  simd_data|>
+  add_population_to_quintile_level_data(pop="depr_pop_schoolleavers", # the population file created above
+                                        ind = 13010, ind_name = "school_leaver_destinations") |>
+  filter(!is.na(rate)) # not all years have data
+
+# calculate the inequality measures
+simd_data <- simd_data |>
+  calculate_inequality_measures() |> # call helper function that will calculate sii/rii/paf
+  select(-c(overall_rate, total_pop, proportion_pop, most_rate,least_rate, par_rr, count)) #delete unwanted fields
+
+# save the data as RDS file
+saveRDS(simd_data, paste0(profiles_data_folder, "/Data to be checked/school_leaver_destinations_ineq.rds"))
+
+  
 # Run QA reports
 ####################
 
 # main data:
 run_qa(type = "main", filename = "school_leaver_destinations", test_file = FALSE) # no historic file
 # Orkney has no data for latest year: plots as zero in QA file, but won't plot in the app I think
+
+# ineq data:
+run_qa(type = "popgrp", filename = "school_leaver_destinations", test_file=FALSE)
 
 # ineq data:
 run_qa(type = "deprivation", filename = "school_leaver_destinations", test_file=FALSE)
