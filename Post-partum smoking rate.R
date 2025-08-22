ab2b796ffa1f44b5606f376c7198dccabd5b7122
abigap01 <abigail.prescott@phs.scot>
2025-08-22 14:20:12 +0000

    Trialling adding deprivation splits

--- Post-partum smoking rate.R
+++ Post-partum smoking rate.R
@@ -6,39 +6,49 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("1.indicator_analysis.R") #Normal indicator functions
+source("./functions/main_analysis.R") #Normal indicator functions
+source("./functions/deprivation_analysis.R") #Deprivation function
 
 ###############################################.
 ## Part 1 - Prepare basefile ----
 ###############################################.
 
 #Data comes from child health team
-postpartum <- read_csv(file=paste0(data_folder, 'Received Data/Post-partum Smoking/IR2025-00008.csv')) %>% 
-  setNames(tolower(names(.))) %>% #set variables to lower case
+postpartum <- read.csv(file.path(profiles_data_folder, '/Received Data/Post-partum Smoking/IR2025-00008.csv')) |>  
+  janitor::clean_names() |> 
   mutate(year=case_when(nchar(fin_year)==3 ~ paste0("200",substr(fin_year,1,1)), 
                    TRUE ~ paste0("20",substr(fin_year,1,2)))) #format year to display financial year
 
-# bringing lookup to match with council
-ca_lookup <- readRDS(paste0(lookups, "Geography/DataZone11_All_Geographies_Lookup.rds")) %>%
-  select(datazone2011, ca2019) %>% distinct()
+# # bringing lookup to match with council
+# ca_lookup <- readRDS(paste0(lookups, "Geography/DataZone11_All_Geographies_Lookup.rds")) %>%
+#   select(datazone2011, ca2019) %>% distinct()
+# 
+# postpartum <- left_join(postpartum, ca_lookup, by = "datazone2011") %>% 
+#   rename(ca = ca2019 ) %>% group_by(ca, year) %>% 
+#   summarise(numerator = sum(smoker), denominator = sum(total_valid_status)) %>% 
+#   ungroup() %>% 
+#   # Selecting out a few cases from early years in Highland CA before the system was 
+#   # properly in place that would cause confusion
+#   filter(!(ca == "S12000017" & year<2007))
 
-postpartum <- left_join(postpartum, ca_lookup, by = "datazone2011") %>% 
-  rename(ca = ca2019 ) %>% group_by(ca, year) %>% 
-  summarise(numerator = sum(smoker), denominator = sum(total_valid_status)) %>% 
-  ungroup() %>% 
-  # Selecting out a few cases from early years in Highland CA before the system was 
-  # properly in place that would cause confusion
-  filter(!(ca == "S12000017" & year<2007))
 
-saveRDS(postpartum, file=paste0(data_folder, 'Prepared Data/postpartum_smoking_raw.rds'))
+postpartum <- postpartum |> 
+  rename(code = datazone2011, 
+         numerator = smoker,
+         denominator = total_valid_status) |> 
+  select(-fin_year) |> 
+  mutate(year = as.numeric(year))
+
+saveRDS(postpartum, file.path(profiles_data_folder, '/Prepared Data/postpartum_smoking_raw.rds'))
 
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
-analyze_first(filename = "postpartum_smoking", geography = "council", hscp = T,
-              measure = "percent", yearstart = 2002, yearend = 2023, time_agg = 3)
+main_analysis(filename = "postpartum_smoking", geography = "datazone11", measure = "percent",
+              yearstart = 2002, yearend = 2023, time_agg = 3, ind_id = 1552, year_type = "financial")
 
-analyze_second(filename = "postpartum_smoking", measure = "percent", time_agg = 3, 
-               ind_id = 1552, year_type = "financial")
+deprivation_analysis(filename = "postpartum_smoking", yearstart = 2002, yearend = 2023,
+                       time_agg = 3, year_type = "financial", measure = "percent", pop_sex = "all",
+                       ind_id = 1552)
 
 ##END
