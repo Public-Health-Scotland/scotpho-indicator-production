b67d81a44cbd16cf3c4079f4aa1ff62144ade7f0
elizabeth-richardson <elizabeth.richardson1@phs.scot>
2025-10-02 16:21:31 +0000

    Updated data now in rds format, and at datazone level. Script simplified as a result.

--- Alc-related hosp stays - mental and behav.R
+++ Alc-related hosp stays - mental and behav.R
@@ -9,8 +9,8 @@
 
 # Whether rate of patients or stays was not specified. I opted to use stays, as this is the headline stat the ARHS team use in their publications. 
 
-# Data provided by Scott Kilgariff, mailto:phs.alcohol@phs.scot, in Sept 2024
-# Single xlsx containing rates and CIs: IR2024-00519-MBD_final.xlsx
+# Data provided by Scott Kilgariff, mailto:phs.alcohol@phs.scot, in Sept 2025
+# rds file at DZ11 level
 
 # Please see attached for the data you have requested for patients/stays from 16 years and older.
 # •	For the SIMD quintile figures I have used a top age group of 85+ whilst for the rest of the figures I have used a top age group of 90+ when calculating EASR.
@@ -28,166 +28,230 @@
 # •	o_upper is the higher end of the 95% CI for the number of observed cases
 
 
-# Load in the packages
+##########################################################
+### Functions/packages -----
+##########################################################
 
-### functions/packages -----
-source("1.indicator_analysis.R")
-source("2.deprivation_analysis.R")
-
-# Load additional packages
-library(openxlsx)
-
-### 1. Read in data ----
-
-# Identify data folder
-arhs_data_folder <- paste0(data_folder, "Received Data/Alc-related hosp stays - mental and behavioural disorders/")
-file <- "IR2024-00519-MBD_final.xlsx"
+source("functions/main_analysis.R") # for packages and QA function 
+source("functions/deprivation_analysis.R") # for packages and QA
 
 
-# Import PHS ARHS data for MBD indicator
+##########################################################
+### Read in data -----
+##########################################################
 
-stays_sex <- read.xlsx(paste0(arhs_data_folder, file),
-                                sheet = "stays_by_sex",
-                                colNames = TRUE) %>%
-  mutate(split_name = "Sex") %>%
-  select(year, split_name,
-         numerator= observed,
-         code=SGcode,
-         rate=easr_rate,
-         lowci=easr_lower,
-         upci=easr_upper,
-         split_value=sex_label) 
+# File location
+arhs_data_folder <- paste0(profiles_data_folder, "/Received Data/Alc-related hosp stays - mental and behavioural disorders/")
+file <- "IR2025-00668-output.RDS"
 
-stays_sex_hb <- read.xlsx(paste0(arhs_data_folder, file),
-                            sheet = "stays_by_sex_hb",
-                            colNames = TRUE) %>%
-  mutate(split_name = "Sex") %>%
-  select(year, split_name,
-         numerator= observed,
-         code=SGcode,
-         rate=easr_rate,
-         lowci=easr_lower,
-         upci=easr_upper,
-         split_value=sex_label) 
+arhs <- readRDS(paste0(arhs_data_folder, file)) %>%
+  mutate(sex_grp = ifelse(sex=="Female", 2, 1)) %>%
+  select(year, datazone2011, sex_grp, age_grp, numerator = stays)
 
-stays_sex_ca <- read.xlsx(paste0(arhs_data_folder, file),
-                            sheet = "stays_by_sex_ca",
-                            colNames = TRUE) %>%
-  mutate(split_name = "Sex") %>%
-  select(year, split_name,
-         numerator= observed,
-         code=SGcode,
-         rate=easr_rate,
-         lowci=easr_lower,
-         upci=easr_upper,
-         split_value=sex_label) 
 
-stays <- rbind(stays_sex,
-               stays_sex_ca,
-               stays_sex_hb) %>%
-  mutate(split_value = case_when(split_value=="Both" ~ "Total", TRUE ~ split_value)) %>% 
-  mutate(trend_axis = as.character(year),
-         def_period = paste0("Calendar year (", year, ")"),
-         ind_id=30006)
-  
-stays_sex_simd <- read.xlsx(paste0(arhs_data_folder, file),
-                            sheet = "stays_by_sex_simd",
-                            colNames = TRUE) %>%
-  mutate(code="S00000001") %>% #all are Scotland
-  select(year, 
-         numerator= observed,
-         code,
-         rate=easr_rate,
-         lowci=easr_lower,
-         upci=easr_upper,
-         quintile=SIMDquintile,
-         sex=sex_label) %>%
-  mutate(sex = case_when(sex=="Both" ~ "Total", TRUE ~ sex),
-         trend_axis = as.character(year),
-         def_period = paste0("Calendar year (", year, ")"),
-         ind_id=30006,
-         quint_type = "sc_quin",
-         quintile = as.character(quintile))
-# get SIMD totals to add to the quintile data
-stays_simd <- stays_sex_simd %>%
-  group_by(year, sex, code) %>%
-  summarise() %>% # gives the breakdowns we need totals for
-  ungroup() %>%
-  merge(y=stays, by.x=c("year", "code", "sex"), by.y=c("year", "code", "split_value")) %>%
-  mutate(quint_type="sc_quin",
-         quintile="Total") %>%
-  select(-split_name) %>%
-  rbind(stays_sex_simd)
-  
+# Save the raw data for feeding to the processing functions
+
+# Total population
+saveRDS(arhs, file.path(profiles_data_folder, "Prepared Data", "ARHS_mental_and_behav_raw.rds"))
+
+# Subsets by sex
+arhs_m <- arhs %>% filter(sex_grp==1)
+saveRDS(arhs_m, file.path(profiles_data_folder, "Prepared Data", "ARHS_mental_and_behav_M_raw.rds"))
+
+arhs_f <- arhs %>% filter(sex_grp==2)
+saveRDS(arhs_f, file.path(profiles_data_folder, "Prepared Data", "ARHS_mental_and_behav_F_raw.rds"))
 
 
 
 ##########################################################
-### 3. Prepare final files -----
+### Main analysis (used for pop group files too) -----
+##########################################################
+
+# Run main_analysis to produce total population rates
+main_analysis(filename="ARHS_mental_and_behav",
+                            measure = "stdrate",
+                            geography = "datazone11",
+                            year_type = "calendar",
+                            ind_id = 30006, 
+                            time_agg = 1, 
+                            yearstart=2002, 
+                            yearend=2023, 
+                            pop = "DZ11_pop_16+", 
+                            epop_total = 165800, # 82900 x 2 sexes if starts at 16y
+                            epop_age = "16+", # waiting for Scott to confirm
+                            test_file = FALSE, 
+                            QA = TRUE, 
+                            police_div = TRUE)
+
+popgrp_total <- main_analysis_result %>%
+  mutate(split_name = "Sex",
+         split_value = "Total")
+
+# Run main_analysis to produce rates by sex
+main_analysis(filename="ARHS_mental_and_behav_M",
+              measure = "stdrate",
+              geography = "datazone11",
+              year_type = "calendar",
+              ind_id = 30006, 
+              time_agg = 1, 
+              yearstart=2002, 
+              yearend=2023, 
+              pop = "DZ11_pop_16+", 
+              epop_total = 82900, # if starts at 16y
+              epop_age = "16+", # waiting for Scott to confirm
+              test_file = FALSE, 
+              QA = TRUE, 
+              police_div = TRUE)
+
+popgrp_m <- main_analysis_result %>%
+  mutate(split_name = "Sex",
+         split_value = "Male")
+
+main_analysis(filename="ARHS_mental_and_behav_F",
+              measure = "stdrate",
+              geography = "datazone11",
+              year_type = "calendar",
+              ind_id = 30006, 
+              time_agg = 1, 
+              yearstart=2002, 
+              yearend=2023, 
+              pop = "DZ11_pop_16+", 
+              epop_total = 82900, # if starts at 16y
+              epop_age = "16+", # waiting for Scott to confirm
+              test_file = FALSE, 
+              QA = TRUE, 
+              police_div = TRUE)
+
+popgrp_f <- main_analysis_result %>%
+  mutate(split_name = "Sex",
+         split_value = "Female")
+
+
+# Combine data
+stays <- rbind(popgrp_total,
+               popgrp_f,
+               popgrp_m) %>%
+# remove small geographies
+  filter(!(substr(code, 1, 3) %in% c("S02", "S99"))) #IZs and localities, these have some counts < 5
+# now smallest numerator is 15
+
+
+
+##########################################################
+### Deprivation analysis -----
+##########################################################
+
+# Total population
+deprivation_analysis(filename="ARHS_mental_and_behav", 
+                                 time_agg = 1, 
+                                 yearstart=2002, 
+                                 yearend=2023,
+                                 year_type = "calendar",
+                                 measure = "stdrate",
+                                 pop_sex = "all", # c("male", "female", "all")
+                                 epop_total = 165800, # if starts at 16y, both sexes
+                                 epop_age = "16+", # waiting for Scott to confirm
+                                 pop_age = c(16, 150), # wait for age confirmation
+                                 ind_id = 30006, 
+                                 QA = TRUE, 
+                                 test_file = FALSE)
+
+arhs_simd_total <- deprivation_analysis_result %>%
+  mutate(sex = "Total")
+
+
+# Male population
+deprivation_analysis(filename="ARHS_mental_and_behav_M", 
+                     time_agg = 1, 
+                     yearstart=2002, 
+                     yearend=2023,
+                     year_type = "calendar",
+                     measure = "stdrate",
+                     pop_sex = "male", # c("male", "female", "all")
+                     epop_total = 82900, # if starts at 16y, both sexes
+                     epop_age = "16+", # waiting for Scott to confirm
+                     pop_age = c(16, 150), # wait for age confirmation
+                     ind_id = 30006, 
+                     QA = TRUE, 
+                     test_file = FALSE)
+
+arhs_simd_m <- deprivation_analysis_result %>%
+  mutate(sex = "Male")
+
+# Female population
+deprivation_analysis(filename="ARHS_mental_and_behav_F", 
+                     time_agg = 1, 
+                     yearstart=2002, 
+                     yearend=2023,
+                     year_type = "calendar",
+                     measure = "stdrate",
+                     pop_sex = "female", # c("male", "female", "all")
+                     epop_total = 82900, # if starts at 16y, both sexes
+                     epop_age = "16+", # waiting for Scott to confirm
+                     pop_age = c(16, 150), # wait for age confirmation
+                     ind_id = 30006, 
+                     QA = TRUE, 
+                     test_file = FALSE)
+
+arhs_simd_f <- deprivation_analysis_result %>%
+  mutate(sex = "Female")
+
+
+# Combine data
+stays_simd <- rbind(arhs_simd_total,
+                    arhs_simd_f,
+                    arhs_simd_m) 
+# smallest numerators for non-Scotland geogs are 1-4.
+# Smallest numerator for Scotland is 590 
+
+# NEED DZ01 TO PRODUCE DEP DATA BACK FURTHER THAN 2014
+# SEND DATA TO SCOTT BEFORE PUBLISHING
+
+
+##########################################################
+### Prepare final files -----
 ##########################################################
 
 
-# Function to prepare final files: 
-prepare_final_files <- function(ind){
-  
-  
-  # 1 - main data (ie data behind summary/trend/rank tab)
-  # Contains Scotland, LA and HB data, total pop
-  main_data <- stays %>% 
-    filter(split_value == "Total") %>% 
-    select(code, ind_id, year, 
-           numerator, rate, upci, lowci, 
-           def_period, trend_axis) %>%
-    unique() 
-  
-  write.csv(main_data, paste0(data_folder, "Test Shiny Data/", ind, "_shiny.csv"), row.names = FALSE)
-  write_rds(main_data, paste0(data_folder, "Test Shiny Data/", ind, "_shiny.rds"))
-  # save to folder that QA script accesses:
-  write_rds(main_data, paste0(data_folder, "Data to be checked/", ind, "_shiny.rds"))
-  
-  # 2 - population groups data (ie data behind population groups tab)
-  # Contains Scotland, LA and HB data by sex (including total)
-  pop_grp_data <- stays %>% # includes the contents of main data, as the 'total' for each split
-    select(code, ind_id, year, numerator, rate, upci, 
-           lowci, def_period, trend_axis, split_name, split_value,) 
-  
-  # Save
-  write.csv(pop_grp_data, paste0(data_folder, "Test Shiny Data/", ind, "_shiny_popgrp.csv"), row.names = FALSE)
-  write_rds(pop_grp_data, paste0(data_folder, "Test Shiny Data/", ind, "_shiny_popgrp.rds"))
-  # save to folder that QA script accesses: (though no QA for popgroups files?)
-  write_rds(pop_grp_data, paste0(data_folder, "Data to be checked/", ind, "_shiny_popgrp.rds"))
-  
-  
-  # 3 - SIMD data (ie data behind deprivation tab)
-  # Contains Scotland data by SIMD quintile 
-  
-  # Process SIMD data
+# 1 - main data (ie data behind summary/trend/rank tab)
 
-  # Save intermediate SIMD file
-  write_rds(stays_simd, file = paste0(data_folder, "Prepared Data/", ind, "_shiny_depr_raw.rds"))
-  write.csv(stays_simd, file = paste0(data_folder, "Prepared Data/", ind, "_shiny_depr_raw.csv"), row.names = FALSE)
-  
-  #get ind_id argument for the analysis function 
-  ind_id <- unique(stays_simd$ind_id)
-  
-  # Run the deprivation analysis (saves the processed file to 'Data to be checked')
-  analyze_deprivation_aggregated(filename = paste0(ind, "_shiny_depr"), 
-                                 pop = "depr_pop_16+", 
-                                 ind_id, 
-                                 ind
-  )
-  
-  # Make data created available outside of function so it can be visually inspected if required
-  main_data_result <<- main_data
-  pop_grp_data_result <<- pop_grp_data
+main_data <- stays %>% 
+  filter(split_value == "Total") %>% 
+  select(code, ind_id, year, 
+         numerator, rate, upci, lowci, 
+         def_period, trend_axis) %>%
+  unique() 
 
-  
-}
+# Save 
+write_rds(main_data, paste0(profiles_data_folder, "Data to be checked/ARHS_mental_and_behav_shiny.rds"))
+write.csv(main_data, paste0(profiles_data_folder, "Data to be checked/ARHS_mental_and_behav_shiny.csv"), row.names = FALSE)
 
 
-# Run function to create final files
-prepare_final_files(ind = "ARHS_mental_and_behav")
+# 2 - population groups data (ie data behind population groups tab)
 
-# Run QA reports
-# main data: failing because the data aren't available at HB level (fix the .rmd later) "Warning: Error in eval: object 'S08' not found"
-run_qa(filename = "ARHS_mental_and_behav")
+pop_grp_data <- stays %>% # includes the contents of main data, as the 'total' for each split
+  select(code, ind_id, year, numerator, rate, upci, 
+         lowci, def_period, trend_axis, split_name, split_value,) 
+
+# Save
+write_rds(pop_grp_data, paste0(profiles_data_folder, "Data to be checked/ARHS_mental_and_behav_shiny_popgrp.rds"))
+write.csv(pop_grp_data, paste0(profiles_data_folder, "Data to be checked/ARHS_mental_and_behav_shiny_popgrp.csv"), row.names = FALSE)
+
+
+# 3 - SIMD data (ie data behind deprivation tab)
+
+# Save
+write_rds(stays_simd, paste0(profiles_data_folder, "Data to be checked/ARHS_mental_and_behav_shiny_ineq.rds"))
+write.csv(stays_simd, paste0(profiles_data_folder, "Data to be checked/ARHS_mental_and_behav_shiny_ineq.csv"), row.names = FALSE)
+
+
+
+
+
+##########################################################
+### QA -----
+##########################################################
+
+run_qa(type = "main", filename = "ARHS_mental_and_behav", test_file = FALSE) 
+run_qa(type = "popgrp", filename = "ARHS_mental_and_behav", test_file = FALSE)
+run_qa(type = "deprivation", filename = "ARHS_mental_and_behav", test_file = FALSE) 
