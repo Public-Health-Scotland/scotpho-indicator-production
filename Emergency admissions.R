7a07364426d50292826d96a8445a9e420ffa8a2a
abigap01 <abigail.prescott@phs.scot>
2025-10-13 16:11:01 +0000

    Refactoring to use new analysis functions and prevent need to produce all geographies independently.

--- Emergency admissions.R
+++ Emergency admissions.R
@@ -2,42 +2,16 @@
 # Parts 1 and 2 take about ~20 minutes to run
 
 #   Part 1 - Extract data from SMRA
-#   Part 2 - Create the different geographies basefiles
+#   Part 2 - Prepare the data
 #   Part 3 - Run analysis functions
-
-# TODO
-#how to deal with deprivation
 
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
+source("./functions/main_analysis.R")
+source("./functions/deprivation_analysis.R")
 
-#Function to create data for different geography levels
-create_geo_levels <- function(geography, list_pos) {
-  #If there are multiple admissions in one year it selects one per geography.
-  # So if a patient is admitted twice but with different residence in each, it will be counted twice
-  data_agg <- data_adm %>% rename(code = {{geography}} ) %>% 
-    group_by(link_no, year, code) %>% 
-    summarise(sex_grp = first(sex_grp), age_grp = first(age_grp), admissions = n()) %>%
-    ungroup()
-
-    #And now it aggregates total count of patients.
-    data_ea <- data_agg %>% group_by(year, code, sex_grp, age_grp) %>% 
-      count() %>% ungroup() %>% rename(numerator = n)
-      
-    data_ea_list[[list_pos]] <<- data_ea #assigning to list
-    
-    #select only patients who have had 2 or more admissions and 65+.
-    data_ma <- data_agg %>% 
-      filter(age_grp >= 14 & admissions >= 2 ) %>% 
-      group_by(year, code, sex_grp, age_grp) %>% 
-      count() %>% ungroup() %>% rename(numerator = n)
-  
-    data_ma_list[[list_pos]] <<- data_ma #assigning to list
-    
-}
+library(data.table) #for summarising large datasets
 
 ###############################################.
 ## Part 1 - Extract data from SMRA ----
@@ -66,65 +40,81 @@ data_adm <- as_tibble(dbGetQuery(channel, statement=paste0(
       AND sex not in ('9', '0')
       AND AGE_IN_YEARS is not null 
       AND (admission_type between '20' and '22' or admission_type between '30' and '39') 
-   ORDER BY link_no, cis_marker"))) %>% 
-  setNames(tolower(names(.)))  #variables to lower case
+   ORDER BY link_no, cis_marker"))) |> 
+  janitor::clean_names()  #variables to lower case
 
-# Bringing geography info.
-postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2024_2.rds') %>% 
-  setNames(tolower(names(.))) %>%   #variables to lower case
-  select(pc7, datazone2001, datazone2011, intzone2011, ca2019, hb2019, hscp2019)
+###############################################.
+## Part 2 - Prepare the data ----
+###############################################.
 
-geo_lookup <- readRDS(paste0(lookups, 'Geography/DataZone11_All_Geographies_Lookup.rds')) %>% 
-  select(datazone2011, hscp_locality) #as locality not present in the postcode one
+# Bringing geography info
+postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2025_2.rds') |> 
+  clean_names() |>    #variables to lower case
+  select(pc7, datazone2011)
 
-## Matching with geography lookups.
-data_adm <- left_join(x=data_adm, y=postcode_lookup, c("pc7")) #first with postcode 
-
-data_adm <- left_join(x=data_adm, y=geo_lookup, c("datazone2011")) %>% 
-  mutate(scotland = "S00000001") %>% 
-  subset(!(is.na(datazone2011))) %>%  #select out non-scottish
-  mutate_if(is.character, factor) %>% # converting variables into factors
-  create_agegroups() %>% # Creating age groups for standardization.
-  arrange(year, link_no, doadm) %>% #sorting needed for part 2
+## Matching with geography lookups
+data_joined <- left_join(data_adm, postcode_lookup, c("pc7")) |>  #first with postcode 
+  filter(!(is.na(datazone2011))) |> #select out non-scottish
+  create_agegroups() |> # Creating age groups for standardization.
+  arrange(year, link_no, doadm) |>  #sorting needed for part 2
   select(-pc7, -cis_marker, -age)
+  
+# Summarise the data using data.table package as dataset very large (~11 million rows)
+dt <- as.data.table(data_joined) #convert to data table type object
 
-saveRDS(data_adm, paste0(data_folder, 'Prepared Data/smr01_emergency_basefile.rds'))
-data_adm <- readRDS(paste0(data_folder, 'Prepared Data/smr01_emergency_basefile.rds'))
+#Aggregates episodes within an admission, keeping the first value for sex_grp and age_grp 
+dt_cleaned <- dt[, .(
+  sex_grp = first(sex_grp),
+  age_grp = first(age_grp),
+  admissions = .N), #summing all admissions
+  by = .(link_no, year, datazone2011)] #grouping variables
 
-###############################################.
-## Part 2 - Create the different geographies basefiles ----
-###############################################.
-#creating file for emergency admissions and multiple admissions
-data_ea_list <- list() #creating empty lists for placing data created by function
-data_ma_list <- list() 
+#Create emergency admissions indicator
+dt_ea <- dt_cleaned[, .N, by = .(year, datazone2011, sex_grp, age_grp)]  #aggregate on number of admissions per year split by sex, age and datazone
+setnames(dt_ea, "N", "numerator") #data.table syntax for renaming "N" to numerator
 
-# This will run the function for all those columns and for both indicators
-# It takes 20-30 minutes to run
-mapply(create_geo_levels, geography = c("scotland", "hb2019", "ca2019", "hscp2019",
-                                        "hscp_locality", "intzone2011"), list_pos = 1:6)
+#Create multiple emergency admissions indicator where age = 65+
+dt_mea <- dt_cleaned[age_grp >= 14 & admissions >= 2, .N, by = .(year, datazone2011, sex_grp, age_grp)] 
+setnames(dt_mea, "N", "numerator") #data.table syntax for renaming "N" to numerator
 
-data_ea <- do.call("rbind", data_ea_list) # converting from list into dataframe
-data_ma <- do.call("rbind", data_ma_list) # converting from list into dataframe
+#Save files to Prepared Data folder
+saveRDS(dt_ea, file.path(profiles_data_folder, "/Prepared Data/emergency_admissions_raw.rds"))
+saveRDS(dt_mea, file.path(profiles_data_folder, "/Prepared Data/multiple_emergency_admissions_raw.rds"))
 
-saveRDS(data_ea, paste0(data_folder, 'Prepared Data/ea_raw.rds'))
-saveRDS(data_ma, paste0(data_folder, 'Prepared Data/ma_raw.rds'))
 
 ###############################################.
 ## Part 3 - Run analysis functions ----
 ###############################################.
-# The function call uses a different geography to datazone11 or council as this way,
-# it skips the parts of the function that bring the geographical info.
-mapply(analyze_first, filename = c("ea", "ma"), geography = "all", measure = "stdrate", 
-       pop = c("DZ11_pop_allages", "DZ11_pop_65+"), yearstart = 2002, yearend = 2023,
-       time_agg = 3, epop_age = "normal", hscp = T)
 
-#Emergency admissions
-analyze_second(filename = "ea", measure = "stdrate", time_agg = 3, 
-               epop_total = 200000, ind_id = 20305, year_type = "calendar")
+#Emergency Admissions
+main_analysis(filename = "emergency_admissions", measure = "stdrate", 
+              geography = "datazone11", year_type = "calendar", ind_id = 20305,
+              time_agg = 3, yearstart = 2002, yearend = 2023, epop_total = 200000, 
+              epop_age = "normal", pop = "DZ11_pop_allages")
 
-#Multiple emergency admissions for 65+
-analyze_second(filename = "ma", measure = "stdrate", time_agg = 3, 
-               epop_total = 39000, ind_id = 20306, year_type = "calendar")
+deprivation_analysis(filename = "emergency_admissions", yearstart = 2014, yearend = 2023,
+                     time_agg = 3, year_type = "calendar", measure = "stdrate", pop_sex = "all",
+                     epop_age = "normal", epop_total = 200000, pop_age = "normal",
+                     ind_id = 20305)
 
+
+#Multiple Emergency Admissions in 65+
+main_analysis(filename = "multiple_emergency_admissions", measure = "stdrate", 
+              geography = "datazone11", year_type = "calendar", ind_id = 20306,
+              time_agg = 3, yearstart = 2002, yearend = 2023, epop_total = 39000, 
+              epop_age = "normal", pop = "DZ11_pop_65+")
+
+deprivation_analysis(filename = "emergency_admissions", yearstart = 2014, yearend = 2023,
+                     time_agg = 3, year_type = "calendar", measure = "stdrate", pop_sex = "all",
+                     epop_age = "normal", epop_total = 39000, pop_age = "normal",
+                     ind_id = 20305)
+
+################################################################################
 ##END
 
+#To do
+#Figure out how to add 65+ deprivation age group to deprivation analysis function
+#Figure out why numerators are not matching published data, especially over 65s.
+#Probably the multiple aspect not being handled properly
+#Create 65+ deprivation basefile
+#Figure out if there's another script floating about somewhere
