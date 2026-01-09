8f6422f29aeccc4feb7a9841fcc0fe324d74f139
monicamcgibbon <monica.mcgibbon@phs.scot>
2026-01-09 12:38:20 +0000

    adding notes, adding QA of combined file, deleting temp files, removing test parameters

--- single adult dwellings new.R
+++ single adult dwellings new.R
@@ -1,8 +1,25 @@
-# ScotPHO indicators: Single Adult Dwellings
+#############################.
+# Analyst notes ----
+#############################.
+
+# ScotPHO indicator: Single Adult Dwellings
+
+# Data extracted from statistics.gov.scot website at datazone, council and Scotland level. 
+# The DZ level data is published by different DZ versions, depending on the year (DZ01 for 2007-2013, DZ11 for 2014-2023 and DZ22 for 2024) 
+# This means that whilst CA level and above figures can be produced for all years (since already pre-aggregated)
+# small area figures (IZ/HSC locality) can only be produced from 2014-2023 for main indicator data (using 2011 DZs) 
+# and from 2007-2023 for deprivation data (using combination of 2001 and 2011 DZs)
+# 2022 DZ data can not yet be used for main analysis or deprivation analysis.
+
+# Files produced:
+# Main: Y
+# Deprivation: Y
+# Popgroups: N
+
 
 #   Part 1 - Create basefiles
 #     1a) Extract data from SG website
-#     1b) Create files for DZ01 and DZ22
+#     1b) Create files for DZ01
 #     1c) Create files for DZ11
 #     1d) Create files for DZ22
 #     1e) Create main deprivation file
@@ -18,9 +35,9 @@ source("./functions/deprivation_analysis.R") # deprivation function
 ## Part 1 - Create basefile ----
 ###############################################.
 
-######################
-# 1a - Extract data
-######################
+#########################.
+# 1a - Extract data ----
+#########################.
 #Note for 2026 update - try to use opendatascot package. Couldn't do historically do to file size or VPN issues
 
 col_names_n <- c("datazone", "name", 2006:2024)
@@ -37,9 +54,9 @@ sad_data_extract <- bind_rows(read_csv("https://statistics.gov.scot/slice/observ
   tidyr::pivot_wider(names_from = type, values_from = count) |>  
   filter(!is.na(denominator))
 
-################################
-# 1b - Create DZ01 file
-################################
+################################.
+# 1b - Create DZ01 file ----
+################################.
 #Main analysis function does not currently accommodate DZ01 data
 #If we use CA figures instead we can produce all geographies other than intermediate zones and localities
 
@@ -50,11 +67,11 @@ saveRDS(dz01_sad, file.path(profiles_data_folder, "Prepared Data/single_adult_dw
 
 main_analysis("single_adult_dwellings_dz01", measure = "percent", geography = "council",
               year_type = "calendar", ind_id = 20504, time_agg = 1, yearstart = 2007, yearend = 2013,
-              test_file = TRUE)
+              QA = FALSE)
 
-################################
-# 1c - Create DZ11 file
-################################
+################################.
+# 1c - Create DZ11 file ----
+################################.
 dz11_sad <- sad_data_extract |> 
   filter(year >= 2014 & year <= 2023 & str_detect(datazone, "S01")) #filter for relevant years and datazone only
 
@@ -62,11 +79,11 @@ saveRDS(dz11_sad, file.path(profiles_data_folder, "Prepared Data/single_adult_dw
 
 main_analysis("single_adult_dwellings_dz11", measure = "percent", geography = "datazone11",
               year_type = "calendar", ind_id = 20504, time_agg = 1, yearstart = 2014, yearend = 2023,
-              test_file = TRUE)
+              QA = FALSE)
 
-################################
-# 1d - Create DZ22 file
-################################
+################################.
+# 1d - Create DZ22 file ----
+################################.
 #Due to the lack of available lookups for DZ22 as of December 2025, we can't use the 2024 datazone figures
 #Repeating method from 1b (DZ01), so IZs and localities will not be available for 2024 right now
 #For 2026 update, ideally change this section to resemble section 1c and run functions at datazone22 level
@@ -78,11 +95,12 @@ saveRDS(dz22_sad, file.path(profiles_data_folder, "Prepared Data/single_adult_dw
 
 main_analysis("single_adult_dwellings_dz22", measure = "percent", geography = "council",
               year_type = "calendar", ind_id = 20504, time_agg = 1, yearstart = 2024, yearend = 2024,
-              test_file = TRUE)
+              QA = FALSE)
 
-################################
-# 1e - Create deprivation file
-################################
+
+####################################.
+# 1e - Create deprivation file ----
+####################################.
 #Cannot include 2024 data as SIMD for dz22 not released yet
 
 sad_data_depr <- sad_data_extract |> 
@@ -91,20 +109,34 @@ sad_data_depr <- sad_data_extract |>
 saveRDS(sad_data_depr, file.path(profiles_data_folder, "Prepared Data/single_adult_dwellings_raw.rds"))
 
 deprivation_analysis(filename = "single_adult_dwellings", yearstart = 2007, yearend = 2023,
-                     time_agg = 1, year_type = "calendar", measure = "percent", ind_id = 20504,
-                     test_file = TRUE)
+                     time_agg = 1, year_type = "calendar", measure = "percent", ind_id = 20504)
 
-#####################################
-# 2 - Join DZ01, DZ11 and DZ22 data
-#####################################
+###########################################################.
+# Part 2 - Aggregate files from different dz periods ----
+##########################################################.
+
+# Read in results from main analysis 
 dz01_sad <- readRDS(file.path(profiles_data_folder, "Test Shiny Data/single_adult_dwellings_dz01_shiny.rds"))
 dz11_sad <- readRDS(file.path(profiles_data_folder, "Test Shiny Data/single_adult_dwellings_dz11_shiny.rds"))
 dz22_sad <- readRDS(file.path(profiles_data_folder, "Test Shiny Data/single_adult_dwellings_dz22_shiny.rds"))
 
+# combine into one single file 
 single_adult_dwellings_final <- bind_rows(dz01_sad, dz11_sad, dz22_sad)
 
-write.csv(single_adult_dwellings_final, file.path(profiles_data_folder, "Data to be checked/single_adult_dwellings.csv"), row.names = FALSE)
-saveRDS(single_adult_dwellings_final, file.path(profiles_data_folder, "Data to be checked/single_adult_dwellings.rds"))
+# delete the 6 individual files produced from main_analysis from data to be checked folder - no longer needed
+list.files(
+  path = file.path(profiles_data_folder, "Data to be checked"), 
+  pattern = "single_adult_dwellings_dz[01-22]+_shiny",
+  full.name = TRUE
+  ) |>
+  walk(~ file.remove(.x))
 
-################################################################################
-#End
+# save combined file 
+write.csv(single_adult_dwellings_final, file.path(profiles_data_folder, "Data to be checked/single_adult_dwellings_shiny.csv"), row.names = FALSE)
+saveRDS(single_adult_dwellings_final, file.path(profiles_data_folder, "Data to be checked/single_adult_dwellings_shiny.rds"))
+
+
+# QA final file
+run_qa(filename = "single_adult_dwellings", type = "main")
+
+## end
