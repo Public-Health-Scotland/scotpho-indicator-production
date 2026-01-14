45836185bc15c8d3c52a61d7db7aa2c365c0a91a
monicamcgibbon <monica.mcgibbon@phs.scot>
2026-01-14 14:30:07 +0000

    hospitalisations update

--- patient hospitalisations from CHD.R
+++ patient hospitalisations from CHD.R
@@ -1,3 +1,11 @@
+## MM Jan 2026:
+# Partial update done at CA level and above to include 2024/25 for main indicator file only. 
+# Analysis functions to be re-run Spring 2026:
+# - re-run main_analysis (no changes to parameters needed)
+# run deprivation_analysis (change yearend to 2024)
+
+
+
 # ScotPHO indicators: patients hospitalised with coronary heart disease
 
 #   Part 1 - Extract data from SMRA.
@@ -7,8 +15,8 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("1.indicator_analysis.R") #Normal indicator functions
-source("2.deprivation_analysis.R") # deprivation function
+source("functions/main_analysis.R") #Normal indicator functions
+source("functions/deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Extract data from SMRA ----
@@ -27,71 +35,64 @@ hospitalisation_CHD <- as_tibble(dbGetQuery(channel, statement=
         THEN extract(year from admission_date) 
         ELSE extract(year from admission_date) -1 END as year
     FROM ANALYSIS.SMR01_PI z 
-      WHERE admission_date between  '1 April 2002' and '31 March 2024'
+      WHERE admission_date between  '1 April 2002' and '31 March 2025'
       AND sex <> 0 
       AND regexp_like(main_condition, 'I2[0-5]')
     GROUP BY link_no,
       CASE WHEN extract(month from admission_date) > 3 
           THEN extract(year from admission_date) 
           ELSE extract(year from admission_date) -1 END" )) %>% 
-  setNames(tolower(names(.)))  #variables to lower case
-
-# Creating age groups for standardization.
-hospitalisation_CHD <- hospitalisation_CHD %>% mutate(age_grp = case_when( 
-  age < 5 ~ 1, age > 4 & age <10 ~ 2, age > 9 & age <15 ~ 3, age > 14 & age <20 ~ 4,
-  age > 19 & age <25 ~ 5, age > 24 & age <30 ~ 6, age > 29 & age <35 ~ 7, 
-  age > 34 & age <40 ~ 8, age > 39 & age <45 ~ 9, age > 44 & age <50 ~ 10,
-  age > 49 & age <55 ~ 11, age > 54 & age <60 ~ 12, age > 59 & age <65 ~ 13, 
-  age > 64 & age <70 ~ 14, age > 69 & age <75 ~ 15, age > 74 & age <80 ~ 16,
-  age > 79 & age <85 ~ 17, age > 84 & age <90 ~ 18, age > 89 ~ 19, 
-  TRUE ~ as.numeric(age)
-))
+  setNames(tolower(names(.))) %>%  #variables to lower case
+  create_agegroups() #Creating age groups for standardization.
 
 # Bringing  LA and datazone info.
-postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2024_2.rds') %>% 
+postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2025_2.rds') %>% 
   setNames(tolower(names(.))) %>%   #variables to lower case
   select(pc7, datazone2001, datazone2011, ca2011)
 
 hospitalisation_CHD <- left_join(hospitalisation_CHD, postcode_lookup, "pc7") %>% 
-  subset(!(is.na(datazone2011))) %>%  #select out non-scottish
-  mutate_if(is.character, factor) # converting variables into factors
+  subset(!(is.na(datazone2011)))  #select out non-scottish
+
 
 ###############################################.
 ## Part 2 - Create the different geographies basefiles ----
 ###############################################.
 # Datazone2011
-hospitalisation_CHD_dz11 <- hospitalisation_CHD %>% group_by(year, datazone2011, sex_grp, age_grp) %>%  
-  summarize(numerator = n()) %>% ungroup() %>%  rename(datazone = datazone2011)
+hospitalisation_CHD_dz11 <- hospitalisation_CHD %>% 
+  group_by(year, datazone2011, sex_grp, age_grp) %>%  
+  summarize(numerator = n()) %>% 
+  ungroup() %>%  
+  rename(datazone = datazone2011)
 
-saveRDS(hospitalisation_CHD_dz11, file=paste0(data_folder, 'Prepared Data/hospitalisation_CHD_dz11_raw.rds'))
+saveRDS(hospitalisation_CHD_dz11, file.path(profiles_data_folder, 'Prepared Data/hospitalisation_CHD_dz11_raw.rds'))
 
 ###############################################.
 #Deprivation basefile
 # DZ 2001 data needed up to 2013 to enable matching to advised SIMD
-hospitalisation_CHD_dz01 <- hospitalisation_CHD %>% group_by(year, datazone2001, sex_grp, age_grp) %>%  
-  summarize(numerator = n()) %>% ungroup() %>% rename(datazone = datazone2001) %>% 
+hospitalisation_CHD_dz01 <- hospitalisation_CHD %>% 
+  group_by(year, datazone2001, sex_grp, age_grp) %>%  
+  summarize(numerator = n()) %>% 
+  ungroup() %>% 
+  rename(datazone = datazone2001) %>% 
   subset(year<=2013)
 
-dep_file <- rbind(hospitalisation_CHD_dz01, hospitalisation_CHD_dz11 %>% subset(year>=2014)) %>% #join dz01 and dz11
-  mutate(age_grp = factor(age_grp))
+dep_file <- rbind(hospitalisation_CHD_dz01, hospitalisation_CHD_dz11 %>% subset(year>=2014)) #join dz01 and dz11
 
 
-saveRDS(dep_file, file=paste0(data_folder, 'Prepared Data/hospitalisation_CHD_depr_raw.rds'))
+
+saveRDS(dep_file, file.path(profiles_data_folder, 'Prepared Data/hospitalisation_CHD_depr_raw.rds'))
 
 ###############################################.
 ## Part 3 - Run analysis functions ----
 ###############################################.
-analyze_first(filename = "hospitalisation_CHD_dz11", geography = "datazone11", measure = "stdrate", 
-              pop = "DZ11_pop_allages", yearstart = 2002, yearend = 2023,
-              time_agg = 3, epop_age = "normal")
+main_analysis(filename = "hospitalisation_CHD_dz11", geography = "datazone11", measure = "stdrate", 
+              pop = "DZ11_pop_allages", yearstart = 2002, yearend = 2024,
+              time_agg = 3, epop_age = "normal", epop_total = 200000, ind_id = 20303, year_type = "financial")
 
-analyze_second(filename = "hospitalisation_CHD_dz11", measure = "stdrate", time_agg = 3, 
-               epop_total = 200000, ind_id = 20303, year_type = "financial")
 
 #Deprivation analysis function
-analyze_deprivation(filename="hospitalisation_CHD_depr", measure="stdrate", time_agg = 3, 
+deprivation_analysis(filename="hospitalisation_CHD_depr", measure="stdrate", time_agg = 3, 
                     yearstart= 2002, yearend=2023,   year_type = "financial", 
-                    pop = "depr_pop_allages", epop_age= "normal",
-                    epop_total = 200000, ind_id = 20303)
+                    epop_age= "normal", epop_total = 200000, ind_id = 20303)
 
 ##END
