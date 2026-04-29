5973f4ab1bb25470c801bb2f610352601411eaa3
abigap01 <abigail.prescott@phs.scot>
2026-04-29 10:35:27 +0000

    24/25 update with new data source

--- Teenage pregnancies.R
+++ Teenage pregnancies.R
@@ -6,8 +6,8 @@
 ###############################################.
 ## Packages/Filepaths/Functions ----
 ###############################################.
-source("./1.indicator_analysis.R") #Normal indicator functions
-source("./2.deprivation_analysis.R") # deprivation function
+source("./functions/main_analysis.R") #Normal indicator functions
+source("./functions/deprivation_analysis.R") # deprivation function
 
 ###############################################.
 ## Part 1 - Prepare basefile ----
@@ -16,28 +16,26 @@ source("./2.deprivation_analysis.R") # deprivation function
 # Reading data provided by maternity team
 # All data is included, but to calculate the rates only 15-19 female pop is used, 
 # following ISD publication methodology.
-teen_preg <- read_csv( paste0(data_folder, "Received Data/Teenage pregnancies/IR2024-00770.csv")) %>% 
-  setNames(tolower(names(.))) %>% #set names to lower case
+teen_preg <- read_csv(file.path(profiles_data_folder, "Received Data/Teenage pregnancies/IR2026-00003_TeenPregs.csv")) %>% 
+  clean_names() %>% #set names to lower case
   rename(datazone = datazone2011, numerator = tp, year = yearcon) %>% 
-  subset(!(is.na(datazone) | datazone == "         ")) %>% #excluding non-Scottish residents
+  subset(!(is.na(datazone) | datazone == "         " | datazone == "Unknown")) %>% #excluding non-Scottish residents
   # aggregate to get the count, removing age groups
   group_by(year, datazone) %>% 
   summarise(numerator = sum(numerator, na.rm =T)) %>% ungroup()
 
-saveRDS(teen_preg, file=paste0(data_folder, 'Prepared Data/teen_preg_raw.rds'))
+saveRDS(teen_preg, file.path(profiles_data_folder, 'Prepared Data/teen_preg_raw.rds'))
 
 ###############################################.
 ## Part 2 - Run analysis functions ----
 ###############################################.
-analyze_first(filename = "teen_preg", geography = "datazone11", measure = "crude", 
-              yearstart = 2002, yearend = 2022, time_agg = 3, pop='DZ11_pop_fem15to19')
-
-analyze_second(filename = "teen_preg", measure = "crude", time_agg = 3, crude_rate=1000,
-               ind_id = 21001, year_type = "calendar")
+main_analysis(filename = "teen_preg", geography = "datazone11", measure = "crude",
+              yearstart = 2002, yearend = 2023, time_agg = 3, pop = "DZ11_pop_fem15to19",
+              ind_id = 21001, year_type = "calendar", crude_rate = 1000)
 
 #Deprivation analysis function
-analyze_deprivation(filename="teen_preg", measure="crude", time_agg=3, crude_rate=1000,
-                    yearstart= 2014, yearend=2022, year_type = "calendar", 
-                    pop = "depr_pop_fem15to19", ind_id = 21001)
+deprivation_analysis(filename="teen_preg", measure="crude", time_agg=3, crude_rate=1000,
+                    yearstart= 2014, yearend=2023, year_type = "calendar", 
+                     ind_id = 21001)
 
 ##END
