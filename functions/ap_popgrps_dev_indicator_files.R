7255bd29d0d97c6fe9cd2ac0db6f55a0c0f67b60
abigap01 <abigail.prescott@phs.scot>
2026-05-11 11:09:16 +0000

    Developing dummy data

--- functions/ap_popgrps_dev_indicator_files.R
+++ functions/ap_popgrps_dev_indicator_files.R
@@ -1,3 +1,7 @@
+#Still to think about
+#Pop group classes? add something to the validation? I think we'd always want them to be characters?
+
+
 #4 main measure types
 
 #Percentage - 
@@ -7,12 +11,30 @@
 
 #First, create data files for each measure type to be input into the new function
 
+source("./functions/popgrps_analysis.R") #Sourcing new function for profiles data folder filepath
+################################################################################
+#Percentage
+bowel_screening <- readRDS(file.path(profiles_data_folder, "Received Data/Bowel Screening Uptake/scotPHO_bowel2023.rds")) |> 
+  mutate(sex = case_when(sex == "1" ~ "Male", 
+                         sex == "2" ~ "Female",
+                         TRUE ~ NA_character_))
+
+saveRDS(bowel_screening, file.path(profiles_data_folder, "Prepared Data/bowel_screening_popgrps_raw.rds"))
+
+#Testing
+
+splits_bs <- list(
+  age_group = c("50-54", "55-59", "60-64", "65-69", "70-74"),
+  sex = c("Male", "Female"))
+
 source("./functions/popgrps_analysis.R")
 
+data <- popgrps_analysis(filename = "bowel_screening", measure = "percent", geography = "datazone11",
+                         year_type = "calendar", ind_id = 21102, time_agg = 3, yearstart = 2008,
+                         yearend = 2023, test_file = TRUE, splits = splits_bs)
 
-
+################################################################################
 #Crude rate
-
 teen_preg <- read_csv(file.path(profiles_data_folder, "Received Data/Teenage pregnancies/IR2026-00003_TeenPregs.csv")) %>% 
   clean_names()  |>  #set names to lower case
   rename(datazone = datazone2011, numerator = tp, year = yearcon)  |>  
@@ -25,15 +47,33 @@ teen_preg <- read_csv(file.path(profiles_data_folder, "Received Data/Teenage pre
 
 
 saveRDS(teen_preg, file.path(profiles_data_folder, "Prepared Data/teen_preg_popgrps_raw.rds"))
+################################################################################
+
 
 splits_tp <- list(
-  age_grp = c("Under 16 year", "16-17 years", "Under 20 years"),
+  age_grp = c("Under 16 years", "16-17 years", "Under 20 years"),
   dummy_group = c("A", "B"))
 
+source("./functions/popgrps_analysis.R")
 
-
-popgrps_analysis(filename = "teen_preg", measure = "crude", geography = "datazone11",
+data <- popgrps_analysis(filename = "teen_preg", measure = "crude", geography = "datazone11",
                  year_type = "calendar", ind_id = 21001, time_agg = 3, yearstart = 2002,
                  yearend = 2023, pop = "DZ11_pop_fem15to19", crude_rate = 1000, test_file = TRUE,
                  splits = splits_tp)
+
+
+#splits variable testing
+#the validate_popgrps_columns checks:
+#1) That all split names specified in the splits argument are actually present in the data
+#2) That there are no split values in the data that do not match an acceptable value from the arguments. 
+#It will not check that all split values specified are actually in the data as they may not be for legitimate reasons
+
+#For testing, consider
+#Adding a new list element for a split not in the data e.g. SIMD
+#Changing the name of a split e.g. age_grp -> age_group
+#Changing the name of a split value e.g. "Under 16 years" -> "Under 16 year"
+#These should all throw errors
+
+
+
 
