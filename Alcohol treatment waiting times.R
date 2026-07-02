3e148a59bd0d8521383dfb83e227fed20cacfad4
BorderBhoy70 <nicholas.scott@phs.scot>
2026-07-02 17:07:27 +0000

    Checked and updated Part 1 code to create basefile

--- Alcohol treatment waiting times.R
+++ Alcohol treatment waiting times.R
@@ -24,7 +24,57 @@ source("functions/main_analysis.R") # Brings in the main_analysis() function
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Read in alcohol treatment waiting times data file supplied by PHS Drugs Team.
-alcohol_wt <- read_csv(paste0(profiles_data_folder, "/Received Data/Drug and alcohol treatment waiting times/2025 request/alcohol_waiting_times_2024-25.csv"))
+alcohol_wt <- read_csv(paste0(profiles_data_folder, "/Received Data/Drug and alcohol treatment waiting times/2025 request/alcohol_waiting_times_2024-25.csv")) |>
+  clean_names() |> # Variable names to lower case
+  mutate(code = case_when( # Create a variable called code to record the code that matches the ADP/HB name in the geography variable
+    geography == "Clackmannanshire_ADP" ~ "S11000005", geography == "Falkirk_ADP" ~ "S11000013", 
+    geography == "Stirling_ADP" ~ "S11000029", geography == "East Ayrshire_ADP" ~ "S11000008", 
+    geography == "North Ayrshire_ADP" ~ "S11000020", geography == "South Ayrshire_ADP" ~ "S11000027", 
+    geography == "Mid and East Lothian_ADP" ~ "S11000051", geography == "West Lothian_ADP" ~ "S11000031", 
+    geography == "City of Edinburgh_ADP" ~ "S11000012", geography == "East Renfrewshire_ADP" ~ "S11000011", 
+    geography == "East Dunbartonshire_ADP" ~ "S11000009", geography == "Inverclyde_ADP" ~ "S11000017", 
+    geography == "Renfrewshire_ADP" ~ "S11000024", geography == "West Dunbartonshire_ADP" ~ "S11000030", 
+    geography == "City of Glasgow_ADP" ~ "S11000015", geography == "Highland_ADP" ~ "S11000016", 
+    geography == "Argyll & Bute_ADP" ~ "S11000004", geography == "Moray_ADP" ~ "S11000019", 
+    geography == "Aberdeen City_ADP" ~ "S11000001", geography == "Aberdeenshire_ADP" ~ "S11000002", 
+    geography == "Perth & Kinross_ADP" ~ "S11000023", geography == "Angus_ADP" ~ "S11000003", 
+    geography == "Dundee City_ADP" ~ "S11000007", geography == "Dumfries & Galloway_ADP" ~ "S11000006", 
+    geography == "Fife_ADP" ~ "S11000014", geography == "Orkney Islands_ADP" ~ "S11000022", 
+    geography == "Scottish Borders_ADP" ~ "S11000025", geography == "Shetland Islands_ADP" ~ "S11000026", 
+    geography == "Western Isles_ADP" ~ "S11000032", geography == "Ayrshire & Arran_HB" ~ "S08000015", 
+    geography == "Borders_HB" ~ "S08000016", geography == "Dumfries & Galloway_HB" ~ "S08000017", 
+    geography == "Fife_HB" ~ "S08000029", geography == "Forth Valley_HB" ~ "S08000019",
+    geography == "Grampian_HB" ~ "S08000020", geography == "Greater Glasgow & Clyde_HB" ~ "S08000031", 
+    geography == "Highland_HB" ~ "S08000022", geography == "Lanarkshire_HB" ~ "S08000032", 
+    geography == "Lothian_HB" ~ "S08000024", geography == "Orkney_HB" ~ "S08000025", 
+    geography == "Shetland_HB" ~ "S08000026", geography == "Tayside_HB" ~ "S08000030", 
+    geography == "Western Isles_HB" ~ "S08000028", 
+    geography == "Scotland" ~ "S00000001", TRUE ~ "Error")) |>
+  mutate(year = as.numeric(substr(financial_year,1,4))) # Create variable called year to record financial year in format required by main_analysis() function
+
+# North and South Lanarkshire ADPs are combined as Lanarkshire ADP for the purposes of the Profiles tool
+alcohol_wt <- alcohol_wt |>
+  mutate(code = case_when(geography == "South Lanarkshire_ADP" ~ "S11000052",
+                          geography == "North Lanarkshire_ADP" ~ "S11000052",
+                          TRUE ~ as.character(code))) |> 
+  mutate(geography = case_when(code == "S11000052" ~ "Lanarkshire_ADP",
+                               TRUE ~ as.character(geography))) |> 
+  group_by(year, geography, code) |> 
+  summarise(numerator = sum(numerator),
+            denominator = sum(denominator)) |> 
+  ungroup()
+
+# Save basefile to 'Prepared Data' folder to be fed into main_analysis() function
+saveRDS(alcohol_wt, file=paste0(profiles_data_folder, '/Prepared Data/alcohol_waiting_times_raw.rds'))
+
+# This will save a file with variables year/geography/code/numerator/denominator - Is this correct?
+# Name of file - how do we know what the filename should be?
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# PART 2 - Run alcohol treatment waiting times basefile through main_analysis() function and save output to 'Data to be checked' folder ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~
